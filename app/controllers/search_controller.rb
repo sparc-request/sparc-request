5d38cc895e7b0b977b07d29d029b6b63574bd332
Andrew Cates <amcates@gmail.com>
2012-09-25 16:50:29 -0400

    filter out by is_available and don't search description

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    results = Service.where("name LIKE '%#{params[:term]}%' OR description LIKE '%#{params[:term]}%' OR abbreviation LIKE '%#{params[:term]}%'")
+    results = Service.where("(name LIKE '%#{params[:term]}%' OR abbreviation LIKE '%#{params[:term]}%') AND is_available != false")
                      .map{|s| {:parents => [s.organization.abbreviation, s.organization.parents.flatten.map(&:abbreviation).reverse].reverse.join(' | '), :label => s.name, :value => s.id}}
     results = [{:label => 'No Results'}] if results.empty?
     render :json => results.to_json

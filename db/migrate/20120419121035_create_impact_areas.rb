b4e034e52c7dca81737bb2420204815b7260ff27
Paul Brannan <curlypaul924@gmail.com>
2012-10-08 11:23:24 -0400

    Made affiliations and impact_areas have a single string field
    This field contains the name of the affiliation or impact area.  Before,
    each affiliation or impact area was a single column in the database.
    This meant changing the schema whenever a new affiliation or impact area
    was added.  It also meant that the schema was very MUSC specific.  This
    changes addresses both of these issues.

--- db/migrate/20120419121035_create_impact_areas.rb
+++ db/migrate/20120419121035_create_impact_areas.rb
@@ -2,12 +2,7 @@ class CreateImpactAreas < ActiveRecord::Migration
   def change
     create_table :impact_areas do |t|
       t.integer :protocol_id
-      t.boolean :hiv_aids
-      t.boolean :pediatrics
-      t.boolean :stroke
-      t.boolean :diabetes
-      t.boolean :hypertension
-      t.boolean :cancer
+      t.string  :name
 
       t.timestamps
     end

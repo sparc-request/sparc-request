e0d8c4d3697de934de90cc8c6923bd0cf4e4ade7
Paul Brannan <curlypaul924@gmail.com>
2012-10-09 13:13:50 -0400

    Changed spelling of plural types from *s to *s_info

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -2,11 +2,11 @@ class StudiesController < ApplicationController
   def new
     @service_request = ServiceRequest.find session[:service_request_id]
     @study = Study.new
-    @study.build_research_types
-    @study.build_human_subjects
-    @study.build_vertebrate_animals
-    @study.build_investigational_products
-    @study.build_ip_patents
+    @study.build_research_types_info
+    @study.build_human_subjects_info
+    @study.build_vertebrate_animals_info
+    @study.build_investigational_products_info
+    @study.build_ip_patents_info
     @study.build_study_types
     @study.build_impact_areas
     @study.build_affiliations  
@@ -19,7 +19,7 @@ class StudiesController < ApplicationController
     puts "#"*50
     @study = Study.new params[:study]
     puts "#"*50
-    puts @study.research_types
+    puts @study.research_types_info
     puts "#"*50
 
     if @study.valid?

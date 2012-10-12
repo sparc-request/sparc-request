cd1d0b25243faa7d86f647d6593da7585892f05e
Andrew Cates <amcates@gmail.com>
2012-10-12 09:49:16 -0400

    paginating calendar and setup headers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,7 +29,35 @@ module ApplicationHelper
     end
   end
 
+  def generate_visit_header_row service_request, page
+    page = page == 0 ? 1 : page
+    beginning_visit = (page * 5) - 4
+    ending_visit = (page * 5) > service_request.visit_count ? service_request.visit_count : (page * 5)
+    returning_html = ""
 
+    (beginning_visit .. ending_visit).each do |n|
+      returning_html += content_tag(:th, "Visit #{n}")
+    end
+
+    ((page * 5) - service_request.visit_count).times do
+      returning_html += content_tag(:th, "&nbsp;")
+    end
+
+    raw(returning_html)
+  end
+
+  def generate_visit_navigation service_request, page
+    page = page == 0 ? 1 : page
+    beginning_visit = (page * 5) - 4
+    ending_visit = (page * 5) > service_request.visit_count ? service_request.visit_count : (page * 5)
+    returning_html = ""
+    returning_html += link_to("<-", service_calendar_service_request_path(service_request, :page => page - 1), :class => 'visit_back') unless page <= 1
+    returning_html += "Visits #{beginning_visit} - #{ending_visit} of #{service_request.visit_count}"
+    returning_html += link_to("->", service_calendar_service_request_path(service_request, :page => page + 1), :class => 'visit_forward') unless ((page + 1) * 5) - 4 > service_request.visit_count
+
+    raw(returning_html)
+  end
+  
   def portal_link
     case Rails.env
     when "development"

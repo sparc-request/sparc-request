8dbe490797980495ad5a0d937b300e3c0288183f
Andrew Cates <amcates@gmail.com>
2012-10-22 14:56:04 -0400

    symlink other apps in so that they can be served using passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,6 +47,9 @@ namespace :db do
   task :symlink do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/setup_load_paths.rb #{release_path}/config/setup_load_paths.rb"
+
+    #symlink other apps so that sparc-rails can run as root
+    run "ln -nfs /var/www/rails/catalog_manager/current/public #{release_path}/public/catalog_manager"
   end
 
   desc "seed the database for the rails environment"

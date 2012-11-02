8a6c14416878d90eae34d9553df5c0346e35ab03
bjk7 <brian.kelsey2@gmail.com>
2012-11-02 11:29:15 -0400

    Fixing rounding error in helper

--- app/helpers/subsidies_helper.rb
+++ app/helpers/subsidies_helper.rb
@@ -1,12 +1,14 @@
 module SubsidiesHelper
 
   def display_requested_funding direct_cost, contribution
-    rf = direct_cost / 100 - contribution rescue 0
+    # multiply contribution by 100 to convert to cents
+    rf = direct_cost - contribution * 100 rescue 0
     currency_converter(rf)
   end
 
   def calculate_subsidy_percentage direct_cost, contribution
-    funded_amount = direct_cost / 100 - contribution rescue 0
-    (funded_amount / direct_cost).round(2) * 100
+    # multiply contribution by 100 to convert to cents
+    funded_amount = direct_cost - contribution * 100 rescue 0
+    ((funded_amount / direct_cost) * 100).round(1)
   end
 end

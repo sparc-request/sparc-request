924b17278c1a2a13730c7f73a7ad7f3bd9d90877
Andrew Cates <amcates@gmail.com>
2012-11-14 01:41:32 -0500

    ssr edit is 3/4 done

--- app/helpers/subsidies_helper.rb
+++ app/helpers/subsidies_helper.rb
@@ -8,6 +8,7 @@ module SubsidiesHelper
 
   def calculate_subsidy_percentage direct_cost, contribution
     # multiply contribution by 100 to convert to cents
+    return 0 if direct_cost == 0.0
     funded_amount = direct_cost - contribution rescue 0
     ((funded_amount / direct_cost) * 100).round(1)
   end

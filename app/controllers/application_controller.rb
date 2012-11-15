5ae629dd564bbce1de3f1ac8b4b5b0c300c9e046
Andrew Cates <amcates@gmail.com>
2012-11-15 14:05:49 -0500

    new config options and handling of users not being logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,20 @@ class ApplicationController < ActionController::Base
   # for now we are assuming auto login
   def authenticate
     @current_user = Identity.find_by_ldap_uid 'jug2'
+
+    #@current_user = nil #uncomment to test as if not logged in
+    # let's do some cleanup if the user changes
+    if @current_user and @current_user.id != session[:identity_id]
+        session.delete(:service_request_id) 
+        session.delete(:sub_service_request_id)
+        session.delete(:first_draft)
+    end
+
+    session[:identity_id] = @current_user.id unless @current_user.nil?
   end
 
   def current_user
-    Identity.find_by_ldap_uid('anc63').id
+    Identity.find session[:identity_id]
   end
   
   def setup_session
@@ -23,17 +33,18 @@ class ApplicationController < ActionController::Base
     @line_items = nil
 
     if params[:controller] == 'service_requests'
-      # blow the session away if we aren't logged in and don't have a service request id
-      unless @current_user and params[:id]
+
+      # we are starting a new service request
+      unless params[:id]
+        if session[:service_request_id] != params[:id].to_i # we are trying to access different service request, so we want to make sure we remove the first_draft session variable
+          session.delete(:first_draft) 
+        end
+        
         session.delete(:service_request_id) 
         session.delete(:sub_service_request_id)
       end
      
       if @current_user and params[:id] # we are logged in and trying to access a service request
-        
-        if session[:service_request_id] != params[:id].to_i # we are trying to access different service request, so we want to make sure we remove the first_draft session variable
-          session.delete(:first_draft) 
-        end
         
         # let's find the service requests with this id
         @service_request = @current_user.protocol_service_requests.where(:id => params[:id]).empty? ? @current_user.requested_service_requests.where(:id => params[:id]).first : @current_user.protocol_service_requests.where(:id => params[:id]).first
@@ -63,14 +74,20 @@ class ApplicationController < ActionController::Base
             error = "The service request you are trying to access is not editable. <br />If you believe this is in error please contact, #{@error_contact}, and provide the following information:"
             error += "<br /> Reference #: SSR#{session[:sub_service_request_id]}"
             render :partial => 'service_requests/authorization_error', :locals => {:error => error}
-          else
+          else #otherwise let's replace the line items we use with the ones provided by the service request
             @line_items = @sub_service_request.line_items
           end
         end
-      elsif @current_user and not session[:service_request_id]
+      elsif @current_user and not session[:service_request_id] # we are logged in and we want to create a new service request
         @service_request = @current_user.requested_service_requests.new(:service_requester_id => @current_user.id)
-        if params[:protocol_id]
-          @service_request.protocol_id = params[:protocol_id]
+        if params[:protocol_id] # we want to create a new service request that belongs to an existing protocol
+          if @current_user.protocols.where(:id => params[:protocol_id]).empty? # this user doesn't have permission to create service request under this protocol
+            error = "You are attempting to create a service request under a study/project that you do not have permissions to access. <br />If you believe this is in error please contact, #{@error_contact}, and provide the following information:"
+            error += "<br /> Reference #: PROTOCOL#{params[:protocol_id]}"
+            render :partial => 'service_requests/authorization_error', :locals => {:error => error}
+          else # otherwise associate the service request with this protocol
+            @service_request.protocol_id = params[:protocol_id]
+          end
         end
 
         @service_request.save :validate => false
@@ -78,8 +95,18 @@ class ApplicationController < ActionController::Base
         session[:service_request_id] = @service_request.id
         session[:first_draft] = true
         redirect_to catalog_service_request_path(@service_request)
-      else #we aren't logged in so let's do some funky stuff
-        render :text => 'You are not logged in'
+      else #we aren't logged in so let's create a service request that doesn't have a requester.  one will be added once they click 'Submit Request'
+        if session[:service_request_id]
+          @service_request = ServiceRequest.find session[:service_request_id]
+          @line_items = @service_request.line_items
+        else
+          @service_request = ServiceRequest.new
+          @service_request.save :validate => false
+          session[:first_draft] = true
+          session[:service_request_id] = @service_request.id
+          redirect_to catalog_service_request_path(@service_request)
+        end
+
       end
     elsif ['search', 'service_calendars'].include? params[:controller]
       @service_request = ServiceRequest.find session[:service_request_id]
@@ -98,6 +125,7 @@ class ApplicationController < ActionController::Base
       @default_mail_to = @application_config['default_mail_to']
       @user_portal_link = @application_config['user_portal_link']
       @error_contact = @application_config['error_contact']
+      @application_title = @application_config['application_title']
     rescue
       raise "application.yml not found, see config/application.yml.example"
     end

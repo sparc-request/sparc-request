7aaa8e2dafcd2dc29b3c48b3f1d8e3e3206325f0
Paul Brannan <curlypaul924@gmail.com>
2012-11-21 13:02:14 -0500

    Multiple changes.
    - Moved ApplicationController tests into application_controller_spec.rb
    - Added a test for ServiceRequestsController#catalog

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
-require './app/controllers/application_controller' # TODO: why is this needed?
+# require './app/controllers/application_controller' # TODO: why is this needed?
 
 describe ApplicationController do
+  controller do
+    def index
+      render :json => { }
+    end
+  end
+
+  describe 'GET index' do
+    # TODO: This test really belongs in ApplicationControllerSpec, but I
+    # don't know how to make it work there yet...
+    it 'should assign current_user' do
+      get :index
+      assigns(:current_user).class.should eq Identity
+    end
+  end
 end
 
--- spec/controllers/service_requests_controller_spec.rb
+++ spec/controllers/service_requests_controller_spec.rb
@@ -1,20 +1,21 @@
 describe ServiceRequestsController do
-  describe 'GET catalog' do
-    # TODO: This test really belongs in ApplicationControllerSpec, but I
-    # don't know how to make it work there yet...
-    it 'should assign current_user' do
-      get :catalog
-      assigns(:current_user).class.should eq Identity
-    end
+  context 'testing base class' do
+  end
 
+  context 'testing everything else' do
     let!(:service_request) { FactoryGirl.create(:service_request) }
+
+    before(:each) do
+      controller.stub!(:authenticate)
+      controller.stub!(:load_defaults)
+      controller.stub!(:setup_session)
+      controller.stub!(:setup_navigation)
+    end
 
     it 'should set institutions to all institutions if there is no sub service request id' do
       session[:service_request_id] = service_request.id
-      p session
-      get :catalog
-      p session
-      assigns(:institutions).should eq nil # TODO: need sample data
+      get :catalog, { :id => service_request.id }.with_indifferent_access
+      assigns(:institutions).should eq Institution.all
     end
   end
 end

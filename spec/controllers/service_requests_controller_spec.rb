9793946b180bb06a8bab490c488efa4b61202b78
Paul Brannan <curlypaul924@gmail.com>
2012-11-21 13:04:36 -0500

    Removed unnecessary contexts.

--- spec/controllers/service_requests_controller_spec.rb
+++ spec/controllers/service_requests_controller_spec.rb
@@ -1,24 +1,19 @@
 require 'spec_helper'
 
 describe ServiceRequestsController do
-  context 'testing base class' do
+  let!(:service_request) { FactoryGirl.create(:service_request) }
+
+  before(:each) do
+    controller.stub!(:authenticate)
+    controller.stub!(:load_defaults)
+    controller.stub!(:setup_session)
+    controller.stub!(:setup_navigation)
   end
 
-  context 'testing everything else' do
-    let!(:service_request) { FactoryGirl.create(:service_request) }
-
-    before(:each) do
-      controller.stub!(:authenticate)
-      controller.stub!(:load_defaults)
-      controller.stub!(:setup_session)
-      controller.stub!(:setup_navigation)
-    end
-
-    it 'should set institutions to all institutions if there is no sub service request id' do
-      session[:service_request_id] = service_request.id
-      get :catalog, { :id => service_request.id }.with_indifferent_access
-      assigns(:institutions).should eq Institution.all
-    end
+  it 'should set institutions to all institutions if there is no sub service request id' do
+    session[:service_request_id] = service_request.id
+    get :catalog, { :id => service_request.id }.with_indifferent_access
+    assigns(:institutions).should eq Institution.all
   end
 end
 

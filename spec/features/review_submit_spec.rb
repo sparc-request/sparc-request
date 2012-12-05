d275cd4d7057c377f1a50c0dbbb4661ff91b569e
Stuart Johnson <work.stuart.johnson@gmail.com>
2012-12-04 19:33:58 -0500

    adding another test to service review specs

--- spec/features/review_submit_spec.rb
+++ spec/features/review_submit_spec.rb
@@ -7,11 +7,21 @@ describe "review page" do
     add_visits
   end
 
+  describe "clicking save and exit/draft" do
+    it 'Should save request as a draft', :js => true do
+      visit review_service_request_path service_request.id
+      sleep 1
+      find(:xpath, "//a/img[@alt='Wait_save_draft']/..").click
+      sleep 1
+      service_request_test = ServiceRequest.find(service_request.id)
+      service_request_test.status.should eq("draft")
+    end
+  end
   describe "clicking submit" do
     it 'Should submit the page', :js => true do
       visit review_service_request_path service_request.id
       sleep 1
-         find(:xpath, "//a/img[@alt='Confirm_request']/..").click
+      find(:xpath, "//a/img[@alt='Confirm_request']/..").click
       sleep 1
       service_request_test = ServiceRequest.find(service_request.id)
       service_request_test.status.should eq("submitted")

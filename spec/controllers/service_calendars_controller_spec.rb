449d2aa14ada55c6280de0365cbe212d36a96bbe
Paul Brannan <curlypaul924@gmail.com>
2012-12-05 19:00:33 -0500

    More tests for #update

--- spec/controllers/service_calendars_controller_spec.rb
+++ spec/controllers/service_calendars_controller_spec.rb
@@ -96,33 +96,218 @@ describe ServiceCalendarsController do
     end
 
     it 'should set subject count on the line item if on the template tab' do
+      session[:service_request_id] = service_request.id
+
+      get :update, {
+        :format              => :js,
+        :service_request_id  => service_request.id,
+        :line_item           => line_item.id,
+        :qty                 => 240,
+        :tab                 => 'template',
+      }.with_indifferent_access
+
+      line_item.reload
+      line_item.subject_count.should eq 240
     end
 
     it 'should set quantity and research billing quantity on the visit if on the template tab and there is no line item, research billing quantity is 0, and checked is true' do
+      LineItem.any_instance.stub_chain(:service, :displayed_pricing_map, :unit_minimum) { 120 }
+
+      Visit.bulk_create(20, line_item_id: line_item.id)
+      visit = line_item.visits[0]
+      visit.update_attributes(:research_billing_qty => 0)
+
+      session[:service_request_id] = service_request.id
+
+      get :update, {
+        :format              => :js,
+        :service_request_id  => service_request.id,
+        :line_item           => nil,
+        :visit               => line_item.visits[0].id,
+        :tab                 => 'template',
+        :checked             => 'true',
+      }.with_indifferent_access
+
+      visit.reload
+
+      visit.quantity.should eq 120
+      visit.research_billing_qty.should eq 120
     end
 
     it 'should set all the quantities to 0 if on the template tab and there is no line item and checked is false' do
+      Visit.bulk_create(20, line_item_id: line_item.id)
+      visit = line_item.visits[0]
+      visit.update_attributes(:research_billing_qty => 0)
+
+      session[:service_request_id] = service_request.id
+
+      get :update, {
+        :format              => :js,
+        :service_request_id  => service_request.id,
+        :line_item           => nil,
+        :visit               => line_item.visits[0].id,
+        :tab                 => 'template',
+        :checked             => 'false',
+      }.with_indifferent_access
+
+      visit.reload
+
+      visit.quantity.should eq 0
+      visit.research_billing_qty.should eq 0
+      visit.insurance_billing_qty.should eq 0
+      visit.effort_billing_qty.should eq 0
     end
 
     it 'should give an error if on the quantity tab and quantity is less than 0' do
+      Visit.bulk_create(20, line_item_id: line_item.id)
+      visit = line_item.visits[0]
+      visit.update_attributes(:research_billing_qty => 0)
+
+      session[:service_request_id] = service_request.id
+
+      get :update, {
+        :format              => :js,
+        :service_request_id  => service_request.id,
+        :line_item           => nil,
+        :visit               => line_item.visits[0].id,
+        :tab                 => 'quantity',
+        :qty                 => -1,
+      }.with_indifferent_access
+
+      assigns(:errors).should eq "Quantity must be greater than zero"
     end
 
     it 'should update quantity on the visit if on the quantity tab and quantity is 0' do
+      Visit.bulk_create(20, line_item_id: line_item.id)
+      visit = line_item.visits[0]
+      visit.update_attributes(:research_billing_qty => 0)
+
+      session[:service_request_id] = service_request.id
+
+      get :update, {
+        :format              => :js,
+        :service_request_id  => service_request.id,
+        :line_item           => nil,
+        :visit               => line_item.visits[0].id,
+        :tab                 => 'quantity',
+        :qty                 => 0
+      }.with_indifferent_access
+
+      visit.reload
+
+      visit.quantity.should eq 0
     end
 
     it 'should update quantity on the visit if on the quantity tab and quantity is greater than 0' do
+      LineItem.any_instance.stub_chain(:service, :displayed_pricing_map, :unit_minimum) { 120 }
+
+      Visit.bulk_create(20, line_item_id: line_item.id)
+      visit = line_item.visits[0]
+      visit.update_attributes(:research_billing_qty => 0)
+
+      session[:service_request_id] = service_request.id
+
+      get :update, {
+        :format              => :js,
+        :service_request_id  => service_request.id,
+        :line_item           => nil,
+        :visit               => line_item.visits[0].id,
+        :tab                 => 'quantity',
+        :qty                 => 18
+      }.with_indifferent_access
+
+      visit.reload
+
+      visit.quantity.should eq 18
     end
 
     it 'should give an error if on the billing strategy tab and quantity is less than 0' do
+      Visit.bulk_create(20, line_item_id: line_item.id)
+      visit = line_item.visits[0]
+      visit.update_attributes(:research_billing_qty => 0)
+
+      session[:service_request_id] = service_request.id
+
+      get :update, {
+        :format              => :js,
+        :service_request_id  => service_request.id,
+        :line_item           => nil,
+        :visit               => line_item.visits[0].id,
+        :tab                 => 'billing_strategy',
+        :qty                 => -1,
+      }.with_indifferent_access
+
+      assigns(:errors).should eq "Quantity must be greater than zero"
     end
 
     it 'should update the given column on the visit if on the billing strategy tab and quantity is 0' do
+      Visit.bulk_create(20, line_item_id: line_item.id)
+      visit = line_item.visits[0]
+      visit.update_attributes(:research_billing_qty => 0)
+      visit.update_attributes(:effort_billing_qty => 42)
+
+      session[:service_request_id] = service_request.id
+
+      get :update, {
+        :format              => :js,
+        :service_request_id  => service_request.id,
+        :line_item           => nil,
+        :visit               => line_item.visits[0].id,
+        :tab                 => 'billing_strategy',
+        :qty                 => 0,
+        :column              => 'effort_billing_qty',
+      }.with_indifferent_access
+
+      visit.reload
+
+      visit.effort_billing_qty.should eq 0
     end
 
     it 'should update the given column on the visit if on the billing strategy tab and quantity is greater than 0' do
+      Visit.bulk_create(20, line_item_id: line_item.id)
+      visit = line_item.visits[0]
+      visit.update_attributes(:research_billing_qty => 0)
+      visit.update_attributes(:effort_billing_qty => 42)
+
+      session[:service_request_id] = service_request.id
+
+      get :update, {
+        :format              => :js,
+        :service_request_id  => service_request.id,
+        :line_item           => nil,
+        :visit               => line_item.visits[0].id,
+        :tab                 => 'billing_strategy',
+        :qty                 => 100,
+        :column              => 'effort_billing_qty',
+      }.with_indifferent_access
+
+      visit.reload
+
+      visit.effort_billing_qty.should eq 100
     end
 
     it 'should update quantity on the visit to the total if on the billing strategy tab' do
+      Visit.bulk_create(20, line_item_id: line_item.id)
+      visit = line_item.visits[0]
+      visit.update_attributes(:research_billing_qty => 8)
+      visit.update_attributes(:insurance_billing_qty => 17)
+      visit.update_attributes(:effort_billing_qty => 42)
+
+      session[:service_request_id] = service_request.id
+
+      get :update, {
+        :format              => :js,
+        :service_request_id  => service_request.id,
+        :line_item           => nil,
+        :visit               => line_item.visits[0].id,
+        :tab                 => 'billing_strategy',
+        :qty                 => 100,
+        :column              => 'effort_billing_qty',
+      }.with_indifferent_access
+
+      visit.reload
+
+      visit.quantity.should eq(8 + 17 + 100)
     end
 
     it 'should set displayed_visits' do

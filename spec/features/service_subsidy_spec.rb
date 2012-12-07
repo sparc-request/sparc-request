1bc4bff01b5a24aa1020f108d0759113567ba9ed
Stuart Johnson <work.stuart.johnson@gmail.com>
2012-12-07 10:06:08 -0500

    initial subsidy spec

---
+++ spec/features/service_subsidy_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "subsidy page" do
+  build_service_request_with_project
+
+  describe "submitting a filled in form" do
+    it 'Should record the pi contribution', :js => true do
+    end
+  end
+
+end

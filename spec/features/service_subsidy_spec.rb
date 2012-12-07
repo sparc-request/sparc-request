560fb5937a2fc6f12c895e8a88ebe0919a29df25
Stuart Johnson <work.stuart.johnson@gmail.com>
2012-12-07 14:33:59 -0500

    *

--- spec/features/service_subsidy_spec.rb
+++ spec/features/service_subsidy_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 describe "subsidy page" do
   build_service_request_with_project
 
+  before :each do
+    visit service_subsidy_service_request_path service_request.id
+    sleep 1
+    sign_in
+    sleep 1
+  end
+
   describe "submitting a filled in form" do
     it 'Should record the pi contribution', :js => true do
     end

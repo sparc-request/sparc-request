ccff78567ccbd91b886dea48a93340164ab112af
Stuart Johnson <work.stuart.johnson@gmail.com>
2012-12-11 16:26:41 -0500

    subsidy page tests

--- spec/features/service_subsidy_spec.rb
+++ spec/features/service_subsidy_spec.rb
@@ -3,15 +3,27 @@ require 'spec_helper'
 describe "subsidy page" do
   build_service_request_with_project
 
+
   before :each do
+    add_visits
     visit service_subsidy_service_request_path service_request.id
     sleep 1
     sign_in
     sleep 1
   end
 
-  describe "submitting a filled in form" do
-    it 'Should record the pi contribution', :js => true do
+  describe "Subsidy Map is not preset" do
+    it 'Should allow PI Contribution to be set', :js => true do
+      page.should_not have_css("input.pi-contribution[disabled=disabled]")
+    end
+    it 'Should save PI Contribution', :js => true do
+      total = sub_service_request.direct_cost_total
+      max = program.subsidy_map.max_dollar_cap
+      amount = total - max + 5
+      find('.pi-contribution').set(amount)
+      find(:xpath, "//a/img[@alt='Savecontinue']/..").click
+      sleep 2
+      sub_service_request.subsidy.pi_contribution.should eq(Service.dollars_to_cents(amount.to_s))
     end
   end
 

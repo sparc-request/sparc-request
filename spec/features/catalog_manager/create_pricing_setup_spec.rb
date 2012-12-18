8c0c177a8431176dcae35194a0c33e67770b5e4c
bjk7 <brian.kelsey2@gmail.com>
2012-12-18 15:14:50 -0500

    Updated routing information, merged integration tests

---
+++ spec/features/catalog_manager/create_pricing_setup_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe 'as a user on catalog page' do
+  it 'the user should create a pricing map', :js => true do
+    default_catalog_manager_setup
+    
+    click_link("South Carolina Clinical and Translational Institute (SCTR)")
+    click_button("Add Pricing Setup")
+    
+    page.execute_script("$('.ui-accordion-header').click()") 
+    within('.ui-accordion') do
+      find('.display_date').click
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $("a.ui-state-default:contains('15')").trigger("click") } # click on day 15    
+      sleep 1
+      find('.effective_date').click
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $("a.ui-state-default:contains('15')").trigger("click") } # click on day 15    
+      sleep 1
+      
+      find('.federal_percentage_field').set('150')
+      click_link('Apply Federal % to All')
+      page.execute_script %Q{ $(".rate").val("full") }
+      page.execute_script %Q{ $(".rate").change() }
+    end
+  
+    page.execute_script %Q{ $(".save_button").click() }
+    sleep 5
+    page.should have_content "South Carolina Clinical and Translational Institute (SCTR) saved successfully"
+    
+  end
+
+end

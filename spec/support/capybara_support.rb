151056b381ba7681c3f0a9e9392b1becfe3ba373
Paul Brannan <curlypaul924@gmail.com>
2012-12-19 14:10:09 -0500

    Removed unused method

--- spec/support/capybara_support.rb
+++ spec/support/capybara_support.rb
@@ -110,17 +110,6 @@ module CapybaraSupport
 
   end
   
-  def retry_on_timeout(n = 3, &block)
-    block.call
-  rescue Capybara::TimeoutError, Capybara::ElementNotFound => e
-    if n > 0
-      puts "Catched error: #{e.message}. #{n-1} more attempts."
-      retry_on_timeout(n - 1, &block)
-    else
-      raise
-    end
-  end
-      
   def default_catalog_manager_setup
     create_default_data
     login_as(Identity.find_by_ldap_uid('jug2'))

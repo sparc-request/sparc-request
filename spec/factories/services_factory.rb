71fd2736b4fc7df6513fab9bbf6847ecfb53f99a
Nick Bucciarelli <nick.bucciarelli@gmail.com>
2012-12-21 13:40:24 -0500

    Fixing all fo the tests broken.

--- spec/factories/services_factory.rb
+++ spec/factories/services_factory.rb
@@ -10,6 +10,7 @@ FactoryGirl.define do
     service_center_cost { Random.rand(100) }
     charge_code         { Faker::Lorem.words().first }
     revenue_code        { Faker::Lorem.words().first }
+    pricing_maps        [ FactoryGirl.create(:pricing_map) ]
     
     trait :disabled do
       is_available false

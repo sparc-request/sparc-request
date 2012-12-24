bb4f24b37ddb0cf21aa8e3df8240947915d8971a
Russell Niller <rdniller@jackrussellsoftware.com>
2012-12-24 15:30:25 -0500

    created integration test in catalog manager to test prcing_map dates are successfully updated when pricing_setup dates are changed.

--- spec/features/catalog_manager/edit_pricing_setup_spec.rb
+++ spec/features/catalog_manager/edit_pricing_setup_spec.rb
@@ -47,4 +47,44 @@ feature 'edit a pricing setup' do
   ## Need to create a test that will confirm that a dialog pops when changing a date of a pricing_setup that has a related pricing_map.
   ## Need to confirm that changing the pricing_map date to match the pricing_setup works.
 
+  scenario 'updating the date of a pricing_setup will update the date on the pricing_map', :js => true do
+    click_link("Office of Biomedical Informatics")
+    sleep 2
+    
+    page.execute_script("$('.ui-accordion-header').click()") 
+    
+    within('.ui-accordion') do
+      
+      find('.display_date').click
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $("a.ui-state-default:contains('15')").trigger("click") } # click on day 15    
+      wait_for_javascript_to_finish
+      
+      page.execute_script("$('.fix_pricing_maps_button').click()")
+      wait_for_javascript_to_finish
+            
+      find('.effective_date').click
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $("a.ui-state-default:contains('15')").trigger("click") } # click on day 15    
+      wait_for_javascript_to_finish
+      
+      page.execute_script("$('.fix_pricing_maps_button').click()")
+      wait_for_javascript_to_finish      
+    end
+  
+    page.execute_script %Q{ $(".save_button").click() }
+    wait_for_javascript_to_finish
+    
+    page.should have_content "Office of Biomedical Informatics saved successfully"
+    
+    new_date = Date.parse(PricingSetup.last.display_date.to_s)
+    pricing_map_date = Date.parse(PricingMap.last.display_date.to_s)
+    
+    new_date.should eq(pricing_map_date)
+  end
+
 end

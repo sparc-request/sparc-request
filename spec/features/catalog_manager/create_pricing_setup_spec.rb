860672d049c230b3aa096cb4c01aa9fb5ca2b5dc
Russell Niller <rdniller@jackrussellsoftware.com>
2012-12-28 13:34:14 -0500

    created catalog manager integration test for successful creation of a pricing map as a result of a pricing setup being created.

--- spec/features/catalog_manager/create_pricing_setup_spec.rb
+++ spec/features/catalog_manager/create_pricing_setup_spec.rb
@@ -82,5 +82,50 @@ describe 'as a user on catalog page' do
       page.should have_content "Member percentage must be >= to the Federal percentage."
     end
   end
+  
+  it 'should create a pricing map with the same dates as the pricing setup', :js => true, :focus => true do
+    click_link("South Carolina Clinical and Translational Institute (SCTR)")
+    click_button("Add Pricing Setup")
+    
+    page.execute_script("$('.ui-accordion-header').click()") 
+    within('.ui-accordion') do
+      find('.display_date').click
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $("a.ui-state-default:contains('15')").trigger("click") } # click on day 15    
+      wait_for_javascript_to_finish
+
+      find('.effective_date').click
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $('a.ui-datepicker-next').trigger("click") } # move one month forward
+      page.execute_script %Q{ $("a.ui-state-default:contains('15')").trigger("click") } # click on day 15    
+      wait_for_javascript_to_finish
+      
+      find('.federal_percentage_field').set('150')
+      click_link('Apply Federal % to All')
+      page.execute_script %Q{ $(".rate").val("full") }
+      page.execute_script %Q{ $(".rate").change() }
+    end
+  
+    page.execute_script %Q{ $(".save_button").click() }
+    wait_for_javascript_to_finish
+
+    page.should have_content "South Carolina Clinical and Translational Institute (SCTR) saved successfully"
+
+    ## Check to verify pricing map was created.
+    click_link("South Carolina Clinical and Translational Institute (SCTR)")
+    click_link("MUSC Research Data Request (CDW)")
+    
+    increase_decrease_date = (Date.today + 2.month).strftime("%-m/15/%Y")
+    page.should have_content("Effective on #{increase_decrease_date} - Display on #{increase_decrease_date}")
+
+    ## Ensure pricing map copied over the content from the existing pricing map
+    page.execute_script("$('.ui-accordion-header:last').click()")
+    
+    within('.pricing_map_form:nth-of-type(4)') do
+      find("input[id$='full_rate']").value.should eq '45.00'
+      find("input[id$='unit_type']").value.should eq 'self'
+    end
+  end
 
 end

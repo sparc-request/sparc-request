501d32cf89a83412843323799496344060ef4030
Paul Brannan <curlypaul924@gmail.com>
2012-12-31 15:12:24 -0500

    Added more tests for VisitsController

--- spec/controllers/portal/visits_controller_spec.rb
+++ spec/controllers/portal/visits_controller_spec.rb
@@ -37,36 +37,108 @@ describe Portal::VisitsController do
         sub_service_request_id: ssr.id)
   }
 
-  let!(:line_item) {
-    FactoryGirl.create(
-        :line_item,
-        service_id: service.id,
-        service_request_id: service_request.id,
-        sub_service_request_id: ssr.id)
-  }
-
-  let!(:visit) {
-    FactoryGirl.create(
-        :visit,
-        line_item_id:
-        line_item.id, research_billing_qty: 5)
-  }
-
   describe 'POST update_from_fulfillment' do
     # TODO
   end
 
   describe 'destroy' do
-    it 'should set instance variables' do
-      post :destroy, {
-        format: :js,
-        id: visit.id,
-      }.with_indifferent_access
-      assigns(:visit).should eq visit
-      assigns(:sub_service_request).should eq ssr
-      assigns(:service_request).should eq service_request
-      assigns(:subsidy).should eq subsidy
-      assigns(:candidate_per_patient_per_visit).should eq [ service ]
+    context 'we have one line item' do
+      let!(:line_item) {
+        FactoryGirl.create(
+            :line_item,
+            service_id: service.id,
+            service_request_id: service_request.id,
+            sub_service_request_id: ssr.id)
+      }
+
+      let!(:visit) {
+        FactoryGirl.create(
+            :visit,
+            line_item_id:
+            line_item.id, research_billing_qty: 5)
+      }
+
+      it 'should set instance variables' do
+        post :destroy, {
+          format: :js,
+          id: visit.id,
+        }.with_indifferent_access
+        assigns(:visit).should eq visit
+        assigns(:sub_service_request).should eq ssr
+        assigns(:service_request).should eq service_request
+        assigns(:subsidy).should eq subsidy
+        assigns(:candidate_per_patient_per_visit).should eq [ service ]
+      end
+
+      it 'should destroy the visit' do
+        post :destroy, {
+          format: :js,
+          id: visit.id,
+        }.with_indifferent_access
+        expect { visit.reload }.to raise_exception(ActiveRecord::RecordNotFound)
+      end
+
+      it 'should fix the pi contribution on the subsidy' do
+        Subsidy.any_instance.stub(:fix_pi_contribution) {
+          subsidy.update_attributes(pi_contribution: 12)
+        }
+
+        post :destroy, {
+          format: :js,
+          id: visit.id,
+        }.with_indifferent_access
+        subsidy.reload
+        subsidy.pi_contribution.should eq 12
+      end
+    end
+
+    context 'we have multiple line items' do
+      let!(:line_item1) {
+        FactoryGirl.create(
+            :line_item,
+            service_id: service.id,
+            service_request_id: service_request.id,
+            sub_service_request_id: ssr.id)
+      }
+
+      let!(:line_item2) {
+        FactoryGirl.create(
+            :line_item,
+            service_id: service.id,
+            service_request_id: service_request.id,
+            sub_service_request_id: ssr.id)
+      }
+
+      let!(:line_item3) {
+        FactoryGirl.create(
+            :line_item,
+            service_id: service.id,
+            service_request_id: service_request.id,
+            sub_service_request_id: ssr.id)
+      }
+
+      it 'should destroy all the other visits at the same position' do
+        line_item1_visits = Visit.bulk_create(10, line_item_id: line_item1.id)
+        line_item2_visits = Visit.bulk_create(10, line_item_id: line_item2.id)
+        line_item3_visits = Visit.bulk_create(10, line_item_id: line_item3.id)
+
+        post :destroy, {
+          format: :js,
+          id: line_item1_visits[4].id,
+        }.with_indifferent_access
+
+        expect { line_item1_visits[4].reload }.to raise_exception(ActiveRecord::RecordNotFound)
+        expect { line_item2_visits[4].reload }.to raise_exception(ActiveRecord::RecordNotFound)
+        expect { line_item3_visits[4].reload }.to raise_exception(ActiveRecord::RecordNotFound)
+
+        line_item1.reload
+        line_item2.reload
+        line_item3.reload
+
+        line_item1.visits.count.should eq 9
+        line_item2.visits.count.should eq 9
+        line_item3.visits.count.should eq 9
+      end
     end
   end
 end

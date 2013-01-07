f6740c109999cf43308e303e8a034858f84565d3
Paul Brannan <curlypaul924@gmail.com>
2013-01-07 14:05:58 -0500

    Slightly better solution for serializing access to the database.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,20 +45,6 @@ end
 load_schema()
 FactoryGirl.find_definitions
 
-class ActiveRecord::Base
-  mattr_accessor :shared_connection
-  @@shared_connection = nil
-
-
-  def self.connection
-    @@shared_connection || retrieve_connection
-  end
-end
-
-# Forces all threads to share the same connection. This works on
-# Capybara because it starts the web server in a thread.
-ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
-
 RSpec.configure do |config|
 
   config.before(:suite) do

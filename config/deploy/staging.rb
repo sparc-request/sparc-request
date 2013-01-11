87de885b4a6f32b928eda31c7969188c195753f5
Andrew Cates <amcates@gmail.com>
2013-01-11 11:14:13 -0500

    fixing deploys for correct branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :deploy_to, "#{deploy_root}/#{application}"
 set :rails_env, "staging"
 set :domain, "obis-sparc-stg.mdc.musc.edu"
-set :branch, "application_merge"
+set :branch, "staging"
 
 role :web, domain
 role :app, domain, :primary => true
--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,7 +1,7 @@
 set :deploy_to, "#{deploy_root}/#{application}"
 set :rails_env, "testing"
 set :domain, "obis-sparc-dev.mdc.musc.edu"
-set :branch, "application_merge"
+set :branch, "master"
 
 role :web, domain
 role :app, domain, :primary => true

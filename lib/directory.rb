162ce8ee83cf3b4e4458584ac98e60f2c5e532bf
Paul Brannan <curlypaul924@gmail.com>
2013-01-14 18:24:43 -0500

    Added more tests for the Directory.

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,12 +1,12 @@
 require 'net/ldap'
-require 'ostruct'
 
 class Directory
   # TODO: needs to use config/application.yml for ldap config
-  LDAP_HOST = 'authldap.musc.edu'
-  LDAP_PORT = 636
-  LDAP_BASE = 'ou=people,dc=musc.edu,dc=edu'
+  LDAP_HOST       = 'authldap.musc.edu'
+  LDAP_PORT       = 636
+  LDAP_BASE       = 'ou=people,dc=musc.edu,dc=edu'
   LDAP_ENCRYPTION = :simple_tls
+  DOMAIN          = 'musc.edu'
 
   # Searches LDAP and the database for a given search string (can be
   # ldap_uid, last_name, first_name, email).  If an identity is found in
@@ -15,12 +15,10 @@ class Directory
   def self.search(term)
     # Search ldap and the database
     ldap_results = search_ldap(term)
-    db_results = search_database(term)
 
     # If there are any entries returned from ldap that were not in the
     # database, then create them
-    missing = ldap_entries_not_in_database(ldap_results, db_results)
-    create_identities(missing)
+    create_or_update_database_from_ldap(ldap_results)
 
     # Finally, search the database a second time and return the results.
     # If there were no new identities created, then this should return
@@ -66,53 +64,76 @@ class Directory
     return res
   end
 
-  # Find LDAP entries that are not in the database.  Returns an array of
-  # objects.
-  def self.ldap_entries_not_in_database(ldap_results, db_results)
-    if ldap_results.nil? # no results from ldap
-      ldap_map = []
-    else
-      ldap_map = res.map { |r|
-        {
-          first_name: r.givenname.first.downcase,
-          last_name:  r.sn.first.downcase,
-          email:      r.mail.first.downcase,
-          uid:        r.uid.first.downcase,
-        }
-      }
-    end
-
-    db_map = db_results.map { |i|
-      {
-        first_name: i.first_name.downcase,
-        last_name:  i.last_name.downcase,
-        email:      i.email.downcase,
-        uid:        i.ldap_uid.downcase,
-      }
+  def ldap_to_common_form(r)
+    {
+      first_name: r.givenname.first.downcase,
+      last_name:  r.sn.first.downcase,
+      email:      r.mail.first.downcase,
+      uid:        "#{r.uid.first.downcase}@#{DOMAIN}",
     }
+  end
 
-    difference = ldap_map - db_map
-    return difference.map { |r| OpenStruct.new(r) }
+  def db_to_common_form(i)
+    {
+      first_name: i.first_name.downcase,
+      last_name:  i.last_name.downcase,
+      email:      i.email.downcase,
+      uid:        i.ldap_uid.downcase,
+    }
   end
 
   # Create a new identity for from each element of the given array.
   # Expects the entry to be an object as would be returned from
   # ldap_entries_not_in_database().
-  def self.create_identities(missing)
+  def self.create_or_update_database_from_ldap(ldap_results)
     # Any users that are in the LDAP results but not the database results, should have
     # a database entry created for them.
-    missing.each do |new_identity|
-      # TODO: needs to use config/application.yml for host config
+    ldap_results.each do |r|
       # since we auto create we need to set a random password and auto confirm the addition so that the user has immediate access
       begin
-        Identity.create!(
-            first_name: new_identity.first_name.capitalize,
-            last_name:  new_identity.last_name.capitalize,
-            email:      new_identity.email,
-            ldap_uid:   new_identity.uid,
-            password:   Devise.friendly_token[0,20],
-            approved:   true)
+        uid         = "#{r.uid.first.downcase}@#{DOMAIN}"
+        email       = r.mail.first
+        first_name  = r.givenname.first
+        last_name   = r.sn.first
+
+        # TODO: This might turn out to be too slow.  Will it need to be
+        # optimized?  In particular, if there are a lot of results from
+        # ldap, then this will result in a lot of queries to the
+        # database.
+
+        # Check to see if the identity is already in the database
+        if (identity = Identity.find_by_ldap_uid(uid)) then
+
+          # Do we need to update any of the fields?  Has someone's last
+          # name changed due to getting married, etc.?
+          if identity.email != email or
+             identity.last_name != last_name or
+             identity.first_name != first_name then
+
+            identity.update_attributes!(
+                email: email,
+                first_name: first_name,
+                last_name: last_name)
+          end
+
+        # if it is not, then added it
+        else
+
+          # TODO: using capitalize will not work for McHenry, e.g.
+          Identity.create!(
+              first_name: first_name,
+              last_name:  last_name,
+              email:      email,
+              ldap_uid:   uid,
+              password:   Devise.friendly_token[0,20],
+              approved:   true)
+
+        end
+
       rescue ActiveRecord::ActiveRecordError => e
+        # TODO: rescuing this exception means that an email will not get
+        # sent.  This may or may not be the behavior that we want, but
+        # it is the existing behavior.
         Rails.logger.info '#'*100
         Rails.logger.info "#{e.message} (#{e.class})"
         Rails.logger.info e.backtrace.first(20)

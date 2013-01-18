d49ee0d9fae27f58a1fcf49b0e6bf6dcf51788ee
Jason Leonard <jleonardw9@gmail.com>
2013-01-18 15:18:41 -0500

    Completed tests for editing a project in admin portal

---
+++ spec/features/portal/admin/admin_edit_project_spec.rb
@@ -0,0 +1,105 @@
+require 'spec_helper'
+
+describe "editing a project", js: true do
+  let_there_be_lane
+  fake_login_for_each_test
+  build_service_request_with_project()
+ 
+  before :each do
+    visit portal_admin_sub_service_request_path sub_service_request.id
+    click_on("Project/Study Information")
+  end
+
+  context "validations" do
+
+    it "should raise an error message if study's status is pending and no potential funding source is selected" do
+      select("Pending Funding", from: "Proposal Funding Status")
+      select("Select a Potential Funding Source", from: "Potential Funding Source")
+      click_button "Save project"
+      page.should have_content("1 error prohibited this project from being saved")
+    end
+
+    it "should raise an error message if study's status is funded but no funding source is selected" do
+      select("Funded", from: "Proposal Funding Status")
+      select("Select a Funding Source", from: "project_funding_source")
+      click_button "Save project"
+      page.should have_content("1 error prohibited this project from being saved")
+    end
+  end
+
+  context "clicking cancel button" do
+
+    it "should not save changes" do
+      fill_in "project_short_title", with: "Jason"
+      find(".admin_cancel_link").click()    
+      find("#project_short_title").should_not eq("Jason")
+    end
+  end
+
+  context "editing the short title" do
+
+    it "should save the new short title" do
+      fill_in "project_short_title", with: "Julius"
+      click_button "Save project"
+      find("#project_short_title").value().should eq("Julius")
+    end
+  end
+
+  context "editing the project title" do
+
+    it "should save the new project title" do
+      fill_in "project_title", with: "Swanson"
+      click_button "Save project"
+      find("#project_title").value().should eq("Swanson")
+    end
+  end
+
+  context "selecting a status of funded" do
+
+    it "should cause the field 'funding source' to be visible" do
+      select("Funded", from: "Proposal Funding Status")
+      find("#project_funding_source").should be_visible
+    end
+  end
+
+  context "selecting a status of pending" do
+
+    it "should cause the field 'potential funding source' to be visible" do
+      select("Pending Funding", from: "Proposal Funding Status")
+      find("#project_potential_funding_source").should be_visible
+    end
+  end
+
+  context "selecting a funding/pending funding source" do
+
+    it "should save the new funding source" do
+      select("Funded", from: "Proposal Funding Status")
+      select("Federal", from: "project_funding_source")
+      find("#project_funding_source").value().should eq("federal")
+    end
+
+    it "should save the new pending funding source" do
+      select("Pending Funding", from: "Proposal Funding Status")
+      select("Federal", from: "Potential Funding Source")
+      find("#project_potential_funding_source").value().should eq("federal")
+    end
+  end
+
+  context "editing the brief description" do
+
+    it "should save the brief description" do
+      fill_in "project_brief_description", with: "This is an amazing description."
+      click_button "Save project"
+      find("#project_brief_description").value().should eq("This is an amazing description.")
+    end
+  end
+
+  context "editing the indirect cost rate" do
+
+    it "should save the indirect cost rate" do
+      fill_in "project_indirect_cost_rate", with: "50.0"
+      click_button "Save project"
+      find("#project_indirect_cost_rate").value().should eq("50.0")
+    end
+  end
+end

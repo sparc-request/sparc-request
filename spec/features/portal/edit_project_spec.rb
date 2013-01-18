4d5b5a0e348ede5a232a57bb3aebf0256b959da5
Jason Leonard <jleonardw9@gmail.com>
2013-01-18 12:03:38 -0500

    Completed testing the edit project page in portal

--- spec/features/portal/edit_project_spec.rb
+++ spec/features/portal/edit_project_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "editing a study", js: true do
+describe "editing a project", js: true do
   let_there_be_lane
   fake_login_for_each_test
   build_service_request_with_project()
@@ -23,6 +23,85 @@ describe "editing a study", js: true do
       select("Select a Funding Source", from: "project_funding_source")
       click_button "Save project"
       page.should have_content("1 error prohibited this project from being saved")
+    end
+  end
+
+  context "cancel button" do
+
+    it "should redirect back to the main portal page" do
+      click_on "Cancel"
+      page.should have_content("Welcome!")
+    end
+  end
+
+  context "editing the short title" do
+
+    it "should save the new short title" do
+      fill_in "project_short_title", with: "Julius"
+      click_button "Save project"
+      visit edit_portal_protocol_path service_request.protocol.id
+      find("#project_short_title").value().should eq("Julius")
+    end
+  end
+
+  context "editing the project title" do
+
+    it "should save the new project title" do
+      fill_in "project_title", with: "Swanson"
+      click_button "Save project"
+      visit edit_portal_protocol_path service_request.protocol.id
+      find("#project_title").value().should eq("Swanson")
+    end
+  end
+
+  context "selecting a status of funded" do
+
+    it "should cause the field 'funding source' to be visible" do
+      select("Funded", from: "Proposal Funding Status")
+      find("#project_funding_source").should be_visible
+    end
+  end
+
+  context "selecting a status of pending" do
+
+    it "should cause the field 'potential funding source' to be visible" do
+      select("Pending Funding", from: "Proposal Funding Status")
+      find("#project_potential_funding_source").should be_visible
+    end
+  end
+
+  context "selecting a funding/pending funding source" do
+
+    it "should save the new funding source" do
+      select("Funded", from: "Proposal Funding Status")
+      select("Federal", from: "project_funding_source")
+      find("#project_funding_source").value().should eq("federal")
+    end
+
+    it "should save the new pending funding source" do
+      select("Pending Funding", from: "Proposal Funding Status")
+      select("Federal", from: "Potential Funding Source")
+      find("#project_potential_funding_source").value().should eq("federal")
+    end
+  end
+
+  context "editing the brief description" do
+
+    it "should save the brief description" do
+      fill_in "project_brief_description", with: "This is an amazing description."
+      click_button "Save project"
+      visit edit_portal_protocol_path service_request.protocol.id
+      find("#project_brief_description").value().should eq("This is an amazing description.")
+    end
+  end
+
+  context "editing the indirect cost rate" do
+
+    it "should save the indirect cost rate" do
+      fill_in "project_indirect_cost_rate", with: "50.0"
+      click_button "Save project"
+      visit edit_portal_protocol_path service_request.protocol.id
+      find("#project_indirect_cost_rate").value().should eq("50.0")
     end
   end
 end

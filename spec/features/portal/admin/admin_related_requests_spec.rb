8519fd692f40cf1bb26c0b310a1a93b4794deeb2
Stuart Johnson <work.stuart.johnson@gmail.com>
2013-01-25 14:19:16 -0500

    admin portal related service requests specs

---
+++ spec/features/portal/admin/admin_related_requests_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "admin related service requests tab", :js => true do
+  let_there_be_lane
+  fake_login_for_each_test
+  build_service_request_with_project
+
+  before :each do
+    add_visits
+    visit portal_admin_sub_service_request_path sub_service_request.id
+    find("a.related_service_requests-tab").click
+    wait_for_javascript_to_finish
+  end
+
+  it "should list the service request" do
+    within "div#related_service_requests" do
+      find("#requests tr td:first").should have_content("#{sub_service_request.id}")
+    end
+  end
+
+  it "should show the first service when the plus link is clicked" do
+    find("div#requests td a.rsr-link").click
+    wait_for_javascript_to_finish
+    find("div#ssr_#{sub_service_request.id}").should have_content("#{sub_service_request.line_items.first.service.name}")
+  end
+
+  it "should also show the second service when the plus link is clicked" do
+    find("div#requests td a.rsr-link").click
+    wait_for_javascript_to_finish
+    find("div#ssr_#{sub_service_request.id}").should have_content("#{sub_service_request.line_items.last.service.name}")
+  end
+
+end

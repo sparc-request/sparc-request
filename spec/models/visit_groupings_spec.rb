9f0e6c3b74f1699298fd6d738ba4e1978f424c6b
Paul Brannan <curlypaul924@gmail.com>
2013-03-06 12:50:22 -0500

    Added some expectations

--- spec/models/visit_groupings_spec.rb
+++ spec/models/visit_groupings_spec.rb
@@ -3,5 +3,8 @@ require 'spec_helper'
 describe VisitGrouping do
   it 'should be possible to create a visit grouping' do
     visit_grouping = VisitGrouping.create!()
+    visit_grouping.arm.should eq nil
+    visit_grouping.line_item.should eq nil
+    visit_grouping.visits.should eq [ ]
   end
 end

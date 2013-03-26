cf69772ac066389f2b2a1db032ee54f5c06e599b
bjk7 <brian.kelsey2@gmail.com>
2013-03-26 14:44:40 -0400

    Added feedback link to sparc banner, feedback message and email are stored and sent to appropriate users

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -94,4 +94,13 @@ class Notifier < ActionMailer::Base
   def obtain_research_pricing service_provider, service_request
 
   end
+
+  def provide_feedback feedback
+    @feedback = feedback
+
+    email_to = Rails.env == 'production' ? ADMIN_MAIL_TO : DEFAULT_MAIL_TO
+    email_from = @feedback.email.blank? ? DEFAULT_MAIL_TO : @feedback.email
+
+    mail(:to => email_to, :from => email_from, :subject => "Feedback")
+  end
 end

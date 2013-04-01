37c86fa22123544bad2b4335a94423d75bf7b271
Jason Leonard <jleonardw9@gmail.com>
2013-04-01 16:28:12 -0400

    Adding specs for available statuses in catalog manager.

--- spec/features/catalog_manager/edit_institution_spec.rb
+++ spec/features/catalog_manager/edit_institution_spec.rb
@@ -1,21 +1,37 @@
 require 'spec_helper'
 
-feature 'edit an institution', :js => true do
-  scenario 'successfully update an existing institution' do
-    default_catalog_manager_setup
+describe 'edit an institution', :js => true do
 
+  before :each do
+    default_catalog_manager_setup
     click_link('Medical University of South Carolina')
-    
-    # General Information fields
-    fill_in 'institution_abbreviation', :with => 'GreatestInstitution'
-    fill_in 'institution_description', :with => 'Description'
-    fill_in 'institution_ack_language', :with => 'Language'
-    fill_in 'institution_order', :with => '1'
-    select('blue', :from => 'institution_css_class')
-    uncheck('institution_is_available')
-    
-    page.execute_script("$('#save_button').click();")
-    page.should have_content( 'Medical University of South Carolina saved successfully' )
   end
-  
+
+  context 'successfully update an existing institution' do
+    
+    it "should sucessfully edit and save the institution" do
+      
+      # General Information fields
+      fill_in 'institution_abbreviation', :with => 'GreatestInstitution'
+      fill_in 'institution_description', :with => 'Description'
+      fill_in 'institution_ack_language', :with => 'Language'
+      fill_in 'institution_order', :with => '1'
+      select('blue', :from => 'institution_css_class')
+      uncheck('institution_is_available')
+      
+      page.execute_script("$('#save_button').click();")
+      page.should have_content( 'Medical University of South Carolina saved successfully' )
+    end
+
+    context "editing status options" do
+
+      it "should only get the statuses that are checked" do
+        institution = Organization.where(abbreviation: "MUSC").first
+        find("#institution_available_statuses_attributes_0__destroy").click
+        first("#save_button").click
+        wait_for_javascript_to_finish
+        institution.get_available_statuses.should eq( {"draft" => "Draft"} )
+      end
+    end
+  end
 end

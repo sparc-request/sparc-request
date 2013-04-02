512f6702fb76c7978481c32631f86c740d7d996b
Jason Leonard <jleonardw9@gmail.com>
2013-04-02 16:18:55 -0400

    Tests added for organization's available statuses.

--- spec/features/catalog_manager/edit_core_spec.rb
+++ spec/features/catalog_manager/edit_core_spec.rb
@@ -1,21 +1,53 @@
 require 'spec_helper'
 
-feature 'edit a core', :js => true do
-  scenario 'successfully update an existing core' do
+describe 'edit a core', :js => true do
+
+  before :each do
     default_catalog_manager_setup
-
     click_link('Clinical Data Warehouse')
-    
-    # General Information fields
-    fill_in 'core_abbreviation', :with => 'PTP'
-    fill_in 'core_order', :with => '2'
-    fill_in 'core_description', :with => 'Description'
-    # Subsidy Information fields
-    fill_in 'core_subsidy_map_attributes_max_percentage', :with => '55.5'
-    fill_in 'core_subsidy_map_attributes_max_dollar_cap', :with => '65'
-
-    page.execute_script("$('#save_button').click();")
-    page.should have_content( 'Clinical Data Warehouse' )
   end
-  
+
+  context 'successfully update an existing core' do
+   
+    it "should successfully edit and save the core" do  
+      # General Information fields
+      fill_in 'core_abbreviation', :with => 'PTP'
+      fill_in 'core_order', :with => '2'
+      fill_in 'core_description', :with => 'Description'
+      # Subsidy Information fields
+      fill_in 'core_subsidy_map_attributes_max_percentage', :with => '55.5'
+      fill_in 'core_subsidy_map_attributes_max_dollar_cap', :with => '65'
+
+      page.execute_script("$('#save_button').click();")
+      page.should have_content( 'Clinical Data Warehouse' )
+    end
+
+    context "editing status options" do
+
+      before :each do
+        @core = Organization.where(abbreviation: "Clinical Data Warehouse").first
+        wait_for_javascript_to_finish
+      end
+
+      it "should get the default statuses" do
+        @core.get_available_statuses.should eq( {"draft" => "Draft", "submitted" => "Submitted", "obtain_research_pricing" => "Obtain Research Pricing", "in_process" => "In Process", "complete" => "Complete", "awaiting_pi_approval" => "Awaiting PI Approval", "on_hold" => "On Hold"} )
+      end
+
+      it "should only get the statuses that are checked" do
+        find("#core_available_statuses_attributes_0__destroy").click
+        first("#save_button").click
+        wait_for_javascript_to_finish
+        @core.get_available_statuses.should eq( {"draft" => "Draft"} )
+      end
+
+      it "should not create duplicates if saved twice" do
+        find("#core_available_statuses_attributes_0__destroy").click
+        first("#save_button").click
+        wait_for_javascript_to_finish
+        first("#save_button").click
+        wait_for_javascript_to_finish
+        @core.get_available_statuses.should eq( {"draft" => "Draft"} )
+      end
+    end
+  end
 end
--- spec/features/catalog_manager/edit_institution_spec.rb
+++ spec/features/catalog_manager/edit_institution_spec.rb
@@ -9,7 +9,7 @@ describe 'edit an institution', :js => true do
 
   context 'successfully update an existing institution' do
     
-    it "should sucessfully edit and save the institution" do
+    it "should successfully edit and save the institution" do
       
       # General Information fields
       fill_in 'institution_abbreviation', :with => 'GreatestInstitution'
@@ -19,18 +19,35 @@ describe 'edit an institution', :js => true do
       select('blue', :from => 'institution_css_class')
       uncheck('institution_is_available')
       
-      page.execute_script("$('#save_button').click();")
+      first("#save_button").click
       page.should have_content( 'Medical University of South Carolina saved successfully' )
     end
 
     context "editing status options" do
 
+      before :each do
+        @institution = Organization.where(abbreviation: "MUSC").first
+        wait_for_javascript_to_finish
+      end
+
+      it "should get the default statuses" do
+        @institution.get_available_statuses.should eq( {"draft" => "Draft", "submitted" => "Submitted", "obtain_research_pricing" => "Obtain Research Pricing", "in_process" => "In Process", "complete" => "Complete", "awaiting_pi_approval" => "Awaiting PI Approval", "on_hold" => "On Hold"} )
+      end
+
       it "should only get the statuses that are checked" do
-        institution = Organization.where(abbreviation: "MUSC").first
         find("#institution_available_statuses_attributes_0__destroy").click
         first("#save_button").click
         wait_for_javascript_to_finish
-        institution.get_available_statuses.should eq( {"draft" => "Draft"} )
+        @institution.get_available_statuses.should eq( {"draft" => "Draft"} )
+      end
+
+      it "should not create duplicates if saved twice" do
+        find("#institution_available_statuses_attributes_0__destroy").click
+        first("#save_button").click
+        wait_for_javascript_to_finish
+        first("#save_button").click
+        wait_for_javascript_to_finish
+        @institution.get_available_statuses.should eq( {"draft" => "Draft"} )
       end
     end
   end

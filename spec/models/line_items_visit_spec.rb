862fb40a56963979ed95375a8ba1bec46e9d4e28
Jason Leonard <jleonardw9@gmail.com>
2013-04-22 12:09:39 -0400

    Refactored line item and line items visit specs to use fixtures.

--- spec/models/line_items_visit_spec.rb
+++ spec/models/line_items_visit_spec.rb
@@ -2,35 +2,30 @@ require 'spec_helper'
 
 describe LineItemsVisit do
 
-  it 'should be possible to create a visit grouping' do
+  it 'should be possible to create a line items visit' do
     line_items_visit = LineItemsVisit.create!()
     line_items_visit.arm.should eq nil
     line_items_visit.line_item.should eq nil
     line_items_visit.visits.should eq [ ]
   end
 
+  let_there_be_lane
+  let_there_be_j
+  build_service_request_with_study
+
   describe "methods" do
 
-    let!(:program)       { FactoryGirl.create(:program) }
-    let!(:pricing_setup) { FactoryGirl.create(:pricing_setup, organization_id: program.id) }
-
     before :each do
-
-      @study = FactoryGirl.build(:study, :funded, :federal, indirect_cost_rate: 200)
-      @study.save(validate: false)
+      service_request.protocol.update_attributes(indirect_cost_rate: 200)
+      add_visits
+      @line_items_visit = arm1.line_items_visits.first
     end
 
     context "business methods" do
-
-      let!(:service_request) { FactoryGirl.create(:service_request, protocol_id: @study.id) }
-      let!(:ssr)             { FactoryGirl.create(:sub_service_request, service_request_id: service_request.id, organization_id: program.id) }
+      
       let!(:service)         { FactoryGirl.create(:service, organization_id: program.id)}
       let!(:pricing_map)     { FactoryGirl.create(:pricing_map, service_id: service.id, display_date: Date.today) }
       let!(:pricing_map2)    { FactoryGirl.create(:pricing_map, service_id: service.id, display_date: Date.today + 1) }
-      let!(:line_item)       { FactoryGirl.create(:line_item, service_request_id: service_request.id, sub_service_request_id: ssr.id, service_id: service.id, quantity: 20)} 
-      let!(:arm)             { FactoryGirl.create(:arm, service_request_id: service_request.id) }
-      let!(:line_items_visit)  { FactoryGirl.create(:line_items_visit, arm_id: arm.id, line_item_id: line_item.id, subject_count: 5) }   
-      let!(:visit)           { FactoryGirl.create(:visit, line_items_visit_id: line_items_visit.id, research_billing_qty: 5)}
 
       describe "per unit cost" do  
 
@@ -39,17 +34,17 @@ describe LineItemsVisit do
         end
 
         it "should return the per unit cost for full quantity with no arguments" do
-          line_items_visit.per_unit_cost.should eq(100)
+          @line_items_visit.per_unit_cost.should eq(50)
         end
 
         it "should return 0 if the quantity is 0" do
-          line_items_visit.line_item.quantity = 0
-          line_items_visit.per_unit_cost.should eq(0)
+          @line_items_visit.line_item.quantity = 0
+          @line_items_visit.per_unit_cost.should eq(0)
         end
 
         it "should return the per unit cost for a specific quantity from arguments" do
-          line_items_visit1 = line_items_visit.dup
-          line_items_visit2 = line_items_visit.dup
+          line_items_visit1 = @line_items_visit.dup
+          line_items_visit2 = @line_items_visit.dup
           line_items_visit1.line_item.quantity = 5
           line_items_visit1.per_unit_cost.should eq(line_items_visit2.per_unit_cost(5))
         end
@@ -60,50 +55,66 @@ describe LineItemsVisit do
         it "should select the correct pricing map based on display date" do
           pricing_map.update_attributes(unit_factor: 5)
           pricing_map2.update_attributes(unit_factor: 10)
-          line_items_visit.units_per_package.should eq(5)
+          @line_items_visit.units_per_package.should eq(5)
         end
       end
 
       describe "quantity total" do
 
         it "should return the correct quantity" do
-          line_items_visit.quantity_total.should eq(25)
+          @line_items_visit.quantity_total.should eq(100)
         end
 
         it "should return zero if the research billing qantity is zero" do
-          visit.update_attributes(research_billing_qty: 0)
-          line_items_visit.quantity_total.should eq(0)
+          @line_items_visit.visits.each do |visit|
+            visit.update_attributes(research_billing_qty: 0)
+          end
+
+          @line_items_visit.quantity_total.should eq(0)
         end
       end
 
       describe "per subject subtotals" do
 
         it "should return the correct cost of each visit" do
-          line_items_visit.per_subject_subtotals.should eq( {visit.id.to_s => 500.0} )
+          costs = Hash.new
+          @line_items_visit.visits.each do |visit|
+            costs[visit.id.to_s] = 250
+          end
+
+          @line_items_visit.per_subject_subtotals.should eq(costs)
         end
 
         it "should return nil if the visit has no research billing" do
-          visit.update_attributes(research_billing_qty: 0)
-          line_items_visit.per_subject_subtotals.should eq( {visit.id.to_s => nil} )
+          research_billing = Hash.new
+          @line_items_visit.visits.each do |visit|
+            visit.update_attributes(research_billing_qty: 0)
+            research_billing[visit.id.to_s] = nil
+          end
+
+          @line_items_visit.per_subject_subtotals.should eq(research_billing)
         end        
       end
 
       describe "direct costs for visit based service single subject" do
 
         it "should return the correct cost for one subject" do
-          line_items_visit.direct_costs_for_visit_based_service_single_subject.should eq(500)
+          @line_items_visit.direct_costs_for_visit_based_service_single_subject.should eq(2500)
         end 
 
         it "should return zero if the research billing quantity is zero" do
-          visit.update_attributes(research_billing_qty: 0)
-          line_items_visit.direct_costs_for_visit_based_service_single_subject.should eq(0)
+          @line_items_visit.visits.each do |visit|
+            visit.update_attributes(research_billing_qty: 0)
+          end
+
+          @line_items_visit.direct_costs_for_visit_based_service_single_subject.should eq(0)
         end
       end
 
       describe "direct costs for visit based services" do
 
         it "should return the correct cost for all subjects" do
-          line_items_visit.direct_costs_for_visit_based_service.should eq(2500)
+          @line_items_visit.direct_costs_for_visit_based_service.should eq(5000)
         end
       end
 
@@ -111,7 +122,7 @@ describe LineItemsVisit do
 
         it "should return the correct direct cost" do
           pricing_map.update_attributes(is_one_time_fee: true)
-          line_items_visit.direct_costs_for_one_time_fee.should eq(2000)
+          @line_items_visit.direct_costs_for_one_time_fee.should eq(250)
         end
       end
 
@@ -124,28 +135,28 @@ describe LineItemsVisit do
         context "indirect cost rate" do
 
           it "should determine the indirect cost rate" do
-            line_items_visit.indirect_cost_rate.should eq(2)
+            @line_items_visit.indirect_cost_rate.should eq(2)
           end
         end
 
         context "indirect costs for visit based service single subject" do
 
           it "should return the correct indirect cost" do
-            line_items_visit.indirect_costs_for_visit_based_service_single_subject.should eq(1000)
+            @line_items_visit.indirect_costs_for_visit_based_service_single_subject.should eq(5000)
           end
         end
 
         context "indirect costs for visit based service" do
 
           it "should return the correct cost" do
-            line_items_visit.indirect_costs_for_visit_based_service.should eq(5000)
+            @line_items_visit.indirect_costs_for_visit_based_service.should eq(10000)
           end
         end
 
         context "indirect costs for one time fee" do
 
           it "should return the correct cost" do
-            line_items_visit.indirect_costs_for_one_time_fee.should eq(4000)
+            @line_items_visit.indirect_costs_for_one_time_fee.should eq(500)
           end
         end
       end
@@ -153,19 +164,19 @@ describe LineItemsVisit do
       describe "add visit" do
 
         it "should add a visit" do
-          vg = arm.visit_groups.create(position: nil)
-          line_items_visit.add_visit(vg)
-          line_items_visit.visits.count.should eq(2)
+          vg = arm1.visit_groups.create(position: nil)
+          @line_items_visit.add_visit(vg)
+          @line_items_visit.visits.count.should eq(11)
         end
       end
 
       describe "remove visit" do
 
         it "should delete a visit" do
-          vg = arm.visit_groups.create(position: nil)
-          line_items_visit.add_visit(vg)
-          line_items_visit.remove_visit(vg)
-          line_items_visit.visits.count.should eq(1)
+          vg = arm1.visit_groups.create(position: nil)
+          @line_items_visit.add_visit(vg)
+          @line_items_visit.remove_visit(vg)
+          @line_items_visit.visits.count.should eq(10)
         end
       end
     end

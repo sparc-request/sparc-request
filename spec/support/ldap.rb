13989cf9b1f33441970b4e218c147d5b9e89f3f1
Paul Brannan <curlypaul924@gmail.com>
2013-05-16 17:15:20 -0400

    Added a check to make sure ldap is enabled on the test environment.

--- spec/support/ldap.rb
+++ spec/support/ldap.rb
@@ -1,4 +1,9 @@
 require "#{Rails.root.join("lib/directory")}"
+
+if not Directory::USE_LDAP then
+  raise "LDAP is not enabled for the test environment.  Please enable it in config/application.yml (Don't worry, the tests won't try to connect to a real LDAP server, because they stub Net::LDAP with test data)."
+end
+
 def create_ldap_filter(term)
   fields = [Directory::LDAP_UID, Directory::LDAP_LAST_NAME, Directory::LDAP_FIRST_NAME, Directory::LDAP_EMAIL]
   return fields.map {|f| Net::LDAP::Filter.contains(f, term)}.inject(:|)

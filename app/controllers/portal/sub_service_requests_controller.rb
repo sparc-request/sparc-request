b142b763b39ff9bc2538c70c70c09147ea53ef43
Matt Scott <scoma@musc.edu>
2013-05-20 16:31:27 -0400

    Re-added sub service request id when adding line items in admin

--- app/controllers/portal/sub_service_requests_controller.rb
+++ app/controllers/portal/sub_service_requests_controller.rb
@@ -88,7 +88,8 @@ class Portal::SubServiceRequestsController < Portal::BaseController
     @selected_arm = params[:arm_id] ? Arm.find(@arm_id) : @service_request.arms.first
     service = Service.find(params[:new_service_id])
     if @sub_service_request.create_line_item(
-        service_id: params[:new_service_id])
+        service_id: params[:new_service_id],
+        sub_service_request_id: params[:sub_service_request_id])
       # Have to reload the service request to get the correct direct cost total for the subsidy
       @subsidy.try(:sub_service_request).try(:reload)
       @subsidy.try(:fix_pi_contribution, percent)

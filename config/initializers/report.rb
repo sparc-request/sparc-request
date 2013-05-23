bd5e8b83236e5a11576b58da374e4755ad979fb2
Paul Brannan <curlypaul924@gmail.com>
2013-05-23 10:35:16 -0400

    Added some comments

--- config/initializers/report.rb
+++ config/initializers/report.rb
@@ -1,4 +1,11 @@
+# Report command, e.g.:
+#
+#   rails r report list
+#   rails r report run <report>
+#
 def report
+  # Require reports dynamically so that reports are not loaded as part
+  # of the regular rails application
   require 'reports'
   run_report_command(ARGV)
 end

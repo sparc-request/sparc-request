247dda625c470704efa2d850a801a84c80322bce
Stuart Johnson <work.stuart.johnson@gmail.com>
2013-06-03 13:41:49 -0400

    editing permissions for study tracker

--- app/controllers/study_tracker/base_controller.rb
+++ app/controllers/study_tracker/base_controller.rb
@@ -18,7 +18,7 @@ class StudyTracker::BaseController < ActionController::Base
   private
   def check_clinical_provider_rights
     @org = Organization.tagged_with("ctrc").first
-    if @user.clinical_providers.empty? && !@user.admin_organizations.include?(@org)
+    if @user.clinical_providers.empty? && !@user.admin_organizations({:su_only => true}).include?(@org)
       redirect_to root_path
     end
   end

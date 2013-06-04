6238d31aec40a2b0787391bf8e0929a2a81f95d6
Matt Scott <scoma@musc.edu>
2013-06-04 11:44:11 -0400

    Removed required as an actual column on procedures

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -3,7 +3,9 @@ class Procedure < ActiveRecord::Base
   belongs_to :visit
   belongs_to :service
   belongs_to :line_item
+  attr_accessible :appointment_id
   attr_accessible :visit_id
   attr_accessible :service_id
   attr_accessible :line_item_id
+  attr_accessible :completed
 end

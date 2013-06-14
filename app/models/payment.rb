68094427dc53319b2d83e3dfacabf713d5a85342
Ryan Schenk <rschenk@gmail.com>
2013-06-14 17:16:46 -0400

    Added virtual attributes to payment model to handle date formatting with the least amount of pain

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,9 +1,11 @@
 class Payment < ActiveRecord::Base
   attr_accessible :sub_service_request_id
   attr_accessible :date_submitted
+  attr_accessible :formatted_date_submitted
   attr_accessible :amount_invoiced
   attr_accessible :amount_received
   attr_accessible :date_received
+  attr_accessible :formatted_date_received
   attr_accessible :payment_method
   attr_accessible :details
 
@@ -11,4 +13,36 @@ class Payment < ActiveRecord::Base
   validates :amount_received, numericality: true
 
   belongs_to :sub_service_request
+
+  PAYMENT_METHOD_OPTIONS = %w(Check Cash EFT IIT).freeze
+
+  def formatted_date_received
+    format_date self.date_received
+  end
+
+  def formatted_date_received=(d)
+    self.date_received = parse_date(d)
+  end
+
+  def formatted_date_submitted
+    format_date self.date_submitted
+  end
+
+  def formatted_date_submitted=(d)
+    self.date_submitted= parse_date(d)
+  end
+
+  private
+
+  def format_date(d)
+    d.try(:strftime, '%-m/%d/%Y')
+  end
+
+  def parse_date(str)
+    begin
+      Date.strptime(str.to_s.strip, '%m/%d/%Y')  
+    rescue ArgumentError => e
+      nil
+    end
+  end
 end

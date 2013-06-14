e66375cadd6843e3c227ebe15c8bb5e5d1f1f342
Paul Brannan <curlypaul924@gmail.com>
2013-06-14 16:49:01 -0400

    New file.

---
+++ lib/epic_interface.rb
@@ -0,0 +1,82 @@
+require 'savon'
+require 'securerandom'
+
+# The Savon client by default does not allow adding new soap headers
+# except via the global configuration.  This monkey patch allows adding
+# soap headers via local (per-message) configuration.
+module Savon
+  class Header
+    def header
+      @header ||= build_header
+    end
+
+    def build_header
+      header = {}
+      header.update(@globals.include?(:soap_header) ? @globals[:soap_header] : {})
+      header.update(@locals.include?(:soap_header) ? @locals[:soap_header] : {})
+      return header
+    end
+  end
+end
+
+# Use this class to send protocols (studies/projects) along with their
+# associated billing calendars to Epic via an InterConnect server.
+#
+# Configuration is stored in config/epic.yml.
+class EpicInterface
+
+  # Create a new EpicInterface
+  def initialize
+    @config = YAML.load_file(Rails.root.join('config', 'epic.yml'))[Rails.env]
+    @namespace = 'urn:ihe:qrph:rpe:2009' # TODO: grab from WSDL
+    @endpoint = 'http://TODO' # TODO: grab from WSDL
+    @root = @config['study_root']
+    @client = Savon.client(
+        soap_version: 2,
+        pretty_print_xml: true,
+        convert_request_keys_to: :none,
+        namespace_identifier: 'rpe',
+        wsdl: @config['wsdl'],
+        headers: {
+        },
+        soap_header: {
+        })
+  end
+
+  # Send a study to the Epic InterConnect server.
+  def send_study(study)
+    soap_header = {
+      'wsa:Action' => "#{@namespace}:RetrieveProtocolDefResponse",
+      'wsa:MessageID' => SecureRandom.uuid,
+      'wsa:To' => @endpoint,
+    }
+
+    message = {
+      'protocolDef' => {
+        'query' => {
+          '@root' => @root,
+          '@extension' => study.id, # TODO
+        },
+        'plannedStudy' => {
+          '@classCode' => 'CLNTRL',
+          '@moodCode' => 'DEF',
+          # TODO: ITSVersion?
+          'id' => {
+            '@root' => @root,
+            '@extension' => study.id, # TODO
+          },
+          'title' => study.title,
+          'text' => study.brief_description,
+        }
+      }
+    }
+
+    @client.call(
+        'RetrieveProtocolDefResponse',
+        soap_header: soap_header,
+        message: message)
+
+    # TODO: handle response from the server
+  end
+end
+

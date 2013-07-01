9be6de854520deac9160e53b306bbfcea8af3ad4
Ryan Schenk <rschenk@gmail.com>
2013-07-01 15:13:17 -0400

    Added cover letter editing capability

--- app/assets/javascripts/portal/cover_letters.js.coffee
+++ app/assets/javascripts/portal/cover_letters.js.coffee
@@ -1,3 +1,3 @@
 $(document).ready ->
-  $('#new_cover_letter').submit ->
+  $('form.new_cover_letter, form.edit_cover_letter').submit ->
     $('#cover_letter_content').val( $('#cover_letter_content_editor').html() )
--- app/controllers/study_tracker/cover_letters_controller.rb
+++ app/controllers/study_tracker/cover_letters_controller.rb
@@ -1,5 +1,6 @@
 class StudyTracker::CoverLettersController < StudyTracker::BaseController
   before_filter :load_sub_service_request
+  before_filter :sanitize_content, only: [:create, :update]
 
   def new
     @cover_letter = @sub_service_request.cover_letters.build
@@ -9,9 +10,6 @@ class StudyTracker::CoverLettersController < StudyTracker::BaseController
   end
 
   def create
-    sanitizer = CoverLetterSanitizer.new
-    params[:cover_letter][:content] = sanitizer.sanitize(params[:cover_letter][:content])
-
     if @cover_letter = @sub_service_request.cover_letters.create(params[:cover_letter])
       redirect_to [:study_tracker, @sub_service_request]
     else
@@ -23,9 +21,26 @@ class StudyTracker::CoverLettersController < StudyTracker::BaseController
     @cover_letter = CoverLetter.find(params[:id])
   end
 
+  def edit
+    @cover_letter = CoverLetter.find(params[:id])
+  end
+
+  def update
+    @cover_letter = CoverLetter.find(params[:id])
+    if @cover_letter.update_attributes(params[:cover_letter])
+      redirect_to [:study_tracker, @sub_service_request]
+    else
+      render :edit
+    end
+  end
+
   private
 
   def load_sub_service_request
-        @sub_service_request = SubServiceRequest.find(params[:sub_service_request_id])
+    @sub_service_request = SubServiceRequest.find(params[:sub_service_request_id])
+  end
+
+  def sanitize_content
+    params[:cover_letter][:content] = CoverLetterSanitizer.new.sanitize(params[:cover_letter][:content].to_s)
   end
 end

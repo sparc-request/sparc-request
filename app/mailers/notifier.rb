85a19e50fcabe10ee073a155ab74f96a65043bf6
Andrew Cates <amcates@gmail.com>
2013-07-05 15:52:22 -0400

    system satisfaction survey e-mail

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,4 +1,15 @@
 class Notifier < ActionMailer::Base
+  def system_satisfaction_survey response_set
+    @response_set = response_set 
+    @identity = Identity.find response_set.user_id
+    
+    email = Rails.env == 'production' ? ADMIN_MAIL_TO : DEFAULT_MAIL_TO
+    cc = Rails.env == 'production' ? SYSTEM_SATISFACTION_SURVEY_CC : nil
+    subject = Rails.env == 'production' ? "System satisfaction survey completed in #{I18n.t('application_title')}" : "[#{Rails.env.capitalize} - EMAIL TO #{ADMIN_MAIL_TO} AND CC TO #{SYSTEM_SATISFACTION_SURVEY_CC}] System satisfaction survey completed in #{I18n.t('application_title')}"
+    
+    mail(:to => email, :cc => cc, :from => @identity.email, :subject => subject) 
+  end
+
   def ask_a_question quick_question
     @quick_question = quick_question
 

be3ec4e11574bd4a623cd5d5928a2ae8cfef204e
Paul Brannan <curlypaul924@gmail.com>
2013-07-17 15:25:59 -0400

    Multiple changes to get InterConnect to accept messages from SPARC.

--- lib/epic_interface.rb
+++ lib/epic_interface.rb
@@ -2,10 +2,10 @@ require 'savon'
 require 'securerandom'
 require 'builder'
 
-# The Savon client by default does not allow adding new soap headers
-# except via the global configuration.  This monkey patch allows adding
-# soap headers via local (per-message) configuration.
 module Savon
+  # The Savon client by default does not allow adding new soap headers
+  # except via the global configuration.  This monkey patch allows adding
+  # soap headers via local (per-message) configuration.
   class LocalOptions < Options
     def soap_header(header)
       @options[:soap_header] = header
@@ -24,6 +24,14 @@ module Savon
       return header
     end
   end
+
+  # We also need to be able to grab the configured endpoint and put it
+  # into the wsa:To header.
+  class Client
+    def endpoint
+      return @globals[:endpoint] || @wsdl.endpoint
+    end
+  end
 end
 
 # Use this class to send protocols (studies/projects) along with their
@@ -38,18 +46,28 @@ class EpicInterface
 
     # TODO: grab these from the WSDL
     @namespace = @config['namespace'] || 'urn:ihe:qrph:rpe:2009'
-    @root = @config['study_root']
+    @study_root = @config['study_root']
     @epoch = Date.parse(@config['epoch'] || '2013-01-01')
+
+    if not @study_root then
+      raise "study_root not configured in epic.yml"
+    end
 
     # TODO: I'm not really convinced that Savon is buying us very much
     # other than some added complexity, but it's working, so no point in
     # pulling it out.
+    #
+    # We must set namespace_identifier to nil here, in order to prevent
+    # Savon from prepending a wsdl: prefix to the
+    # RetrieveProtocolDefResponse tag and to force it to set an xmlns
+    # attribute (ensuring that all the children of the
+    # RetrieveProtocolDefResponse element are in the right namespace).
     @client = Savon.client(
         logger: Rails.logger,
         soap_version: 2,
         pretty_print_xml: true,
         convert_request_keys_to: :none,
-        namespace_identifier: 'rpe',
+        namespace_identifier: nil,
         namespace: @namespace,
         endpoint: @config['endpoint'],
         wsdl: @config['wsdl'],
@@ -65,8 +83,8 @@ class EpicInterface
   def soap_header(msg_type)
     soap_header = {
       'wsa:Action' => "#{@namespace}:#{msg_type}",
-      'wsa:MessageID' => SecureRandom.uuid,
-      'wsa:To' => @endpoint,
+      'wsa:MessageID' => "uuid:#{SecureRandom.uuid}",
+      'wsa:To' => @client.endpoint,
     }
 
     return soap_header
@@ -90,7 +108,7 @@ class EpicInterface
 
   # Send a study to the Epic InterConnect server.
   def send_study(study)
-    message = self.class.study_creation_message
+    message = study_creation_message(study)
     call('RetrieveProtocolDefResponse', message)
 
     # TODO: handle response from the server
@@ -100,7 +118,7 @@ class EpicInterface
   # The study must have already been created (via #send_study) before
   # calling this method.
   def send_billing_calendar(study)
-    message = self.class.study_calendar_definition_message
+    message = study_calendar_definition_message(study)
     call('RetrieveProtocolDefResponse', message)
 
     # TODO: handle response from the server
@@ -108,14 +126,14 @@ class EpicInterface
 
   # Build a study creation message to send to epic and return it as a
   # string.
-  def self.study_creation_message(study)
+  def study_creation_message(study)
     xml = Builder::XmlMarkup.new(indent: 2)
 
-    xml.query(root: @root, extension: study.id)
+    xml.query(root: @study_root, extension: study.id)
 
     xml.protocolDef {
       xml.plannedStudy(xmlns: 'urn:hl7-org:v3', classCode: 'CLNTRL', moodCode: 'DEF') {
-        xml.id(root: @root, extension: study.id)
+        xml.id(root: @study_root, extension: study.id)
         xml.title study.title
         xml.text study.brief_description
 
@@ -142,16 +160,16 @@ class EpicInterface
 
   # Bulid a study calendar definition message to send to epic and return
   # it as a string.
-  def self.study_calendar_definition_message(study)
+  def study_calendar_definition_message(study)
     xml = Builder::XmlMarkup.new(indent: 2)
 
-    xml.query(root: @root, extension: self.id)
+    xml.query(root: @study_root, extension: study.id)
 
     xml.protocolDef {
       xml.plannedStudy(xmlns: 'urn:hl7-org:v3', classCode: 'CLNTRL', moodCode: 'DEF') {
-        xml.id(root: @root, extension: self.id)
-        xml.title self.title
-        xml.text self.brief_description
+        xml.id(root: @study_root, extension: study.id)
+        xml.title study.title
+        xml.text study.brief_description
 
         # component1 - One calendar event definition out of a sequence.
         # Must contain a timePointEventDefinition tag.
@@ -163,12 +181,12 @@ class EpicInterface
         # Calendar, Cycles, and Days/Visits) or setup of specific visit
         # (Procedures within a visit.  Also known as activities).
 
-        self.service_requests.each do |service_request|
+        study.service_requests.each do |service_request|
           service_request.arms.each_with_index do |arm, arm_idx|
 
             xml.component4(typeCode: 'COMP') {
               xml.timePointEventDefinition(classCode: 'CTTEVENT', moodCode: 'DEF') {
-                xml.id(root: @root, extension: "STUDY#{self.id}.ARM#{arm.id}")
+                xml.id(root: @study_root, extension: "STUDY#{study.id}.ARM#{arm.id}")
                 xml.title(arm.name)
                 xml.code(code: 'CELL', codeSystem: 'n/a')
 
@@ -176,7 +194,7 @@ class EpicInterface
                   xml.sequenceNumber(value: arm_idx + 1) 
 
                   xml.timePointEventDefinition(classCode: 'CTTEVENT', moodCode: 'DEF') {
-                    xml.id(root: @root, extension: "STUDY#{self.id}.ARM#{arm.id}.CYCLE1")
+                    xml.id(root: @study_root, extension: "STUDY#{study.id}.ARM#{arm.id}.CYCLE1")
                     xml.title('Cycle 1')
                     xml.code(code: 'CYCLE', codeSystem: 'n/a')
 
@@ -191,7 +209,7 @@ class EpicInterface
                       xml.component1(typeCode: 'COMP') {
                         xml.sequenceNumber(value: visit_group.position)
                         xml.timePointEventDefinition(classCode: 'CTTEVENT', moodCode: 'DEF') {
-                          xml.id(root: @root, extension: "STUDY#{self.id}.ARM#{arm.id}.CYCLE1.DAY#{visit_group.position}")
+                          xml.id(root: @study_root, extension: "STUDY#{study.id}.ARM#{arm.id}.CYCLE1.DAY#{visit_group.position}")
                           xml.title(visit_group.name)
                         }
                       }
@@ -204,20 +222,20 @@ class EpicInterface
           end
         end
 
-        self.service_requests.each do |service_request|
+        study.service_requests.each do |service_request|
           service_request.arms.each do |arm|
             arm.visit_groups.each do |visit_group|
 
               xml.component4(typeCode: 'COMP') {
                 xml.timePointEventDefinition(classCode: 'CTTEVENT', moodCode: 'DEF') {
-                  xml.id(root: @root, extension: "STUDY#{self.id}.ARM#{arm.id}.DAY#{visit_group.position}")
+                  xml.id(root: @study_root, extension: "STUDY#{study.id}.ARM#{arm.id}.DAY#{visit_group.position}")
                   xml.title(visit_group.name)
                   xml.code(code: 'VISIT', codeSystem: 'n/a')
 
                   arm.line_items.each do |line_item|
                     xml.component1(typeCode: 'COMP') {
                       xml.timePointEventDefinition(classCode: 'CTTEVENT', moodCode: 'DEF') {
-                        xml.id(root: @root, extension: "STUDY#{self.id}.ARM#{arm.id}.DAY#{visit_group.position}.PROC#{line_item.id}")
+                        xml.id(root: @study_root, extension: "STUDY#{study.id}.ARM#{arm.id}.DAY#{visit_group.position}.PROC#{line_item.id}")
                         xml.code(code: 'PROC', codeSystem: 'n/a')
 
                         xml.component2(typeCode: 'COMP') {
@@ -265,7 +283,7 @@ class EpicInterface
   # contain a valid date.
   #
   # The day passed in here is assumed to be 1-based.
-  def self.relative_date(day)
+  def relative_date(day)
     date = @epoch + day - 1
     return date.strftime("%Y%m%d")
   end

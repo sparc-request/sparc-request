f3aa97d70299fcaa633037846b50694d9c66ac22
Jason Leonard <jleonardw9@gmail.com>
2013-07-26 14:19:54 -0400

    Added quantity to procedures, updated visit schedule logic.

--- app/controllers/study_tracker/calendars_controller.rb
+++ app/controllers/study_tracker/calendars_controller.rb
@@ -15,7 +15,7 @@ class StudyTracker::CalendarsController < StudyTracker::BaseController
     @uncompleted_appointments = @appointments.reject{|x| x.completed_at? }
     @default_appointment = @uncompleted_appointments.first || @appointments.first
     default_procedures = @default_appointment.procedures.select{|x| x.line_item.service.organization == @nutrition}
-    @default_subtotal = default_procedures.sum{|x| x.line_item.per_unit_cost}
+    @default_subtotal = default_procedures.sum{|x| x.total}
 
     
   end
--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -8,6 +8,7 @@ class Procedure < ActiveRecord::Base
   attr_accessible :visit_id
   attr_accessible :line_item_id
   attr_accessible :completed
+  attr_accessible :quantity
 
   def required?
     self.visit.to_be_performed?
@@ -15,5 +16,15 @@ class Procedure < ActiveRecord::Base
 
   def core
     self.line_item.service.organization
+  end
+
+  def default_quantity
+    default = self.quantity
+    default ||= self.visit.research_billing_qty
+    default
+  end
+
+  def total
+    self.quantity * self.line_item.per_unit_cost
   end
 end

4bcab42f5d81a975a580d69e4cc871a12c7ec8c7
Stuart Johnson <work.stuart.johnson@gmail.com>
2013-08-07 13:30:31 -0400

    initial add_service commit

--- app/controllers/study_tracker/calendars_controller.rb
+++ app/controllers/study_tracker/calendars_controller.rb
@@ -1,23 +1,9 @@
 class StudyTracker::CalendarsController < StudyTracker::BaseController
-  before_filter :check_work_fulfillment_status, :except => :add_note
+  before_filter :check_work_fulfillment_status, :except => [:add_note, :add_service]
 
   def show
-    # Get the cores
-    @nutrition = Organization.tagged_with("nutrition").first
-    @nursing   = Organization.tagged_with("nursing").first
-    @lab       = Organization.tagged_with("laboratory").first
-    @imaging   = Organization.tagged_with("imaging").first
-
     @calendar = Calendar.find(params[:id])
-    @subject = @calendar.subject
-    @appointments = @calendar.appointments.includes(:visit_group).sort{|x,y| x.visit_group.position <=> y.visit_group.position }
-
-    @uncompleted_appointments = @appointments.reject{|x| x.completed_at? }
-    @default_appointment = @uncompleted_appointments.first || @appointments.first
-    default_procedures = @default_appointment.procedures.select{|x| x.line_item.service.organization == @nursing}
-    @default_subtotal = default_procedures.sum{|x| x.total}
-
-    
+    get_calendar_data(@calendar)
   end
 
   def add_note
@@ -32,11 +18,40 @@ class StudyTracker::CalendarsController < StudyTracker::BaseController
     end
   end
 
+  def add_service
+    appointment = Appointment.find(params[:appointment_id])
+    @calendar = appointment.calendar
+    @sub_service_request = SubServiceRequest.find(params[:ssr_id])
+    if appointment.procedures.create(:service_id => params[:service_id])
+      get_calendar_data(@calendar)
+      render :action => 'show'
+    else
+      respond_to do |format|
+        format.js { render :status => 500, :json => clean_errors(appointment.errors) }
+      end
+    end
+  end
+
   private
   def check_work_fulfillment_status
     @sub_service_request ||= SubServiceRequest.find(params[:sub_service_request_id])
     unless @sub_service_request.in_work_fulfillment?
       redirect_to root_path
     end
+  end
+  def get_calendar_data(calendar)
+    # Get the cores
+    @nutrition = Organization.tagged_with("nutrition").first
+    @nursing   = Organization.tagged_with("nursing").first
+    @lab       = Organization.tagged_with("laboratory").first
+    @imaging   = Organization.tagged_with("imaging").first
+
+    @subject = calendar.subject
+    @appointments = calendar.appointments.includes(:visit_group).sort{|x,y| x.visit_group.position <=> y.visit_group.position }
+
+    @uncompleted_appointments = @appointments.reject{|x| x.completed_at? }
+    @default_appointment = @uncompleted_appointments.first || @appointments.first
+    default_procedures = @default_appointment.procedures.select{|x| (x.line_item ? x.line_item.service.organization : x.service.organization) == @nursing}
+    @default_subtotal = default_procedures.sum{|x| x.total}
   end
 end

0b42df014f2a1fc79efd37ca9e8c35cb1e3d1f22
Stuart Johnson <work.stuart.johnson@gmail.com>
2013-08-22 11:33:03 -0400

    fixing otf logic in study tracker

--- app/controllers/portal/line_items_controller.rb
+++ app/controllers/portal/line_items_controller.rb
@@ -52,6 +52,7 @@ class Portal::LineItemsController < Portal::BaseController
     @selected_arm = @service_request.arms.first
     @study_tracker = params[:study_tracker] == "true"
     @line_items = @sub_service_request.line_items
+    was_one_time_fee = @line_item.service.is_one_time_fee?
     
     if @line_item.destroy
       # Have to reload the service request to get the correct direct cost total for the subsidy
@@ -59,11 +60,8 @@ class Portal::LineItemsController < Portal::BaseController
       @service_request = @sub_service_request.service_request
       @candidate_one_time_fees = @sub_service_request.candidate_services.select {|x| x.is_one_time_fee?}
       @candidate_per_patient_per_visit = @sub_service_request.candidate_services.reject {|x| x.is_one_time_fee?}
-      unless @study_tracker
-        render 'portal/sub_service_requests/add_line_item'
-      else
-        render 'portal/sub_service_requests/add_otf_line_item'
-      end
+
+      render 'portal/sub_service_requests/add_line_item'
     end
   end
 end

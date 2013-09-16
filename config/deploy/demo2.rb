073cf580a23bc7799a9087d55d00a22ee5d4e94e
Andrew Cates <amcates@gmail.com>
2013-09-16 14:09:37 -0400

    setup to deploy sparc-demo2

---
+++ config/deploy/demo2.rb
@@ -0,0 +1,14 @@
+set :rvm_ruby_string, "ruby-1.9.3-p286"
+set :rvm_type, :system
+set :application, "sparc-rails-2"
+
+set :deploy_to, "#{deploy_root}/#{application}"
+set :rails_env, "testing"
+set :domain, "sparc-demo.musc.edu"
+set :branch, "demo"
+
+role :web, domain
+role :app, domain, :primary => true
+role :db, domain, :primary => true
+
+require 'rvm/capistrano'

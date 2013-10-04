79d2ea1b66cfd383a77ad927d5a5a51132ac4957
Jason Leonard <jleonardw9@gmail.com>
2013-10-04 09:10:57 -0400

    Small tweak to filtering logic on cwf home page.

--- app/helpers/portal/sub_service_requests_helper.rb
+++ app/helpers/portal/sub_service_requests_helper.rb
@@ -25,8 +25,7 @@ module Portal::SubServiceRequestsHelper
   #ctrc can see all ssr's.
   def user_can_view_ssr?(study_tracker, ssr, user)
     can_view = false
-
-    if user.is_super_user? || user.is_service_provider? || user.clinical_provider_for_ctrc? || (study_tracker == false)
+    if user.is_super_user? || user.clinical_provider_for_ctrc? || (user.is_service_provider? && (study_tracker == false)) 
       can_view = true
     else
       ssr.line_items.each do |line_item|

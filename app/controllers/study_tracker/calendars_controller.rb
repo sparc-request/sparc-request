a34a05b450e6ef2b13e8df0026ca6a92af164e3b
Jason Leonard <jleonardw9@gmail.com>
2013-10-14 16:10:19 -0400

    Cores are now shown in cwf based on their 'show_in_cwf' flag and ordered by their position attribute.

--- app/controllers/study_tracker/calendars_controller.rb
+++ app/controllers/study_tracker/calendars_controller.rb
@@ -51,23 +51,19 @@ class StudyTracker::CalendarsController < StudyTracker::BaseController
 
   def get_calendar_data(calendar)
     # Get the cores
-    @nutrition = Organization.tagged_with("nutrition").first
-    @nursing   = Organization.tagged_with("nursing").first
-    @lab       = Organization.tagged_with("laboratory").first
-    @imaging   = Organization.tagged_with("imaging").first
-    @pft       = Organization.tagged_with("pft").first
+    cwf_cores = Organization.get_cwf_organizations
 
     @subject = calendar.subject
     @appointments = calendar.appointments.sort{|x,y| x.position_switch <=> y.position_switch }
 
     
-    @default_core = (cookies['current_core'] ? Organization.find(cookies['current_core']) : @nursing)
+    @default_core = (cookies['current_core'] ? Organization.find(cookies['current_core']) : cwf_cores.first)
 
     @uncompleted_appointments = @appointments.reject{|x| x.completed_for_core?(@default_core.id) }
     @completed_appointments = @appointments.select{|x| x.completed?}
     @default_appointment = @uncompleted_appointments.first || @appointments.first
 
-    default_procedures = @default_appointment.procedures.select{|x| x.core == @nursing}
+    default_procedures = @default_appointment.procedures.select{|x| x.core == cwf_cores.first}
     @default_subtotal = @default_appointment.completed_for_core?(@default_core.id) ? default_procedures.sum{|x| x.total} : 0.00
   end
 

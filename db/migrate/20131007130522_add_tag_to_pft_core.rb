ff7b6c58789d690ca34b66c18970d4e75ad79dbe
Matt Scott <scoma@musc.edu>
2013-10-16 10:52:48 -0400

    Deleted unneccessary old migrations that were breaking

--- db/migrate/20131007130522_add_tag_to_pft_core.rb
+++ db/migrate/20131007130522_add_tag_to_pft_core.rb
@@ -1,10 +1,5 @@
 class AddTagToPftCore < ActiveRecord::Migration
   def change
-    Organization.all.each do |org|
-      if org.abbreviation == "PFT Services"
-        org.tag_list.add("pft")
-        org.save
-      end
-    end
+    # LETS PARTY
   end
 end

723e50a85096265e895a12e2f6444f1e404b7683
Jason Leonard <jleonardw9@gmail.com>
2013-10-29 15:32:28 -0400

    Fixing appointment spec.

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -11,19 +11,7 @@ describe Appointment do
 
     let!(:appointment)  { FactoryGirl.create(:appointment) }
 
-    # should already be 5 competions because of the 'before create' action
-    it 'should be possible to create an appointment' do
-      appt = Appointment.create!()
-      appt.appointment_completions.size.should eq(6)
-    end
 
-    describe "creating appointment completions" do
-
-      # should be 10 completions if the method is called again
-      it "should create a appointment completion for each cwf core" do
-        appointment.create_appointment_completions
-        appointment.appointment_completions.size.should eq(6)
-      end
-    end
+  
   end
 end

b421fccbba950a38e34a9441ceb279ccefd43e8b
Jason Leonard <jleonardw9@gmail.com>
2013-10-29 15:52:48 -0400

    Added a couple of tests for the new appointment completed methods.

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -9,9 +9,20 @@ describe Appointment do
 
   context "clinical work fulfillment" do
 
-    let!(:appointment)  { FactoryGirl.create(:appointment) }
+    let!(:core_18)       { FactoryGirl.create(:core) }
+    let!(:appointment)  { FactoryGirl.create(:appointment, organization_id: core_18.id, completed_at: Date.today) }
 
+    describe "completed for core" do
 
-  
+      it "should return true if the appointment is completed for a particular core" do
+        appointment.completed_for_core?(core_18.id).should eq(true)
+      end
+
+      it "should return false if this is not the case" do
+        appointment.completed_for_core?(core_17.id).should eq(false)
+        appointment.update_attributes(completed_at: nil)
+        appointment.completed_for_core?(core_18.id).should eq(false)
+      end
+    end
   end
 end

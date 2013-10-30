b052b9cf39a85ae00312ad231947b21052470f48
Jason Leonard <jleonardw9@gmail.com>
2013-10-30 11:03:03 -0400

    Fixing the survey notification spec.

--- spec/mailers/survey_notification_spec.rb
+++ spec/mailers/survey_notification_spec.rb
@@ -28,7 +28,7 @@ describe SurveyNotification do
  
     #ensure that the e-mail body is correct
     it 'contains survey name' do
-      mail.body.encoded.should include("#{identity.display_name}\r\nhas completed a system satisfaction survey.\r\nResults can be found\r\n<a href=\"http://localhost:3000/surveys/system-satisfaction-survey/abc123\">here</a>\r\n")
+      mail.body.encoded.should include("#{identity.display_name}\r\nhas completed a system satisfaction survey.\r\nResults can be found\r\n<a href=\"http://localhost:0/surveys/system-satisfaction-survey/abc123\">here</a>\r\n")
     end
   end
 
@@ -58,7 +58,7 @@ describe SurveyNotification do
  
     #ensure that the e-mail body is correct
     it 'contains survey name' do
-      mail.body.encoded.should include("Dear #{identity.full_name},\r\n<br>\r\n<br>\r\nThank you for requesting services from\r\n#{ssr_institution(institution)} - #{ssr_provider(provider)} - #{ssr_program_core(core)}.\r\nThe service(s) you requested have now been completed.\r\n<br>\r\nPlease click on the link(s) below to complete the following survey(s) regarding the service(s) you received. Your feedback is important and appreciated!\r\n<br>\r\n<br>\r\n<ul></ul>\r\n<li><a href=\"http://localhost:3000/direct_link_to/system-satisfaction-survey?survey_version=0\">System Satisfaction survey</a></li>\r\n")
+      mail.body.encoded.should include("Dear #{identity.full_name},\r\n<br>\r\n<br>\r\nThank you for requesting services from\r\n#{ssr_institution(institution)} - #{ssr_provider(provider)} - #{ssr_program_core(core)}.\r\nThe service(s) you requested have now been completed.\r\n<br>\r\nPlease click on the link(s) below to complete the following survey(s) regarding the service(s) you received. Your feedback is important and appreciated!\r\n<br>\r\n<br>\r\n<ul></ul>\r\n<li><a href=\"http://localhost:0/direct_link_to/system-satisfaction-survey?survey_version=0\">System Satisfaction survey</a></li>\r\n")
     end
   end
 end

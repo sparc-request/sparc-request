ff048c2b83718b705b1e1949f709437cf3e669bc
Jason Leonard <jleonardw9@gmail.com>
2013-12-20 11:54:56 -0500

    Oops.  Set all one time fee quantity minimums back to one.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131126141638) do
+ActiveRecord::Schema.define(:version => 20131220165307) do
 
   create_table "affiliations", :force => true do |t|
     t.integer  "protocol_id"

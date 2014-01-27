04fa12aad7c5d65eab553024a46269a390c6b38c
Jason Leonard <jleonardw9@gmail.com>
2014-01-27 15:41:41 -0500

    Fixing the visits_for_delete method to use position instead of visit count.

--- app/helpers/portal/service_requests_helper.rb
+++ app/helpers/portal/service_requests_helper.rb
@@ -116,15 +116,34 @@ module Portal::ServiceRequestsHelper
   end
 
   def visits_for_delete arm
-    vg = arm.line_items_visits.first
-    visit_count = vg.visits.count
-    arr = []
-    visit_count.times do |visit|
-      visit_name = vg.visits[visit].visit_group.name || "Visit #{visit}"
-      arr << ["Delete Visit #{visit + 1} - #{visit_name}", visit + 1]
+    unless arm.line_items_visits.empty?
+      vg = arm.line_items_visits.first
+        unless vg.visits.empty?
+          visit_count = vg.visits.last.position
+          arr = []
+          visit_count.times do |visit|
+            visit_name = vg.visits[visit].visit_group.name || "Visit #{visit}"
+            arr << ["Delete Visit #{visit + 1} - #{visit_name}", visit + 1]
+          end
+        else
+          arr = [["No Visits", nil]]
+        end
+    else
+      arr = []
+      visit_count = 0
     end
 
-    options_for_select(arr, vg.visits.count)
+    options_for_select(arr, visit_count)
+  end
+
+  def show_delete_visit_link? arm
+    show_link = false
+    vg = arm.line_items_visits.first
+    unless vg.visits.empty?
+      show_link = true
+    end
+
+    show_link
   end
 
 

72b7c3e5ae1e00c8bd64258e2b211c581d9704f4
Stuart Johnson <work.stuart.johnson@gmail.com>
2014-03-17 13:18:33 -0400

    tests for cm institutions

--- spec/features/catalog_manager/edit_institution_spec.rb
+++ spec/features/catalog_manager/edit_institution_spec.rb
@@ -4,6 +4,7 @@ describe 'edit an institution', :js => true do
 
   before :each do
     default_catalog_manager_setup
+    Tag.create(:name => "ctrc")
     click_link('Medical University of South Carolina')
   end
 
@@ -23,36 +24,6 @@ describe 'edit an institution', :js => true do
       page.should have_content( 'Medical University of South Carolina saved successfully' )
     end
 
-    context "editing status options" do
-
-      before :each do
-        @institution = Organization.where(abbreviation: "MUSC").first
-        wait_for_javascript_to_finish
-      end
-
-      it "should get the default statuses" do
-        @institution.get_available_statuses.should eq( {"draft" => "Draft", "submitted" => "Submitted", "get_a_quote" => "Get a Quote", "in_process" => "In Process", "complete" => "Complete", "awaiting_pi_approval" => "Awaiting PI Approval", "on_hold" => "On Hold"} )
-      end
-
-      it "should only get the statuses that are checked" do
-        find("#institution_available_statuses_attributes_0__destroy").click
-        first("#save_button").click
-        wait_for_javascript_to_finish
-        
-        @institution.get_available_statuses.should eq( {"draft" => "Draft"} )
-      end
-
-      it "should not create duplicates if saved twice" do
-        find("#institution_available_statuses_attributes_0__destroy").click
-        first("#save_button").click
-        wait_for_javascript_to_finish
-        first("#save_button").click
-        wait_for_javascript_to_finish
-
-        @institution.get_available_statuses.should eq( {"draft" => "Draft"} )
-      end
-    end
-
     context "adding and removing tags" do
 
       before :each do
@@ -60,31 +31,24 @@ describe 'edit an institution', :js => true do
         wait_for_javascript_to_finish
       end
 
-      it "should get the tag that is entered" do
-        fill_in 'institution_tag_list', :with => 'The Doctor'
-        first("#save_button").click
-        wait_for_javascript_to_finish
-
-        @institution.tag_list.should eq(["The Doctor"])
+      it "should list the tags" do
+        page.should have_css("#institution_tag_list_ctrc")
       end
 
-      it "should delete the tag once the field is cleared and saved" do
-        fill_in 'institution_tag_list', :with => 'The Doctor'
+      it "should be able to check a tag box" do
+        find('#institution_tag_list_ctrc').click
         first("#save_button").click
-        wait_for_javascript_to_finish
-        fill_in 'institution_tag_list', :with => ''
-        first("#save_button").click
-        wait_for_javascript_to_finish
-
-        @institution.tag_list.should eq([])
+        page.should have_content( 'Medical University of South Carolina saved successfully' )
+        find('#institution_tag_list_ctrc').should be_checked
+        @institution.tag_list.should eq(['ctrc'])
       end
+    end
 
-      it "should create an array of tags if more than one is entered" do
-        fill_in 'institution_tag_list', :with => 'The Doctor, Dalek, Amy Pond'
-        first("#save_button").click
-        wait_for_javascript_to_finish
-
-        @institution.tag_list.should eq(['The Doctor', 'Dalek', 'Amy Pond'])
+    context "viewing user rights section" do
+      it "should show user rights section" do
+        find('#user_rights').click
+        sleep 3
+        find('#su_info').should be_visible
       end
     end
   end

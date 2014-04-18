82475a66e02f8ea6ad5df08cfad60707cfd953ac
Jason Leonard <jleonardw9@gmail.com>
2014-04-18 08:32:16 -0400

    Fixing another ajax issue with auto pricing spec.

--- spec/features/catalog_manager/auto_pricing_adj_spec.rb
+++ spec/features/catalog_manager/auto_pricing_adj_spec.rb
@@ -61,6 +61,7 @@ feature 'automatic pricing adjustment' do
     end
     within('.pricing_map_accordion') do
       increase_decrease_date = (Date.today + 1.month).strftime("%-m/#{numerical_day}/%Y")
+      sleep 3
       page.should have_content("Effective on #{increase_decrease_date} - Display on #{increase_decrease_date}")
     end
 

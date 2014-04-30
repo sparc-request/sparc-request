f83f8900e614b28cc5c59a175623101bf003e85f
Jason Leonard <jleonardw9@gmail.com>
2014-04-30 15:05:47 -0400

    Finishing up otf charges spec.

--- spec/features/study_tracker/study_level_charges_spec.rb
+++ spec/features/study_tracker/study_level_charges_spec.rb
@@ -7,7 +7,7 @@ describe "study level charges", js: true do
   build_service_request_with_project()
 
   before :each do
-    create_visits    
+    add_visits
     sub_service_request.update_attributes(in_work_fulfillment: true)
   end
 
@@ -26,6 +26,30 @@ describe "study level charges", js: true do
       find('.add_nested_fields', visible: true).click
       wait_for_javascript_to_finish
       page.should have_content('Date')
+    end
+
+    it 'should set and save the fields' do
+      find('.add_nested_fields', visible: true).click
+      wait_for_javascript_to_finish
+      within('.cwf_one_time_fee_service') do
+        within('.fields', visible: true) do
+          find('.fulfillment_date').set("14/5/1")
+          find('.fulfillment_quantity').set(1)
+          find('.fulfillment_unit_quantity').set(1)
+          find('.fulfillment_notes').set("You're darn tootin'!")
+        end
+      end
+      within('.study_level_charges', visible: true) do
+        click_button("Save")
+        wait_for_javascript_to_finish
+      end
+
+      otf = sub_service_request.one_time_fee_line_items.first
+      fulfillment = otf.fulfillments.first
+      fulfillment.date.should eq("Thu, 01 May 2014 00:00:00 EDT -04:00")
+      fulfillment.quantity.should eq(1)
+      fulfillment.unit_quantity.should eq(1)
+      fulfillment.notes.should eq("You're darn tootin'!")
     end
   end
 end

bc90d99d405c6a32815349ed038cbde75e950849
Jason Leonard <jleonardw9@gmail.com>
2014-05-02 10:43:41 -0400

    Updated fix for subsidy bug.

--- app/helpers/subsidies_helper.rb
+++ app/helpers/subsidies_helper.rb
@@ -6,10 +6,13 @@ module SubsidiesHelper
     currency_converter(rf)
   end
 
-  def calculate_subsidy_percentage direct_cost, contribution
+  def calculate_subsidy_percentage direct_cost, contribution, subsidy
     # multiply contribution by 100 to convert to cents
     return 0 if direct_cost == 0.0
     funded_amount = direct_cost - contribution rescue 0
-    ((funded_amount / direct_cost) * 100).round(2)
+    percent = ((funded_amount / direct_cost) * 100).round(2)
+    subsidy.update_attributes(:admin_percent_subsidy => percent)
+
+    percent
   end
 end

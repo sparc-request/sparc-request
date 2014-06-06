2df6e962a1fa3d57d541cebba2c062e6bfcb0e92
bjk7 <brian.kelsey2@gmail.com>
2014-06-06 14:27:26 -0400

    Grand totals and Arm/Other totals added to consolidated request tab [#65598564]

--- app/helpers/service_calendar_helper.rb
+++ app/helpers/service_calendar_helper.rb
@@ -125,11 +125,11 @@ module ServiceCalendarHelper
   def display_protocol_total_otfs protocol, current_request, portal
     sum = 0
     protocol.service_requests.each do |service_request|
+      next unless service_request.has_one_time_fee_services?
       if ['first_draft', 'draft'].include?(service_request.status)
         next if portal
         next if service_request != current_request
       end
-      next unless service_request.has_one_time_fee_services?
       sum += service_request.total_costs_one_time
     end
     currency_converter sum

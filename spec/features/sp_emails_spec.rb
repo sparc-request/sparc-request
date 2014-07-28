2096edb24018a74355d65956a09ded905cf01f42
ctt3 <thiry@musc.edu>
2014-07-28 14:25:11 -0400

    sp_emails_spec continuation

--- spec/features/sp_emails_spec.rb
+++ spec/features/sp_emails_spec.rb
@@ -2,27 +2,80 @@ require 'spec_helper'
 require 'surveyor/parser'
 require 'rake'
 
-describe "Service Provider Emails", :js => true do
+describe "Emails", :js => true do
   let_there_be_lane
   let_there_be_j
   fake_login_for_each_test
   build_service_request_with_project
 
+  let!(:service3)            { FactoryGirl.create(:service, organization_id: program.id, name: 'ABCD') }
+  let!(:pricing_setup)       { FactoryGirl.create(:pricing_setup, organization_id: program.id, display_date: Time.now - 1.day, federal: 50, corporate: 50, other: 50, member: 50, college_rate_type: 'federal', federal_rate_type: 'federal', industry_rate_type: 'federal', investigator_rate_type: 'federal', internal_rate_type: 'federal', foundation_rate_type: 'federal')}
+  let!(:pricing_map)         { FactoryGirl.create(:pricing_map, unit_minimum: 1, unit_factor: 1, service_id: service3.id, is_one_time_fee: true, quantity_type: "Each", quantity_minimum: 5, otf_unit_type: "Week", display_date: Time.now - 1.day, full_rate: 2000, units_per_qty_max: 20) }
+
+
   before :each do
     add_visits
-    visit review_service_request_path service_request.id
   end
 
-  describe "clicking submit on the review page" do
-    it "should send emails to user, admins, and service providers", :js => true do
-      find("#submit_services2").click
-      wait_for_javascript_to_finish
-      click_button("No")
-      wait_for_javascript_to_finish
-      sp_email = get_mail(service_request.id, sub_service_request.id, role = 'service provider')
-      user_email = get_mail(service_request.id, sub_service_request.id, role = 'user')
-      admin_email = get_mail(service_request.id, sub_service_request.id, role = 'admin')
-      visit_email sp_email
+  describe "should render correctly for" do
+    it "service providers", :js => true do
+      visit_mail_for 'service provider'
+      assert_email_project_information
+      assert_email_project_roles
+
+      #with no changes, should not have audited information table
+      page.should_not have_xpath "//th[text()='Service']/following-sibling::th[text()='Action']"
+
+      if service_request.arms.empty?
+        #no arms, should not have arm information table
+        page.should_not have_xpath "//table//strong[text()='Protocol Arm Information']"
+      else
+        #should have appropriate arm information in table
+        page.should have_xpath "//table//strong[text()='Protocol Arm Information']"
+        service_request.arms.each do |arm|
+          page.should have_xpath "//td[text()='#{arm.name}']/following-sibling::td[text()='#{arm.subject_count}']/following-sibling::td[text()='#{arm.visit_count}']"
+        end
+      end
+    end
+
+    it "users", :js => true do
+      visit_mail_for 'user'
+      assert_email_project_information
+      assert_email_project_roles
+      #users should not have audited information table
+      page.should_not have_xpath "//th[text()='Service']/following-sibling::th[text()='Action']"
+      #users should not have arm information table
+      page.should_not have_xpath "//table//strong[text()='Protocol Arm Information']"
+    end
+
+    it "admins", :js => true do
+      visit_mail_for 'admin'
+      assert_email_project_information
+      assert_email_project_roles
+      #admins should not have audited information table
+      page.should_not have_xpath "//th[text()='Service']/following-sibling::th[text()='Action']"
+      #admins should not have arm information table
+      page.should_not have_xpath "//table//strong[text()='Protocol Arm Information']"
+    end
+  end
+
+  describe "should have audited information" do
+    it "when adding a line item to the service request", :js => true do
+      #Submit SR and then add line item as jug2
+      Audited.audit_class.as_user(Identity.find(1)) do
+        service_request.update_status('submitted')
+        service_request.update_attribute(:submitted_at, Time.now)
+        existing_service_ids = service_request.line_items.map(&:service_id)
+        service_request.create_line_items_for_service(service: service3, optional: true, existing_service_ids: existing_service_ids, recursive_call: false)
+        service_request.reload
+        service_request.line_items.each do |li|
+          li.update_attribute(:sub_service_request_id, sub_service_request.id)
+        end
+      end
+
+      visit_mail_for 'service provider'
+      #should have audited information table
+      page.should have_xpath "//th[text()='Service']/following-sibling::th[text()='Action']"
     end
   end
 end

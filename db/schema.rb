54ba72da0db025a4fcc3650e9f44d4cc9f6cb3ff
bjk7 <brian.kelsey2@gmail.com>
2014-09-15 12:20:51 -0400

    [#78105814] Removed old code that checked the visit for the name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140819133958) do
+ActiveRecord::Schema.define(:version => 20140915132438) do
 
   create_table "admin_rates", :force => true do |t|
     t.integer  "line_item_id"

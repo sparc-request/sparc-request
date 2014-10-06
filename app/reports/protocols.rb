7b6943fca17dd81a3d4cd5f81e5ba171448a655d
Jason Leonard <jleonardw9@gmail.com>
2014-10-06 10:28:34 -0400

    Optimization of admin portal landing page [#80119518]

--- app/reports/protocols.rb
+++ app/reports/protocols.rb
@@ -95,13 +95,14 @@ class ProtocolsReport < ReportingModule
 
   # Conditions
   def where args={}
+    organizations = Organization.find(:all)
     selected_organization_id = args[:core_id] || args[:program_id] || args[:provider_id] || args[:institution_id] # we want to go up the tree, service_organization_ids plural because we might have child organizations to include
 
     # get child organization that have services to related to them
     service_organization_ids = [selected_organization_id]
     if selected_organization_id
       org = Organization.find(selected_organization_id)
-      service_organization_ids += org.all_children.map(&:id)
+      service_organization_ids += org.all_children(organizations).map(&:id)
       service_organization_ids.flatten!
       service_organization_ids.uniq!
     end
@@ -111,7 +112,7 @@ class ProtocolsReport < ReportingModule
     # get child organizations that have process_ssr
     if not ssr_organization_ids.empty?
       org = Organization.find(selected_organization_id)
-      ssr_organization_ids = [ssr_organization_ids, org.all_children.select{|x| x.process_ssrs?}.map(&:id)].flatten.uniq
+      ssr_organization_ids = [ssr_organization_ids, org.all_children(organizations).select{|x| x.process_ssrs?}.map(&:id)].flatten.uniq
     end
 
     if args[:service_requests_submitted_at_from] and args[:service_requests_submitted_at_to]

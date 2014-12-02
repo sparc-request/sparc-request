96e138b49e7c87a7db7fb8b65e7039588d6c9d1a
Jason Leonard <jleonardw9@gmail.com>
2014-12-02 12:43:51 -0500

    Deleted wrong file.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -18,38 +18,94 @@
 # INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-class AppointmentCompletion < ActiveRecord::Base
+class Appointment < ActiveRecord::Base
   audited
 
-  belongs_to :appointment
+  belongs_to :calendar
+  belongs_to :visit_group
   belongs_to :organization
-  attr_accessible :completed_date
+  has_many :procedures, :dependent => :destroy
+  has_many :visits, :through => :procedures
+  has_many :notes
+  attr_accessible :visit_group_id
   attr_accessible :organization_id
+  attr_accessible :completed_at
+  attr_accessible :position
+  attr_accessible :name
+
+  attr_accessible :procedures_attributes
+
+  accepts_nested_attributes_for :procedures
+
   attr_accessible :formatted_completed_date
 
   def formatted_completed_date
-    format_date self.completed_date
+    format_date self.completed_at
   end
+
   def formatted_completed_date=(d)
-    self.completed_date = parse_date(d)
+    self.completed_at = parse_date(d)
+  end
+
+
+  def populate_procedures(visits)
+    columns =[:line_item_id,:visit_id,:toasts_generated, :appointment_id]
+    values =[]
+    visits.each do |visit|
+      line_item = visit.line_items_visit.line_item
+      if line_item.service.is_ctrc? && !line_item.service.is_one_time_fee?
+        values << [line_item.id,visit.id,true,self.id]
+      end
+    end
+    if !(values.empty?)
+      Procedure.import columns, values, {:validate=> true}
+      self.reload
+    end
+  end
+
+  def position_switch
+    self.visit_group ? self.visit_group.position : self.position
+  end
+
+  def name_switch
+    self.visit_group ? self.visit_group.name : self.name
+  end
+
+  def completed?
+    if self.completed_at
+      true
+    else
+      false
+    end
+  end
+  
+  def completed_for_core? (core_id)
+    if self.completed? && (self.organization_id == core_id)
+      return true
+    else
+      return false
+    end
+  end
+
+
+  def display_name
+    name_switch 
   end
   
   ### audit reporting methods ###
  
-  def audit_field_value_mapping
-    {"completed_date" => "'ORIGINAL_VALUE'.to_time.strftime('%Y-%m-%d')"}
-  end
-
-  def audit_excluded_fields
-    {"create" => ['appointment_id', 'organization_id']}
-  end
-
   def audit_label audit
-    subject = appointment.calendar.subject
-    subject_label = subject.respond_to?(:audit_label) ? subject.audit_label(audit) : "Subject #{subject.id}"
-    "#{appointment.visit_group.name} #{organization.abbreviation} Appointment Completed for #{subject_label}"
+    name_switch
   end
 
+  def audit_field_value_mapping
+    {"completed_at" => "'ORIGINAL_VALUE'.to_time.strftime('%Y-%m-%d')"}
+  end
+    
+  def audit_excluded_actions
+    ['create']
+  end
+  
   ### end audit reporting methods ###
 
   private
@@ -65,4 +121,6 @@ class AppointmentCompletion < ActiveRecord::Base
       nil
     end
   end
+
+
 end

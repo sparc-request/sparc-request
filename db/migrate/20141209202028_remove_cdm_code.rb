8c636b1f94e5a92a4861074a0f4d9df3a529854e
bjk7 <brian.kelsey2@gmail.com>
2014-12-12 11:30:40 -0500

    Revising charge code migration to remove the current charge codes before merging cdm codes

--- db/migrate/20141209202028_remove_cdm_code.rb
+++ db/migrate/20141209202028_remove_cdm_code.rb
@@ -1,5 +1,11 @@
 class RemoveCdmCode < ActiveRecord::Migration
   def up
+    charge_codes = Service.where("charge_code is not null and charge_code != ''")
+
+    charge_codes.each do |service|
+      service.update_attribute(:charge_code, nil)
+    end
+
     services = Service.where("cdm_code is not null and cdm_code != ''")
 
     services.each do |service|

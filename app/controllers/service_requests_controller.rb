732a7e13521b5730ce9caa5518ab0ef66b63326a
bjk7 <brian.kelsey2@gmail.com>
2015-03-19 10:44:45 -0400

    [#89614878] styling fixes for projects add users, fixed studies showing up in projects when switching and vice versa

--- app/controllers/service_requests_controller.rb
+++ app/controllers/service_requests_controller.rb
@@ -111,10 +111,10 @@ class ServiceRequestsController < ApplicationController
   def protocol
     @service_request.update_attribute(:service_requester_id, current_user.id) if @service_request.service_requester_id.nil?
     if @sub_service_request.nil?
-      studies = current_user.project_roles.map{|pr| pr.protocol unless ['view','none'].include? pr.project_rights}.compact
+      studies = current_user.project_roles.map{|pr| pr.protocol unless ['view','none'].include?(pr.project_rights) || pr.protocol.type.eql?("Project")}.compact
     end
-    @studies = @sub_service_request.nil? ? studies : @service_request.protocol.type == "Study" ? [@service_request.protocol] : []
-    @projects = @sub_service_request.nil? ? current_user.projects(:order => 'id') : @service_request.protocol.type == "Project" ? [@service_request.protocol] : []
+    @studies = @sub_service_request.nil? ? studies : @service_request.protocol.type.eql?("Study") ? [@service_request.protocol] : []
+    @projects = @sub_service_request.nil? ? current_user.projects(:order => 'id') : @service_request.protocol.type.eql?("Project")? [@service_request.protocol] : []
     if session[:saved_protocol_id]
       @service_request.protocol = Protocol.find session[:saved_protocol_id]
       session.delete :saved_protocol_id

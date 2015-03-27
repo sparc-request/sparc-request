2afbd2249b4dfdb8e30a2011f837660e73975928
Andrew Cates <amcates@gmail.com>
2015-03-27 10:23:51 -0400

    changing how the creation of data works,  should happen in migration so that it goes out and doesn't break stuff

--- app/models/study_type_question.rb
+++ app/models/study_type_question.rb
@@ -2,5 +2,5 @@ class StudyTypeQuestion < ActiveRecord::Base
   default_scope { order('`order`') }
   has_many :study_type_answers
 
-  attr_accessible :order, :question
+  attr_accessible :order, :question, :friendly_id
 end

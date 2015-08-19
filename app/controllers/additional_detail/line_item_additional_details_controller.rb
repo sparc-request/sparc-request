fb3b22d750c70a84ad7802cf829182bb3c7a485a
rmjames <robert-james-miller@uiowa.edu>
2015-08-19 09:00:24 -0500

    working on angular js

--- app/controllers/additional_detail/line_item_additional_details_controller.rb
+++ app/controllers/additional_detail/line_item_additional_details_controller.rb
@@ -1,2 +1,16 @@
 class AdditionalDetail::LineItemAdditionalDetailsController < ApplicationController
+  layout 'additional_detail/application'
+
+  def show
+    
+  end
+
+# create a record as part of the service request flow?    
+#  def create
+#    
+#  end
+  
+  def update
+    
+  end
 end

3adb7dcaf2449520d2ab3bbb100fa76fd041e644
rmjames <robert-james-miller@uiowa.edu>
2015-08-20 15:58:08 -0500

    changed data type of form definition json

--- db/migrate/20150818145657_create_additional_details.rb
+++ db/migrate/20150818145657_create_additional_details.rb
@@ -3,7 +3,7 @@ class CreateAdditionalDetails < ActiveRecord::Migration
     create_table :additional_details do |t|
       t.string :name
       t.string :description
-      t.string :form_definition_json
+      t.text :form_definition_json
       t.date :effective_date
       t.boolean :approved
       t.references :service

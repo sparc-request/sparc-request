542e20a38140ae4b3c8b5fc97f1bea9853c16499
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-09-21 14:30:58 -0400

    Add additional models to migration

--- db/migrate/20150915155404_change_service_request_quote_status_to_get_cost_estimate.rb
+++ db/migrate/20150915155404_change_service_request_quote_status_to_get_cost_estimate.rb
@@ -1,9 +1,29 @@
 class ChangeServiceRequestQuoteStatusToGetCostEstimate < ActiveRecord::Migration
   def up
-    ServiceRequest.where(status: 'get_a_quote').update_all status: 'get_a_cost_estimate'
+    [
+      'ServiceRequest',
+      'SubServiceRequest',
+      'AvailableStatus',
+      'PastStatus'
+    ].each do |model|
+      model.
+        constantize.
+        where(status: 'get_a_quote').
+        update_all status: 'get_a_cost_estimate'
+    end
   end
 
   def down
-    ServiceRequest.where(status: 'get_a_cost_estimate').update_all status: 'get_a_quote'
+    [
+      'ServiceRequest',
+      'SubServiceRequest',
+      'AvailableStatus',
+      'PastStatus'
+    ].each do |model|
+      model.
+        constantize.
+        where(status: 'get_a_cost_estimate').
+        update_all status: 'get_a_quote'
+    end
   end
 end

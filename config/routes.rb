a44523b05a33998de64405183ef8412194f79e06
rmjames <robert-james-miller@uiowa.edu>
2015-09-30 15:48:08 -0500

    Additional Details: removed unused JS

--- config/routes.rb
+++ config/routes.rb
@@ -342,7 +342,8 @@ SparcRails::Application.routes.draw do
     resources :services, only: [:index] do
       resources :additional_details
     end
-    resources :line_item_additional_details, except: [:index, :destroy]
+    # we may add :destroy later so a service provider can allow an updated version of the form to be rendered and completed
+    resources :line_item_additional_details, only: [:show, :update] 
     resources :service_requests, only: [:show]
   end
 

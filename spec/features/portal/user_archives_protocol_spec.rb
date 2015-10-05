d68f2b538f8bcb956b3a286becebebdf914c0cd9
Jerry Hardee <hardeeje@musc.edu>
2015-10-05 15:08:11 -0400

    Fix user_archived_protocol_spec.

--- spec/features/portal/user_archives_protocol_spec.rb
+++ spec/features/portal/user_archives_protocol_spec.rb
@@ -9,16 +9,14 @@ RSpec.describe 'user archives a protocol', js: true do
 
   before :each do
     visit portal_root_path
-    wait_for_javascript_to_finish
   end
 
   describe 'clicking the archive button' do
     it "should hide the protocol" do
       protocol_id = Protocol.first.id
-      within ".protocol-information-#{protocol_id}" do
-        find('.protocol-archive-button').click
-      end
-      wait_for_javascript_to_finish
+
+      find(".protocol-archive-button[data-protocol_id='#{protocol_id}']").click
+
       expect(page).to_not have_css("#blue-provider-#{protocol_id}")
       expect(page).to_not have_css(".protocol-information-#{protocol_id}")
     end

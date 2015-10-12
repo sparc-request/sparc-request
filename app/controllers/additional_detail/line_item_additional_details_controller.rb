117088fa4d6842d3ef07b267c79b9039d09ce1eb
rmjames <robert-james-miller@uiowa.edu>
2015-10-12 13:48:01 -0700

    Additional Details: Implemented security for line_item_additional_details_controller

--- app/controllers/additional_detail/line_item_additional_details_controller.rb
+++ app/controllers/additional_detail/line_item_additional_details_controller.rb
@@ -1,7 +1,10 @@
 class AdditionalDetail::LineItemAdditionalDetailsController < ApplicationController
   protect_from_forgery
   before_filter :authenticate_identity!
-  before_filter :load_line_item_additional_detail_and_authorize_user
+  before_filter :load_line_item_additional_detail_and_protocol
+  before_filter :protocol_authorizer_view, :only => [:show]
+  before_filter :protocol_authorizer_edit, :only => [:update]
+
   
   def show
      render :json => @line_item_additional_detail
@@ -17,18 +20,27 @@ class AdditionalDetail::LineItemAdditionalDetailsController < ApplicationControl
   
   private
   
-  def load_line_item_additional_detail_and_authorize_user
+  def load_line_item_additional_detail_and_protocol
     @line_item_additional_detail = LineItemAdditionalDetail.where(id: params[:id]).first()
     if !@line_item_additional_detail 
       render :json => "", :status => :not_found
-    # verify that user is either a super user or service provider for this service; catalog managers are not allowed!
-#    elsif current_identity.admin_organizations(:su_only => false).include?(@line_item_additional_detail.line_item.service.organization) 
-#      return true
-    # next, try to verify that the user is either the original service requester or a team member on the project 
-    elsif ServiceRequest.where(:id => @line_item_additional_detail.line_item.service_request_id, :service_requester_id => current_identity.id).first
-      return true
     else
-      @line_item_additional_detail = nil
+      @protocol = @line_item_additional_detail.line_item.service_request.protocol
+    end
+  end
+  # verify that a user is a team member on the project
+  # or, a super user or service provider for this service
+  # but catalog managers are not allowed!
+  def protocol_authorizer_view
+    authorized_user = ProtocolAuthorizer.new(@protocol, current_identity)
+    if !authorized_user.can_view?
+      render :json => "", :status => :unauthorized
+    end
+  end
+
+  def protocol_authorizer_edit
+    authorized_user = ProtocolAuthorizer.new(@protocol, current_identity)
+    if !authorized_user.can_edit?
       render :json => "", :status => :unauthorized
     end
   end
--- spec/controllers/additional_detail/line_item_additional_details_controller_spec.rb
+++ spec/controllers/additional_detail/line_item_additional_details_controller_spec.rb
@@ -3,15 +3,50 @@ require 'rails_helper'
 RSpec.describe AdditionalDetail::LineItemAdditionalDetailsController do
   
   before :each do
-    @additional_detail = AdditionalDetail.new
-    @additional_detail.save(:validate => false)
+    # create a catalog hierarchy
+    @institution = Institution.new
+    @institution.type = "Institution"
+    @institution.save(validate: false)
+
+    @provider = Provider.new
+    @provider.type = "Provider"
+    @provider.parent_id = @institution.id
+    @provider.save(validate: false)
+
+    @program = Program.new
+    @program.type = "Program"
+    @program.parent_id = @provider.id
+    @program.save(validate: false)
+
+    @core = Core.new
+    @core.type = "Core"
+    @core.parent_id = @program.id
+    @core.save(validate: false)
     
+    # associate a protocol to a service request and sub service request
+    @protocol = Study.new
+    @protocol.type = 'Study'
+    @protocol.save(validate: false)
+    
+    @service_request = ServiceRequest.new
+    @service_request.protocol_id = @protocol.id
+    @service_request.save(:validate => false)
+    
+    @sub_service_request = SubServiceRequest.new
+    @sub_service_request.service_request_id = @service_request.id
+    SubServiceRequest.skip_callback(:save, :after, :update_org_tree)
+    @sub_service_request.save(validate: false)
+    SubServiceRequest.set_callback(:save, :after, :update_org_tree)
+    
+    # add a line item to the service request  
     @line_item = LineItem.new
+    @line_item.service_request_id = @service_request.id
+    @line_item.sub_service_request_id = @sub_service_request.id
     @line_item.save(:validate => false)
     
+    # add a line item additional detail to the line item
     @line_item_additional_detail = LineItemAdditionalDetail.new
     @line_item_additional_detail.line_item_id = @line_item.id
-    @line_item_additional_detail.additional_detail_id = @additional_detail.id
     @line_item_additional_detail.save(:validate => false)
   end
     
@@ -39,7 +74,7 @@ RSpec.describe AdditionalDetail::LineItemAdditionalDetailsController do
     end
 
     it 'should see 404s for an invalid line_item_additional_detail.id' do
-      get(:show, {:id => 1231231231, :format => :json})
+      get(:show, {:id => 1231231231})
       expect(response.status).to eq(404)
       expect(response.body).to eq("")
       
@@ -51,7 +86,7 @@ RSpec.describe AdditionalDetail::LineItemAdditionalDetailsController do
     
     describe 'has no affiliation with the project and, thus, has no access to' do
       it 'view a line_item_additional_detail' do
-        get(:show, {:id => @line_item_additional_detail, :format => :json})
+        get(:show, {:id => @line_item_additional_detail})
         expect(response.status).to eq(401)
         expect(response.body).to eq("")
       end
@@ -64,23 +99,17 @@ RSpec.describe AdditionalDetail::LineItemAdditionalDetailsController do
       end
     end
     
-    describe 'is the service requester and, thus, has access to' do
+    describe 'is a project team member with "approve" rights and, thus, has no access to' do
       before :each do
-        @service_request = ServiceRequest.new
-        @service_request.service_requester_id = @identity.id
-        @service_request.save(:validate => false)
-            
-        @service = Service.new
-        @service.save(:validate => false)
-    
-        # update the line item
-        @line_item.service_id = @service.id
-        @line_item.service_request_id = @service_request.id
-        @line_item.save(:validate => false)
+        @project_role = ProjectRole.new
+        @project_role.identity_id = @identity.id
+        @project_role.protocol_id = @protocol.id
+        @project_role.project_rights = 'approve'
+        @project_role.save(validate: false)
       end
       
       it 'view a line_item_additional_detail' do
-        get(:show, {:id => @line_item_additional_detail, :format => :json})
+        get(:show, {:id => @line_item_additional_detail})
         expect(response.status).to eq(200)
         expect(response.body).to eq(@line_item_additional_detail.to_json)
       end
@@ -99,22 +128,270 @@ RSpec.describe AdditionalDetail::LineItemAdditionalDetailsController do
       end
     end
     
-#    describe 'is a service provider and, thus, has access to' do
-#      it 'view a line_item_additional_detail' do
-#        get(:show, {:id => @line_item_additional_detail, :format => :json})
-#        expect(response.status).to eq(200)
-#        expect(response.body).to eq(@line_item_additional_detail.to_json) 
-#      end
-#    end
+    describe 'is a project team member with "request" rights and, thus, has no access to' do
+      before :each do
+        @project_role = ProjectRole.new
+        @project_role.identity_id = @identity.id
+        @project_role.protocol_id = @protocol.id
+        @project_role.project_rights = 'request'
+        @project_role.save(validate: false)
+      end
+      
+      it 'view a line_item_additional_detail' do
+        get(:show, {:id => @line_item_additional_detail})
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(@line_item_additional_detail.to_json)
+      end
+      
+      it 'update a line_item_additional_detail (i.e., submit/update answers to questions)' do
+        put(:update, {:id => @line_item_additional_detail, :line_item_additional_detail => { :form_data_json => "{ fake json }"} }) 
+        expect(response.status).to eq(204)
+        expect(LineItemAdditionalDetail.find(@line_item_additional_detail).form_data_json).to eq("{ fake json }")
+      end
+     
+    end 
     
-#   describe 'is a project team member and, thus, has access to' do
-#
- #     it 'view a line_item_additional_detail HTML and JSON' do
- #       get(:show, {:id => @line_item_additional_detail, :format => :json})
- #       expect(response.status).to eq(200)
- #       expect(response.body).to eq(@line_item_additional_detail.to_json)
- #     end
- #   end
-  
+    describe 'is a project team member with "view" rights and, thus, ' do
+      before :each do
+        @project_role = ProjectRole.new
+        @project_role.identity_id = @identity.id
+        @project_role.protocol_id = @protocol.id
+        @project_role.project_rights = 'view'
+        @project_role.save(validate: false)
+      end
+      
+      it 'has access to view a line_item_additional_detail' do
+        get(:show, {:id => @line_item_additional_detail})
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(@line_item_additional_detail.to_json)
+      end
+      
+      it 'does NOT have access to update a line_item_additional_detail (i.e., submit/update answers to questions)' do
+        put(:update, {:id => @line_item_additional_detail, :line_item_additional_detail => { :form_data_json => "{ fake json }"} }) 
+        expect(response.status).to eq(401)
+        expect(response.body).to eq("")
+        expect(LineItemAdditionalDetail.find(@line_item_additional_detail).form_data_json).to eq(nil)
+      end
+    end   
+    
+    describe 'is a project team member with "none" rights and, thus, ' do
+      before :each do
+        @project_role = ProjectRole.new
+        @project_role.identity_id = @identity.id
+        @project_role.protocol_id = @protocol.id
+        @project_role.project_rights = 'none'
+        @project_role.save(validate: false)
+      end
+      
+      it 'does NOT have access to view a line_item_additional_detail' do
+        get(:show, {:id => @line_item_additional_detail})
+        expect(response.status).to eq(401)
+        expect(response.body).to eq("")
+      end
+      
+      it 'does NOT have access to update a line_item_additional_detail (i.e., submit/update answers to questions)' do
+        put(:update, {:id => @line_item_additional_detail, :line_item_additional_detail => { :form_data_json => "{ fake json }"} }) 
+        expect(response.status).to eq(401)
+        expect(response.body).to eq("")
+        expect(LineItemAdditionalDetail.find(@line_item_additional_detail).form_data_json).to eq(nil)
+      end
+    end        
+          
+    describe 'is a service provider for a core service and, thus, has access to' do
+      before :each do
+        @core_service = Service.new
+        @core_service.organization_id = @core.id
+        @core_service.save(validate: false)
+                
+        @service_provider = ServiceProvider.new
+        @service_provider.identity_id = @identity.id
+        @service_provider.organization_id = @core.id
+        @service_provider.save(validate: false)
+        
+        # update the line item
+        @line_item.service_id = @core_service.id
+        @line_item.save(:validate => false)
+        
+        # update the organization
+        @sub_service_request.organization_id = @core.id
+        SubServiceRequest.skip_callback(:save, :after, :update_org_tree)
+        @sub_service_request.save(validate: false)
+        SubServiceRequest.set_callback(:save, :after, :update_org_tree)
+      end
+      
+      it 'view a line_item_additional_detail' do
+        get(:show, {:id => @line_item_additional_detail})
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(@line_item_additional_detail.to_json)
+      end
+      
+      it 'update a line_item_additional_detail (i.e., submit/update answers to questions)' do
+        put(:update, {:id => @line_item_additional_detail, :line_item_additional_detail => { :form_data_json => "{ fake json }"} }) 
+        expect(response.status).to eq(204)
+        expect(LineItemAdditionalDetail.find(@line_item_additional_detail).form_data_json).to eq("{ fake json }")
+      end
+    end
+    
+    describe 'is a service provider for a program service and, thus, has access to' do
+      before :each do
+        @program_service = Service.new
+        @program_service.organization_id = @program.id
+        @program_service.save(validate: false)
+                
+        @service_provider = ServiceProvider.new
+        @service_provider.identity_id = @identity.id
+        @service_provider.organization_id = @program.id
+        @service_provider.save(validate: false)
+        
+        # update the line item
+        @line_item.service_id = @program_service.id
+        @line_item.save(:validate => false)
+        
+        # update the organization
+        @sub_service_request.organization_id = @program.id
+        SubServiceRequest.skip_callback(:save, :after, :update_org_tree)
+        @sub_service_request.save(validate: false)
+        SubServiceRequest.set_callback(:save, :after, :update_org_tree)
+      end
+      
+      it 'view a line_item_additional_detail' do
+        get(:show, {:id => @line_item_additional_detail})
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(@line_item_additional_detail.to_json)
+      end
+      
+      it 'update a line_item_additional_detail (i.e., submit/update answers to questions)' do
+        put(:update, {:id => @line_item_additional_detail, :line_item_additional_detail => { :form_data_json => "{ fake json }"} }) 
+        expect(response.status).to eq(204)
+        expect(LineItemAdditionalDetail.find(@line_item_additional_detail).form_data_json).to eq("{ fake json }")
+      end
+    end    
+    
+    describe 'is a super user for a core service and, thus, has access to' do
+      before :each do
+        @core_service = Service.new
+        @core_service.organization_id = @core.id
+        @core_service.save(validate: false)
+        
+        @super_user = SuperUser.new
+        @super_user.identity_id = @identity.id
+        @super_user.organization_id = @core.id
+        @super_user.save(validate: false)
+        
+        # update the organization
+        @sub_service_request.organization_id = @core.id
+        SubServiceRequest.skip_callback(:save, :after, :update_org_tree)
+        @sub_service_request.save(validate: false)
+        SubServiceRequest.set_callback(:save, :after, :update_org_tree)
+      end
+      
+      it 'view a line_item_additional_detail' do
+        get(:show, {:id => @line_item_additional_detail})
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(@line_item_additional_detail.to_json)
+      end
+      
+      it 'update a line_item_additional_detail (i.e., submit/update answers to questions)' do
+        put(:update, {:id => @line_item_additional_detail, :line_item_additional_detail => { :form_data_json => "{ fake json }"} }) 
+        expect(response.status).to eq(204)
+        expect(LineItemAdditionalDetail.find(@line_item_additional_detail).form_data_json).to eq("{ fake json }")
+      end
+    end     
+
+    describe 'is a super user for a program service and, thus, has access to' do
+      before :each do
+        @program_service = Service.new
+        @program_service.organization_id = @program.id
+        @program_service.save(validate: false)
+        
+        @super_user = SuperUser.new
+        @super_user.identity_id = @identity.id
+        @super_user.organization_id = @program.id
+        @super_user.save(validate: false)
+        
+        # update the organization
+        @sub_service_request.organization_id = @program.id
+        SubServiceRequest.skip_callback(:save, :after, :update_org_tree)
+        @sub_service_request.save(validate: false)
+        SubServiceRequest.set_callback(:save, :after, :update_org_tree)
+      end
+      
+      it 'view a line_item_additional_detail' do
+        get(:show, {:id => @line_item_additional_detail})
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(@line_item_additional_detail.to_json)
+      end
+      
+      it 'update a line_item_additional_detail (i.e., submit/update answers to questions)' do
+        put(:update, {:id => @line_item_additional_detail, :line_item_additional_detail => { :form_data_json => "{ fake json }"} }) 
+        expect(response.status).to eq(204)
+        expect(LineItemAdditionalDetail.find(@line_item_additional_detail).form_data_json).to eq("{ fake json }")
+      end
+    end   
+
+    describe 'is a catalog manager for a core service and, thus, has NO access to' do
+      before :each do
+        @core_service = Service.new
+        @core_service.organization_id = @core.id
+        @core_service.save(validate: false)
+        
+        @catalog_manager = CatalogManager.new
+        @catalog_manager.identity_id = @identity.id
+        @catalog_manager.organization_id = @core.id
+        @catalog_manager.save(validate: false)
+        
+        # update the organization
+        @sub_service_request.organization_id = @core.id
+        SubServiceRequest.skip_callback(:save, :after, :update_org_tree)
+        @sub_service_request.save(validate: false)
+        SubServiceRequest.set_callback(:save, :after, :update_org_tree)
+      end
+      
+      it 'view a line_item_additional_detail' do
+        get(:show, {:id => @line_item_additional_detail})
+        expect(response.status).to eq(401)
+        expect(response.body).to eq("")
+      end
+      
+      it 'update a line_item_additional_detail (i.e., submit/update answers to questions)' do
+        put(:update, {:id => @line_item_additional_detail, :line_item_additional_detail => { :form_data_json => "{ fake json }"} }) 
+        expect(response.status).to eq(401)
+        expect(response.body).to eq("")
+        expect(LineItemAdditionalDetail.find(@line_item_additional_detail).form_data_json).to eq(nil)
+      end
+    end     
+
+    describe 'is a catalog manager for a program service and, thus, has NO access to' do
+      before :each do
+        @program_service = Service.new
+        @program_service.organization_id = @program.id
+        @program_service.save(validate: false)
+        
+        @catalog_manager = CatalogManager.new
+        @catalog_manager.identity_id = @identity.id
+        @catalog_manager.organization_id = @program.id
+        @catalog_manager.save(validate: false)
+        
+        # update the organization
+        @sub_service_request.organization_id = @program.id
+        SubServiceRequest.skip_callback(:save, :after, :update_org_tree)
+        @sub_service_request.save(validate: false)
+        SubServiceRequest.set_callback(:save, :after, :update_org_tree)
+      end
+      
+      it 'view a line_item_additional_detail' do
+        get(:show, {:id => @line_item_additional_detail})
+        expect(response.status).to eq(401)
+        expect(response.body).to eq("")
+      end
+      
+      it 'update a line_item_additional_detail (i.e., submit/update answers to questions)' do
+        put(:update, {:id => @line_item_additional_detail, :line_item_additional_detail => { :form_data_json => "{ fake json }"} }) 
+        expect(response.status).to eq(401)
+        expect(response.body).to eq("")
+        expect(LineItemAdditionalDetail.find(@line_item_additional_detail).form_data_json).to eq(nil)
+      end
+    end                  
   end
+  
 end

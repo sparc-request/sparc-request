d3e62937b656f030d4b5c481d85345ddf71ee264
rmjames <robert-james-miller@uiowa.edu>
2015-10-19 15:07:56 -0700

    Updated to try removing index by index name first

--- db/migrate/20150724142715_add_missing_unique_indices.acts_as_taggable_on_engine.rb
+++ db/migrate/20150724142715_add_missing_unique_indices.acts_as_taggable_on_engine.rb
@@ -3,10 +3,11 @@ class AddMissingUniqueIndices < ActiveRecord::Migration
   def self.up
     add_index :tags, :name, unique: true
 
-    remove_index :taggings, :tag_id if index_exists?(:taggings, :tag_id)
-    # in case the previous line does nothing, also try removing it by "index name"
+    # first, try removing by "index name" because column name and index name may not match
     remove_index(:taggings, name: "index_taggings_on_tag_id") if index_exists?(:taggings, :tag_id, name: "index_taggings_on_tag_id")
-    
+    # in case the previous line does nothing, also try removing it by "column name"
+    remove_index :taggings, :tag_id if index_exists?(:taggings, :tag_id)
+        
     remove_index :taggings, [:taggable_id, :taggable_type, :context]
     add_index :taggings,
               [:tag_id, :taggable_id, :taggable_type, :context, :tagger_id, :tagger_type],

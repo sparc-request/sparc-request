aaddb6671aee0aff0ccbaac1dad279ac6a2ddc04
Jason Leonard <jleonardw9@gmail.com>
2015-10-19 11:07:33 -0400

    merge resolution

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150915155404) do
+ActiveRecord::Schema.define(:version => 20150930181724) do
 
   create_table "admin_rates", :force => true do |t|
     t.integer  "line_item_id"

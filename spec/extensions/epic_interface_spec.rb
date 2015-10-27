1211c79e84a37245600d619e125b5d388e3cd707
Andrew Cates <amcates@gmail.com>
2015-10-27 12:15:35 -0400

    getting rid of extra spec on production branch

--- spec/extensions/epic_interface_spec.rb
+++ spec/extensions/epic_interface_spec.rb
@@ -486,33 +486,6 @@ RSpec.describe EpicInterface do
 
       node.should be_equivalent_to(expected)
     end
-    
-    it 'should emit a subjectOf for an ide number' do
-      study.investigational_products_info.update_attributes(ide_number: '12345678')
-
-      epic_interface.send_study_creation(study)
-
-      xml = <<-END
-        <subjectOf typeCode="SUBJ"
-                   xmlns='urn:hl7-org:v3'
-                   xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'>
-          <studyCharacteristic classCode="OBS" moodCode="EVN">
-            <code code="RGFT2" />
-            <value value="12345678" />
-          </studyCharacteristic>
-        </subjectOf>
-      END
-
-      expected = Nokogiri::XML(xml)
-
-      node = epic_received[0].xpath(
-          '//env:Body/rpe:RetrieveProtocolDefResponse/rpe:protocolDef/hl7:plannedStudy/hl7:subjectOf',
-          'env' => 'http://www.w3.org/2003/05/soap-envelope',
-          'rpe' => 'urn:ihe:qrph:rpe:2009',
-          'hl7' => 'urn:hl7-org:v3')
-
-      node.should be_equivalent_to(expected)
-    end
 
     it 'should emit a subjectOf for a pro number if the study has both a pro number and an hr number' do
       study.human_subjects_info.update_attributes(pro_number: '1234')

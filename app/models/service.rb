0b93f7ff8f159486bcc54d5857efcd6ad4414377
Jason Leonard <jleonardw9@gmail.com>
2015-11-04 15:30:47 -0500

    Had the rates and funding sources backwards.

--- app/models/service.rb
+++ app/models/service.rb
@@ -27,7 +27,7 @@ class Service < ActiveRecord::Base
 
   RATE_TYPES = [{:display => "Service Rate", :value => "full"}, {:display => "Federal Rate", :value => "federal"},
                 {:display => "Corporate Rate", :value => "corporate"}, {:display => "Other Rate", :value => "other"},
-                {:display => "Member Rate", :value => "member"}, {:display => "Unfunded Student Research Rate", :value => 'unfunded'}]
+                {:display => "Member Rate", :value => "member"}]
 
   belongs_to :organization, :include => [:pricing_setups]
   belongs_to :revenue_code_range

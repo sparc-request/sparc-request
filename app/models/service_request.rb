8f11fc42e0c09b80d235665c9e49df8254c34284
rmjames <robert-james-miller@uiowa.edu>
2015-11-09 15:36:34 -0800

    Additional Details: Removed unnecessary changes

--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -584,7 +584,7 @@ class ServiceRequest < ActiveRecord::Base
     line_item_audits = AuditRecovery.where("audited_changes LIKE '%service_request_id: #{self.id}%' AND
                                       auditable_type = 'LineItem' AND user_id = #{identity.id} AND action IN ('create', 'destroy') AND
                                       created_at BETWEEN '#{start_date}' AND '#{end_date}'")
-                                     .group_by(&:auditable_id)
+                                    .group_by(&:auditable_id)
 
     {:line_items => line_item_audits}
   end

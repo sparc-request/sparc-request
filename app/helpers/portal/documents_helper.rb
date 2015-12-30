50fdcc426d60bba614bdcf6cff283398aa5017df
Charlie Thiry <thiry@musc.edu>
2015-12-30 10:05:37 -0500

    convert buttons from dropdown to standalone on documents table

--- app/helpers/portal/documents_helper.rb
+++ app/helpers/portal/documents_helper.rb
@@ -4,20 +4,19 @@ module Portal::DocumentsHelper
     link_to document.document_file_name, document.document.url
   end
 
-  def display_document_actions
-    options = raw(
-      content_tag(:li, raw(
-        content_tag(:button, raw(content_tag(:span, '', class: "glyphicon glyphicon-edit", aria: {hidden: "true"}))+' Edit Document', type: 'button', class: 'btn btn-default form-control actions-button document_edit'))
-      )+
-      content_tag(:li, raw(
-        content_tag(:button, raw(content_tag(:span, '', class: "glyphicon glyphicon-remove", aria: {hidden: "true"}))+' Delete Document', type: 'button', class: 'btn btn-default form-control actions-button document_delete'))
-      )
+  def document_edit_button
+    content_tag(:button, 
+      raw(
+        content_tag(:span, '', class: "glyphicon glyphicon-edit", aria: {hidden: "true"})
+      ), type: 'button', class: 'btn btn-warning actions-button document_edit'
     )
+  end
 
-    span = raw content_tag(:span, '', class: 'glyphicon glyphicon-triangle-bottom')
-    button = raw content_tag(:button, raw(span), type: 'button', class: 'btn btn-default btn-sm dropdown-toggle form-control available-actions-button', 'data-toggle' => 'dropdown', 'aria-expanded' => 'false')
-    ul = raw content_tag(:ul, options, class: 'dropdown-menu', role: 'menu')
-
-    raw content_tag(:div, button + ul, class: 'btn-group overflow_webkit_button')
+  def document_delete_button
+    content_tag(:button, 
+      raw(
+        content_tag(:span, '', class: "glyphicon glyphicon-remove", aria: {hidden: "true"})
+      ), type: 'button', class: 'btn btn-danger actions-button document_delete'
+    )
   end
 end
--- app/views/portal/documents/index.json.jbuilder
+++ app/views/portal/documents/index.json.jbuilder
@@ -3,5 +3,6 @@ json.(@documents) do |doc|
   json.type doc.display_document_type
   json.title display_document_title(doc)
   json.uploaded format_date(doc.created_at)
-  json.actions display_document_actions
+  json.edit document_edit_button
+  json.delete document_delete_button
 end

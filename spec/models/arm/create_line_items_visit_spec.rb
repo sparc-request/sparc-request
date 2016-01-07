bb8ba92302bbed356c1ee82a9b3f7c4386d49608
Jerry Hardee <hardeeje@musc.edu>
2016-01-07 11:17:59 -0500

    Use acts_as_list methods over array indexing to work with an Arm's VisitGroups. Fix Arm model specs.

--- spec/models/arm/create_line_items_visit_spec.rb
+++ spec/models/arm/create_line_items_visit_spec.rb
@@ -14,58 +14,25 @@ RSpec.describe Arm, type: :model do
       end
     end
 
-    context 'visit_count is 0' do
-      let!(:arm) { create(:arm, visit_count: 0) }
-
-      context 'Arm has no VisitGroups' do
-        it 'should not create any VisitGroups' do
-          expect { arm.create_line_items_visit line_item }.to_not change { arm.visit_groups.count }
-        end
-      end
-
-      context 'Arm has some VisitGroups' do
-        before(:each) { create(:visit_group, arm: arm) }
-
-        it 'should not create any VisitGroups' do
-          expect { arm.create_line_items_visit line_item }.to_not change { arm.visit_groups.count }
-        end
-      end
-    end
-
-    context 'visit_count is positive' do
-      let!(:arm) { create(:arm, visit_count: 2) }
+    context 'visit_count is not nil' do
+      let!(:arm) { create(:arm, visit_count: 3) }
 
       context 'Arm has fewer VisitGroups than visit_count' do
-        before(:each) { create(:visit_group, arm: arm) }
-
-        it 'should create enough VisitGroups to match visit_count' do
-          expect { arm.create_line_items_visit line_item }.to change { arm.visit_groups.count }.from(1).to(2)
+        before(:each) do
+          arm.create_visit_group 0
+          arm.create_visit_group 0
         end
 
-        it 'should create a LineItemsVisit for LineItem with new visits' do
+        it 'should create a LineItemsVisit for LineItem with new Visits' do
+          vg_count = arm.visit_groups.count
           arm.update_attributes(subject_count: 1)
-          expect { arm.create_line_items_visit line_item }.to change { arm.line_items_visits.count }.from(0).to(1)
+          expect { arm.create_line_items_visit line_item }.to change { arm.reload.line_items_visits.count }.from(0).to(1)
           liv = arm.reload.line_items_visits.first
           expect(liv.line_item).to              eq line_item
           expect(liv.subject_count).to          eq arm.subject_count
-          expect(liv.visits.count).to           eq arm.visit_count
-          expect(liv.visits.map(&:position)).to eq (1..arm.visit_count).to_a
-        end
-      end
 
-      context 'Arm has the same number of VisitGroups as visit_count' do
-        before(:each) { create_list(:visit_group, 2, arm: arm) }
-
-        it 'should not create any VisitGroups' do
-          expect { arm.create_line_items_visit line_item }.to_not change { arm.visit_groups.count }
-        end
-      end
-
-      context 'Arm has more VisitGroups than visit_count' do
-        before(:each) { create_list(:visit_group, 3, arm: arm) }
-
-        it 'should not create any VisitGroups' do
-          expect { arm.create_line_items_visit line_item }.to_not change { arm.visit_groups.count }
+          expect(liv.visits.count).to           eq vg_count
+          expect(liv.visits.map(&:position)).to eq (0..vg_count-1).to_a
         end
       end
     end

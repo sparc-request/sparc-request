e054ab4e11e22e6fdcd1f504a2a27dd58eada860
Jason Leonard <jleonardw9@gmail.com>
2016-01-14 14:17:28 -0500

    Accidently deleted code needed to create visit groups based on the arm's visit count.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151116155206) do
+ActiveRecord::Schema.define(version: 20160108152313) do
+
+  create_table "additional_details", force: :cascade do |t|
+    t.string   "name",                 limit: 255
+    t.string   "description",          limit: 255
+    t.text     "form_definition_json", limit: 65535
+    t.date     "effective_date"
+    t.boolean  "enabled"
+    t.integer  "service_id",           limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "additional_details", ["service_id"], name: "index_additional_details_on_service_id", using: :btree
 
   create_table "admin_rates", force: :cascade do |t|
     t.integer  "line_item_id", limit: 4
@@ -183,6 +196,12 @@ ActiveRecord::Schema.define(:version => 20151116155206) do
 
   add_index "charges", ["service_id"], name: "index_charges_on_service_id", using: :btree
   add_index "charges", ["service_request_id"], name: "index_charges_on_service_request_id", using: :btree
+
+  create_table "click_counters", force: :cascade do |t|
+    t.integer  "click_count", limit: 4
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
 
   create_table "clinical_providers", force: :cascade do |t|
     t.integer  "identity_id",     limit: 4
@@ -412,6 +431,17 @@ ActiveRecord::Schema.define(:version => 20151116155206) do
 
   add_index "ip_patents_info", ["protocol_id"], name: "index_ip_patents_info_on_protocol_id", using: :btree
 
+  create_table "line_item_additional_details", force: :cascade do |t|
+    t.text     "form_data_json",       limit: 65535
+    t.integer  "line_item_id",         limit: 4
+    t.integer  "additional_detail_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "line_item_additional_details", ["additional_detail_id"], name: "index_line_item_additional_details_on_additional_detail_id", using: :btree
+  add_index "line_item_additional_details", ["line_item_id"], name: "index_line_item_additional_details_on_line_item_id", using: :btree
+
   create_table "line_items", force: :cascade do |t|
     t.integer  "service_request_id",     limit: 4
     t.integer  "sub_service_request_id", limit: 4
@@ -493,9 +523,9 @@ ActiveRecord::Schema.define(:version => 20151116155206) do
     t.string   "abbreviation", limit: 255
     t.text     "ack_language", limit: 65535
     t.boolean  "process_ssrs"
-    t.boolean  "is_available"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
+    t.boolean  "is_available",               default: true
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.datetime "deleted_at"
     t.boolean  "show_in_cwf"
   end
@@ -558,7 +588,7 @@ ActiveRecord::Schema.define(:version => 20151116155206) do
     t.date     "display_date"
     t.decimal  "other_rate",                             precision: 12, scale: 4
     t.decimal  "member_rate",                            precision: 12, scale: 4
-    t.integer  "units_per_qty_max",          limit: 4,                            default: 1
+    t.integer  "units_per_qty_max",          limit: 4,                            default: 10000
     t.string   "quantity_type",              limit: 255
     t.string   "otf_unit_type",              limit: 255,                          default: "N/A"
     t.integer  "quantity_minimum",           limit: 4,                            default: 1
@@ -582,7 +612,7 @@ ActiveRecord::Schema.define(:version => 20151116155206) do
     t.string   "internal_rate_type",     limit: 255
     t.string   "foundation_rate_type",   limit: 255
     t.datetime "deleted_at"
-    t.string   "unfunded_rate_type"
+    t.string   "unfunded_rate_type",     limit: 255
   end
 
   add_index "pricing_setups", ["organization_id"], name: "index_pricing_setups_on_organization_id", using: :btree
@@ -655,7 +685,7 @@ ActiveRecord::Schema.define(:version => 20151116155206) do
     t.datetime "recruitment_end_date"
     t.boolean  "selected_for_epic",                                                           default: false
     t.boolean  "has_cofc"
-    t.boolean  "archived",                                                            :default => false
+    t.boolean  "archived",                                                                    default: false
   end
 
   add_index "protocols", ["next_ssr_id"], name: "index_protocols_on_next_ssr_id", using: :btree
@@ -843,7 +873,7 @@ ActiveRecord::Schema.define(:version => 20151116155206) do
     t.string   "abbreviation",          limit: 255
     t.integer  "order",                 limit: 4
     t.text     "description",           limit: 65535
-    t.boolean  "is_available"
+    t.boolean  "is_available",                                                 default: true
     t.decimal  "service_center_cost",                 precision: 12, scale: 4
     t.string   "cpt_code",              limit: 255
     t.string   "charge_code",           limit: 255

f364fb6fc5813312e1bbef7a3b802acd6f97b32e
Jerry Hardee <hardeeje@musc.edu>
2016-02-09 11:06:53 -0500

    Added create new note spec for dashboard index. Also added a gem that allows running specs in parallel.

--- spec/features/dashboard/index/filters_spec.rb
+++ spec/features/dashboard/index/filters_spec.rb
@@ -20,12 +20,12 @@ RSpec.describe 'filters', js: :true do
     before(:each) do
       visit_protocols_index_page
       expect(@page).to have_protocols
-      @page.filters.archived_checkbox.click
+      @page.filter_protocol.archived_checkbox.click
     end
 
     context 'user clicks save' do
       before(:each) do
-        @page.filters.save_link.click
+        @page.filter_protocol.save_link.click
       end
 
       it 'should present a modal asking for filter name' do
@@ -40,7 +40,7 @@ RSpec.describe 'filters', js: :true do
         end
 
         it 'should create a new ProtocolFilter' do
-          expect(@page.filters.recently_saved_filters).to have_filters
+          expect(@page.filter_protocol.recently_saved_filters).to have_filters
         end
 
         it 'should apply filter, if not already' do
@@ -76,7 +76,7 @@ RSpec.describe 'filters', js: :true do
           pluck(:search_name)
 
         actual_filters = @page.
-          filters.
+          filter_protocol.
           recently_saved_filters.
           filters.
           map(&:text)
@@ -85,7 +85,7 @@ RSpec.describe 'filters', js: :true do
 
       context 'user clicks a saved filter name' do
         it 'should apply that filter' do
-          @page.filters.recently_saved_filters.filters.first.click
+          @page.filter_protocol.recently_saved_filters.filters.first.click
           wait_for_javascript_to_finish
           expect(@page).to have_protocols
         end
@@ -105,7 +105,7 @@ RSpec.describe 'filters', js: :true do
         p2 = create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: false)
         visit_protocols_index_page
 
-        filters = @page.filters
+        filters = @page.filter_protocol
         filters.search_field.set('abc')
         filters.archived_checkbox.click
         filters.select_status('draft')
@@ -116,7 +116,7 @@ RSpec.describe 'filters', js: :true do
         wait_for_javascript_to_finish
 
         expect(@page.displayed_protocol_ids).to eq [p1.id]
-        @page.filters.reset_link.click
+        @page.filter_protocol.reset_link.click
         wait_for_javascript_to_finish
         expect(@page.displayed_protocol_ids.sort).to eq [p2.id]
       end
@@ -131,7 +131,7 @@ RSpec.describe 'filters', js: :true do
         p2 = create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: false)
         visit_protocols_index_page
 
-        filters = @page.filters
+        filters = @page.filter_protocol
         filters.search_field.set('abc')
         filters.archived_checkbox.click
         filters.select_status('draft')
@@ -139,7 +139,7 @@ RSpec.describe 'filters', js: :true do
         wait_for_javascript_to_finish
 
         expect(@page.displayed_protocol_ids).to eq [p1.id]
-        @page.filters.reset_link.click
+        @page.filter_protocol.reset_link.click
         wait_for_javascript_to_finish
         expect(@page.displayed_protocol_ids.sort).to eq [p2.id]
       end
@@ -156,7 +156,7 @@ RSpec.describe 'filters', js: :true do
 
       it 'should not be checked' do
         visit_protocols_index_page
-        expect(@page.filters.archived_checkbox).to_not be_checked
+        expect(@page.filter_protocol.archived_checkbox).to_not be_checked
       end
     end
 
@@ -165,8 +165,8 @@ RSpec.describe 'filters', js: :true do
         archived_protocol = create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: true)
         create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: false)
         visit_protocols_index_page
-        @page.filters.archived_checkbox.set(true)
-        @page.filters.apply_filter_button.click
+        @page.filter_protocol.archived_checkbox.set(true)
+        @page.filter_protocol.apply_filter_button.click
         wait_for_javascript_to_finish
         expect(@page.displayed_protocol_ids).to eq [archived_protocol.id]
       end
@@ -177,11 +177,11 @@ RSpec.describe 'filters', js: :true do
         create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: true)
         unarchived_protocol = create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: false)
         visit_protocols_index_page
-        @page.filters.archived_checkbox.set(true)
-        @page.filters.apply_filter_button.click
-        expect(@page.filters.archived_checkbox).to be_checked
-        @page.filters.archived_checkbox.set(true)
-        @page.filters.apply_filter_button.click
+        @page.filter_protocol.archived_checkbox.set(true)
+        @page.filter_protocol.apply_filter_button.click
+        expect(@page.filter_protocol.archived_checkbox).to be_checked
+        @page.filter_protocol.archived_checkbox.set(true)
+        @page.filter_protocol.apply_filter_button.click
         expect(@page.displayed_protocol_ids).to eq [unarchived_protocol.id]
       end
     end
@@ -226,8 +226,8 @@ RSpec.describe 'filters', js: :true do
         visit_protocols_index_page
         expect(@page.displayed_protocol_ids.sort).to eq [p1.id, p2.id, p3.id, p4.id]
 
-        @page.filters.select_status('approved')
-        @page.filters.apply_filter_button.click
+        @page.filter_protocol.select_status('approved')
+        @page.filter_protocol.apply_filter_button.click
         wait_for_javascript_to_finish
         expect(@page.displayed_protocol_ids.sort).to eq [p2.id, p3.id]
       end
@@ -240,8 +240,8 @@ RSpec.describe 'filters', js: :true do
       protocol2 = create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: false, short_title: 'xTitle')
       create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: false, short_title: 'aaa')
       visit_protocols_index_page
-      @page.filters.search_field.set('title')
-      @page.filters.apply_filter_button.click
+      @page.filter_protocol.search_field.set('title')
+      @page.filter_protocol.apply_filter_button.click
       wait_for_javascript_to_finish
       expect(@page.displayed_protocol_ids.sort).to eq [protocol1.id, protocol2.id]
     end
@@ -251,8 +251,8 @@ RSpec.describe 'filters', js: :true do
       protocol2 = create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: false, title: 'xTitle')
       create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: false, title: 'aaa')
       visit_protocols_index_page
-      @page.filters.search_field.set('title')
-      @page.filters.apply_filter_button.click
+      @page.filter_protocol.search_field.set('title')
+      @page.filter_protocol.apply_filter_button.click
       wait_for_javascript_to_finish
       expect(@page.displayed_protocol_ids.sort).to eq [protocol1.id, protocol2.id]
     end
@@ -262,8 +262,8 @@ RSpec.describe 'filters', js: :true do
       create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: false)
       create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: false)
       visit_protocols_index_page
-      @page.filters.search_field.set(protocol1.id.to_s)
-      @page.filters.apply_filter_button.click
+      @page.filter_protocol.search_field.set(protocol1.id.to_s)
+      @page.filter_protocol.apply_filter_button.click
       wait_for_javascript_to_finish
       expect(@page.displayed_protocol_ids.sort).to eq [protocol1.id]
     end
@@ -277,8 +277,8 @@ RSpec.describe 'filters', js: :true do
       create(:project_role, protocol: protocol3, identity: create(:identity, first_name: 'name3'))
 
       visit_protocols_index_page
-      @page.filters.search_field.set('name1')
-      @page.filters.apply_filter_button.click
+      @page.filter_protocol.search_field.set('name1')
+      @page.filter_protocol.apply_filter_button.click
       wait_for_javascript_to_finish
       expect(@page.displayed_protocol_ids.sort).to eq [protocol1.id, protocol2.id]
     end
@@ -292,8 +292,8 @@ RSpec.describe 'filters', js: :true do
       create(:project_role, protocol: protocol3, identity: create(:identity, last_name: 'name3'))
 
       visit_protocols_index_page
-      @page.filters.search_field.set('name1')
-      @page.filters.apply_filter_button.click
+      @page.filter_protocol.search_field.set('name1')
+      @page.filter_protocol.apply_filter_button.click
       wait_for_javascript_to_finish
       expect(@page.displayed_protocol_ids.sort).to eq [protocol1.id, protocol2.id]
     end
@@ -305,12 +305,12 @@ RSpec.describe 'filters', js: :true do
 
       it 'should show the My Protocols checkbox' do
         visit_protocols_index_page
-        expect(@page.filters).to have_my_protocols_checkbox
+        expect(@page.filter_protocol).to have_my_protocols_checkbox
       end
 
       it 'should be checked' do
         visit_protocols_index_page
-        expect(@page.filters.my_protocols_checkbox).to be_checked
+        expect(@page.filter_protocol.my_protocols_checkbox).to be_checked
       end
 
       context 'user unchecks My Protocols and clicks the filter button' do
@@ -318,8 +318,8 @@ RSpec.describe 'filters', js: :true do
           protocol1 = create(:protocol_federally_funded, :without_validations, primary_pi: create(:identity), type: 'Project', archived: false)
           protocol2 = create(:protocol_federally_funded, :without_validations, primary_pi: jug2, type: 'Project', archived: false)
           visit_protocols_index_page
-          @page.filters.my_protocols_checkbox.click
-          @page.filters.apply_filter_button.click
+          @page.filter_protocol.my_protocols_checkbox.click
+          @page.filter_protocol.apply_filter_button.click
           wait_for_javascript_to_finish
           expect(@page.displayed_protocol_ids.sort).to eq [protocol1.id, protocol2.id]
         end
@@ -350,7 +350,7 @@ RSpec.describe 'filters', js: :true do
     context 'user is not both a service provider and a superuser for any Organization' do
       it 'should not show the My Protocols checkbox' do
         visit_protocols_index_page
-        expect(@page.filters).to have_no_my_protocols_checkbox
+        expect(@page.filter_protocol).to have_no_my_protocols_checkbox
       end
     end
 
@@ -393,7 +393,7 @@ RSpec.describe 'filters', js: :true do
 
       it 'should not be checked' do
         visit_protocols_index_page
-        expect(@page.filters.my_admin_organizations_checkbox).to_not be_checked
+        expect(@page.filter_protocol.my_admin_organizations_checkbox).to_not be_checked
       end
     end
 
@@ -408,8 +408,8 @@ RSpec.describe 'filters', js: :true do
         create(:service_request_without_validations, protocol: p2, organizations: [create(:organization)])
 
         visit_protocols_index_page
-        @page.filters.my_admin_organizations_checkbox.click
-        @page.filters.apply_filter_button.click
+        @page.filter_protocol.my_admin_organizations_checkbox.click
+        @page.filter_protocol.apply_filter_button.click
         wait_for_javascript_to_finish
         expect(@page.displayed_protocol_ids.sort).to eq [p1.id]
       end
@@ -426,11 +426,11 @@ RSpec.describe 'filters', js: :true do
         create(:service_request_without_validations, protocol: p2, organizations: [create(:organization)])
 
         visit_protocols_index_page
-        @page.filters.my_admin_organizations_checkbox.click
-        @page.filters.apply_filter_button.click
+        @page.filter_protocol.my_admin_organizations_checkbox.click
+        @page.filter_protocol.apply_filter_button.click
         wait_for_javascript_to_finish
-        @page.filters.my_admin_organizations_checkbox.click
-        @page.filters.apply_filter_button.click
+        @page.filter_protocol.my_admin_organizations_checkbox.click
+        @page.filter_protocol.apply_filter_button.click
         wait_for_javascript_to_finish
 
         expect(@page.displayed_protocol_ids.sort).to eq [p1.id, p2.id]
@@ -443,14 +443,14 @@ RSpec.describe 'filters', js: :true do
 
         it 'should show the My Admin Organizations checkbox' do
           visit_protocols_index_page
-          expect(@page.filters).to have_my_admin_organizations_checkbox
+          expect(@page.filter_protocol).to have_my_admin_organizations_checkbox
         end
       end
 
       context 'user is not both a service provider and a superuser for any Organization' do
         it 'should not show the My Admin Organizations checkbox' do
           visit_protocols_index_page
-          expect(@page.filters).to have_no_my_admin_organizations_checkbox
+          expect(@page.filter_protocol).to have_no_my_admin_organizations_checkbox
         end
       end
     end
@@ -480,7 +480,7 @@ RSpec.describe 'filters', js: :true do
         create(:service_request_without_validations, protocol: p1, organizations: [org1])
 
         visit_protocols_index_page
-        expect(@page.filters.selected_core).to eq '- Any -'
+        expect(@page.filter_protocol.selected_core).to eq '- Any -'
       end
     end
 
@@ -498,8 +498,8 @@ RSpec.describe 'filters', js: :true do
         create(:service_request_without_validations, protocol: p3, organizations: [create(:organization)])
 
         visit_protocols_index_page
-        @page.filters.select_core(org1.name)
-        @page.filters.apply_filter_button.click
+        @page.filter_protocol.select_core(org1.name)
+        @page.filter_protocol.apply_filter_button.click
         wait_for_javascript_to_finish
 
         expect(@page.displayed_protocol_ids.sort).to eq [p1.id, p2.id]
@@ -520,12 +520,12 @@ RSpec.describe 'filters', js: :true do
         create(:service_request_without_validations, protocol: p3, organizations: [create(:organization)])
 
         visit_protocols_index_page
-        @page.filters.select_core(org1.name)
-        @page.filters.apply_filter_button.click
+        @page.filter_protocol.select_core(org1.name)
+        @page.filter_protocol.apply_filter_button.click
         wait_for_javascript_to_finish
-        @page.filters.core_select.click
-        @page.filters.core_options.first.click
-        @page.filters.apply_filter_button.click
+        @page.filter_protocol.core_select.click
+        @page.filter_protocol.core_options.first.click
+        @page.filter_protocol.apply_filter_button.click
         wait_for_javascript_to_finish
 
         expect(@page.displayed_protocol_ids.sort).to eq [p1.id, p2.id, p3.id]
@@ -538,14 +538,14 @@ RSpec.describe 'filters', js: :true do
 
         it 'should show the My Admin Organizations checkbox' do
           visit_protocols_index_page
-          expect(@page.filters).to have_core_select
+          expect(@page.filter_protocol).to have_core_select
         end
       end
 
       context 'user is not both a service provider and a superuser for any Organization' do
         it 'should not show the My Admin Organizations checkbox' do
           visit_protocols_index_page
-          expect(@page.filters).to have_no_core_select
+          expect(@page.filter_protocol).to have_no_core_select
         end
       end
     end
--- spec/support/pages/dashboard/protocols/index_page.rb
+++ spec/support/pages/dashboard/protocols/index_page.rb
@@ -12,18 +12,18 @@ module Dashboard
         new_protocol_options.select { |opt| opt.text == "New #{protocol_type}" }.first.click
       end
 
-      section :filters, '#filterrific_form' do
+      section :filter_protocol, '#filterrific_form' do
+        element :save_link, '#save_filters_link'
+        element :reset_link, '#reset_filters_link'
         element :search_field, '#filterrific_search_query'
         element :archived_checkbox, '#filterrific_show_archived'
-        element :apply_filter_button, '#apply-filter-button'
         element :status_select, 'div.status-select button'
         elements :status_options, 'div.status-select li'
         element :my_protocols_checkbox, '#filterrific_for_identity_id'
         element :my_admin_organizations_checkbox, '#filterrific_for_admin'
         element :core_select, 'div.core-select button'
         elements :core_options, 'div.core-select li'
-        element :reset_link, '#reset_filters_link'
-        element :save_link, '#save_filters_link'
+        element :apply_filter_button, '#apply-filter-button'
 
         def select_status(status)
           status_select.click
@@ -72,6 +72,7 @@ module Dashboard
         element :title, '.modal-header h4'
         sections :service_requests, '.panel.service-request-info' do
           element :header, '.panel-heading .panel-title'
+          element :notes_button, 'button.notes.list'
           sections :sub_service_requests, 'tbody tr' do
             element :pretty_ssr_id, 'td.pretty-ssr-id'
             element :organization, 'td.organization'
@@ -90,6 +91,20 @@ module Dashboard
       section :filter_form_modal, 'form.new_protocol_filter' do
         element :name_field, '#protocol_filter_search_name'
         element :save_button, 'input.btn[value="Save"]'
+      end
+
+      section :notes_modal, '#notes-modal' do
+        element :new_note_button, 'button.note.new'
+        sections :notes, '.detail' do
+          element :name, '.name'
+          element :created_at, '.created-at'
+          element :comment, '.comment'
+        end
+      end
+
+      section :new_note_modal, '#new-note-modal' do
+        element :input_field, 'textarea'
+        element :add_note_button, 'input[value="Add Note"]'
       end
     end
   end

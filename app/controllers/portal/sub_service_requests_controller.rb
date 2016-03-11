1832b3492133c3f02128e47b4e36957b72353ae9
Johanna Wiel <jwiel86@gmail.com>
2016-03-11 10:58:15 -0500

    comments added

--- app/controllers/portal/sub_service_requests_controller.rb
+++ app/controllers/portal/sub_service_requests_controller.rb
@@ -86,6 +86,8 @@ class Portal::SubServiceRequestsController < Portal::BaseController
     if @protocol.update_attributes(attrs)
       redirect_to portal_admin_sub_service_request_path(@sub_service_request)
     else
+      # @user_toasts set to an empty array for Wenjun's sanity until bootstrap is merged in
+      # @user_toasts = @user.received_toast_messages.select {|x| x.sending_class == 'SubServiceRequest'}
       @user_toasts = []
       @service_request = @sub_service_request.service_request
       @protocol.populate_for_edit if @protocol.type == "Study"

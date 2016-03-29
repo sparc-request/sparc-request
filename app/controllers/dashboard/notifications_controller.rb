44a788a5de459a5a3dc41cc93e9702688e85abfc
Kyle Glick <kyleglick99@gmail.com>
2016-03-29 09:44:41 -0400

    changes

--- app/controllers/dashboard/notifications_controller.rb
+++ app/controllers/dashboard/notifications_controller.rb
@@ -52,7 +52,12 @@ class Dashboard::NotificationsController < Dashboard::BaseController
         @notification = Notification.new
       end
 
-        @message = @notification.messages.new(to: params[:identity_id])
+      if Identity.find(params[:identity_id]) == current_user
+        @notification.errors.add(:notifications, "can't be sent to yourself.")
+        @errors = @notification.errors
+      end
+
+      @message = @notification.messages.new(to: params[:identity_id])
     end
   end
 

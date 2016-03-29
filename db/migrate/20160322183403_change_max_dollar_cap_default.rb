b9615c6be547d86b9efcf954f357fb8c9dda0c26
Johanna Wiel <jwiel86@gmail.com>
2016-03-29 13:34:23 -0400

    migrated all max_percentages with nil to 0

--- db/migrate/20160322183403_change_max_dollar_cap_default.rb
+++
@@ -1,5 +0,0 @@
-class ChangeMaxDollarCapDefault < ActiveRecord::Migration
-  def change
-    change_column :subsidy_maps, :max_dollar_cap, :decimal, :default => 0.00, :precision => 12, :scale => 4
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322183511) do
+ActiveRecord::Schema.define(version: 20160329154930) do
 
   create_table "admin_rates", force: :cascade do |t|
     t.integer  "line_item_id", limit: 4
@@ -983,7 +983,7 @@ ActiveRecord::Schema.define(version: 20160322183511) do
   create_table "subsidy_maps", force: :cascade do |t|
     t.integer  "organization_id", limit: 4
     t.decimal  "max_dollar_cap",            precision: 12, scale: 4, default: 0.0
-    t.decimal  "max_percentage",            precision: 5,  scale: 2
+    t.decimal  "max_percentage",            precision: 5,  scale: 2, default: 0.0
     t.datetime "created_at",                                                       null: false
     t.datetime "updated_at",                                                       null: false
     t.datetime "deleted_at"

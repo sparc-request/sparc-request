a1106e8fa5f68b2f341887c76bfe4a0e88079ce9
Jerry Hardee <hardeeje@musc.edu>
2016-04-05 10:09:33 -0400

    Progress commit for notifications/index_spec refactor

--- spec/support/pages/dashboard/notifications/index_page.rb
+++ spec/support/pages/dashboard/notifications/index_page.rb
@@ -25,8 +25,12 @@ module Dashboard
       end
 
       section :send_notification_modal, '.modal', text: "Send Notification" do
-        element :select_user, :field, "Select User:"
-        elements :search_results, "tt-selectable"
+        # element :select_user, :field, "Select User:"        -- label not associated correctly
+        element :select_user, "input[placeholder='Search for a User']"
+        elements :search_results, ".tt-selectable"
+        element :subject_line, "input[placeholder='Subject']"
+        element :message_box, "textarea[placeholder='Please enter message text here...']"
+        element :send_button, "button", text: "Send"
       end
     end
   end

418a03843564f246d522859e6bb77381774e0f4e
shanhong cheng <shanhong.cheng@utah.edu>
2016-04-15 10:05:57 -0600

    selected for epic validator only make sense is use epic is true

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -110,7 +110,7 @@ class Protocol < ActiveRecord::Base
     validate  :validate_funding_source
     validates :sponsor_name, :presence => true, :if => :is_study?
     validates_associated :human_subjects_info, :message => "must contain 8 numerical digits", :if => :validate_nct
-    validates :selected_for_epic, inclusion: [true, false], :if => :is_study?
+    validates :selected_for_epic, inclusion: [true, false], :if => [:is_study?, USE_EPIC]
     validate  :validate_study_type_answers, if: [:is_study?, :selected_for_epic?, "StudyTypeQuestionGroup.active.pluck(:id).first == study_type_question_group_id"]
   end
 

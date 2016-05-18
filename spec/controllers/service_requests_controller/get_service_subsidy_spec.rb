54c5c1501e9b8d605ae249984594567ee7d237f4
Stuart Johnson <work.stuart.johnson@gmail.com>
2016-05-18 11:29:01 -0400

    fixing subsidy specs in service request controller

--- spec/controllers/service_requests_controller/get_service_subsidy_spec.rb
+++ spec/controllers/service_requests_controller/get_service_subsidy_spec.rb
@@ -19,8 +19,12 @@ RSpec.describe ServiceRequestsController do
         get :service_subsidy, id: service_request.id
       end
 
-      it 'should set subsidies to an empty array if there are no sub service requests' do
-        expect(assigns(:subsidies)).to eq [ ]
+      it 'should set has_subsidies to false if there are no sub service requests' do
+        expect(assigns(:has_subsidy)).to eq false
+      end
+
+      it 'should set eligible for subsidy to false if there are no sub service requests' do
+        expect(assigns(:eligible_for_subsidy)).to eq false
       end
 
       it 'should redirect to document_management' do
@@ -32,8 +36,8 @@ RSpec.describe ServiceRequestsController do
 
       before(:each) { get :service_subsidy, id: service_request.id }
 
-      it 'should put the Subsidy into @subsidies' do
-        expect(assigns(:subsidies)).to eq [ subsidy ]
+      it 'has subsidy should return true' do
+        expect(assigns(:has_subsidy)).to eq true
       end
 
       it 'should responsd with status 200' do
@@ -41,24 +45,11 @@ RSpec.describe ServiceRequestsController do
       end
     end
 
-    context 'ServiceRequest has Subsidies but not current SubServiceRequest' do
-
-      let!(:core2)            { create(:core, parent_id: program.id) }
-      let!(:ssr_no_subsidies) { create(:sub_service_request, service_request: service_request, organization: core2) }
+    context 'SubServiceRequest does not have a subsidy but is eligible for one' do
 
       before(:each) do
-        session[:sub_service_request_id] = ssr_no_subsidies.id
-        get :service_subsidy, id: service_request.id
-      end
-
-      it 'should redirect to document_management' do
-        expect(response).to redirect_to "/service_requests/#{service_request.id}/document_management"
-      end
-    end
-
-    context 'SubServiceRequest does not have a Study and is eligible for one' do
-
-      before(:each) do
+        sub_service_request.subsidies.destroy_all
+        sub_service_request.reload
         sub_service_request.organization.subsidy_map.update_attributes(
           max_dollar_cap: 100,
           max_percentage: 100)
@@ -66,54 +57,28 @@ RSpec.describe ServiceRequestsController do
         get :service_subsidy, id: service_request.id
       end
 
-      it 'should create a new Subsidy and put it into @subsidies' do
-        expect(assigns(:subsidies).map { |s| s.class}).to eq [ Subsidy ]
+      it 'has subsidy should return false' do
+        expect(assigns(:has_subsidy)).to eq false
+      end
+
+      it 'eligible for subsidy should return true' do
+        expect(assigns(:eligible_for_subsidy)).to eq true
       end
     end
 
-    context 'with subsidy maps' do
-
-      let!(:core_subsidy_map)     { create(:subsidy_map, organization_id: core.id) }
-      let!(:provider_subsidy_map) { create(:subsidy_map, organization_id: provider.id) }
-      let!(:program_subsidy_map)  { subsidy_map }
-
+    context 'with sub service request' do
       context 'SubServiceRequest does not have a subsidy and is not eligible for one' do
 
         before(:each) do
-          # destroy the subsidy; we want to ensure that #service_subsidy
-          # doesn't create a subsidy
-          sub_service_request.subsidy.destroy
-
-          core.build_subsidy_map
-          provider.build_subsidy_map
-          program.build_subsidy_map
-
-          core.subsidy_map.update_attributes!(
-            max_dollar_cap: 0,
-            max_percentage: 0)
-          provider.subsidy_map.update_attributes!(
-            max_dollar_cap: 0,
-            max_percentage: 0)
-          program.subsidy_map.update_attributes!(
-            max_dollar_cap: 0,
-            max_percentage: 0)
-
-          # make sure before we start the test that the ssr is not
-          # eligible for subsidy
+          subsidy.destroy
+          subsidy_map.destroy
+          # make sure before we start the test that the ssr is not eligible for subsidy
           expect(sub_service_request.eligible_for_subsidy?).to eq false
 
           # call service_subsidy
           get :service_subsidy, id: service_request.id
 
           sub_service_request.reload
-        end
-
-        it 'should not create a new subsidy' do
-          # Now the ssr should not have a subsidy
-          subsidy = sub_service_request.subsidy
-          expect(subsidy).to eq nil
-
-          expect(assigns(:subsidies)).to eq [ ]
         end
 
         it 'should redirect to document_management' do

25f5c7b0e18cf1a8d7e921d185122f5f32decd4f
Ying Wu <ying.wu@pbrc.edu>
2016-05-24 10:46:45 -0500

    moved nav_links from constant.yml to application.yml; checking if admin/fulfillment is blank? in config

--- config/initializers/obis_setup.rb
+++ config/initializers/obis_setup.rb
@@ -68,6 +68,7 @@ begin
   CREATE_AN_ACCOUNT                         = application_config['create_an_account']
   ABOUT_SPARC_URL                           = application_config['about_sparc_url'] || nil
   USE_FEEDBACK_LINK                         = application_config['use_feedback_link'] || false
+  NAV_LINKS                                 = application_config['nav_links']
 
   if LOCALE_OVERRIDE
     I18n.available_locales = [:en, LOCALE_OVERRIDE]
@@ -124,7 +125,6 @@ begin
   AUDIT_ACTIONS                  = config['audit_actions']
   ALERT_TYPES                    = config['alert_types']
   ALERT_STATUSES                 = config['alert_statuses']
-  NAV_LINKS                      = config['nav_links']
 rescue
   raise "constants.yml not found"
 end

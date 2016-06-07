92b90b678c50be9dd60020a3eb576b2ae986272a
Andrew Cates <amcates@gmail.com>
2016-06-07 16:27:27 -0400

    small spec fix

--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -487,7 +487,7 @@ class ServiceRequest < ActiveRecord::Base
     self.assign_attributes(status: new_status)
 
     self.sub_service_requests.each do |ssr|
-      next if ssr.can_be_edited?
+      next unless ssr.can_be_edited?
       ssr.update_attribute(:status, new_status)
     end
 

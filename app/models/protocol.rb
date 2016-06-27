6ef535c20bef7aaa5ba9df718c1b94f2945bbc30
Andrew Cates <amcates@gmail.com>
2016-06-27 11:02:31 -0400

    fixnum search needs to be converted to string

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -141,7 +141,7 @@ class Protocol < ActiveRecord::Base
     # Protects against SQL Injection with ActiveRecord::Base::sanitize
 
     # inserts ! so that we can escape special characters
-    escaped_search_term = search_term.gsub(/[!%_]/) { |x| '!' + x }
+    escaped_search_term = search_term.to_s.gsub(/[!%_]/) { |x| '!' + x }
 
     like_search_term = ActiveRecord::Base::sanitize("%#{escaped_search_term}%")
     exact_search_term = ActiveRecord::Base::sanitize(search_term)

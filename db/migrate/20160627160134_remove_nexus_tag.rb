f9cb6f0b77c41728943127fddbda07a264719998
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2016-06-30 15:26:17 -0400

    Fixed migration

--- db/migrate/20160627160134_remove_nexus_tag.rb
+++ db/migrate/20160627160134_remove_nexus_tag.rb
@@ -1,3 +1,9 @@
+class Tag < ActiveRecord::Base
+  audited
+  attr_accessible :name
+  has_many :taggings, dependent: :destroy, class_name: '::ActsAsTaggableOn::Tagging'
+end
+
 class RemoveNexusTag < ActiveRecord::Migration
   def change
   	Tag.where(name: 'ctrc_clinical_services').destroy_all

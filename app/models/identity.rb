4f90a79edf7202f71c2097490e5789d1b45a4d59
Kyle Glick <glickk@musc.edu>
2016-07-13 11:13:54 -0400

    removing unnecessary code from method [#122763601]

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -242,11 +242,7 @@ class Identity < ActiveRecord::Base
   def has_correct_project_role? request
     protocol = request.class == ServiceRequest ? request.protocol : request.service_request.protocol
 
-    if protocol.nil?
-      return false
-    else
-      protocol.project_roles.where(identity_id: self.id, project_rights: ['approve', 'request']).any?
-    end
+    protocol.project_roles.where(identity_id: self.id, project_rights: ['approve', 'request']).any?
   end
 
   # Determines whether this identity can edit a given organization's information in CatalogManager.

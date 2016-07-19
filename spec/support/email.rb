daaf820a063ac4954611c27714e3c6c7d8b9f279
Johanna Wiel <jwiel86@gmail.com>
2016-07-19 16:27:38 -0400

    fixed, deleted, and added specs [#126234829]

--- spec/support/email.rb
+++ spec/support/email.rb
@@ -10,27 +10,32 @@ module EmailHelpers
     expect(mail).to have_xpath "//th[text()='Funding Source']/following-sibling::td[text()='#{service_request.protocol.funding_source.capitalize}']"
   end
 
-  def assert_email_project_roles
+  def assert_email_user_information
     #assert correct project roles information is included in notification email
     expect(mail).to have_xpath "//table//strong[text()='User Information']"
     expect(mail).to have_xpath "//th[text()='User Name']/following-sibling::th[text()='Contact Information']/following-sibling::th[text()='Role']"
     service_request.protocol.project_roles.each do |role|
-      expect(mail).to have_xpath "//td[text()='#{role.identity.full_name}']/following-sibling::td[text()='#{role.identity.email}']/following-sibling::td[text()='PRIMARY-PI (Requester)']"
+      if identity.id == role.identity.id
+        requester_flag = " (Requester)"
+      else
+        requester_flag = ""
+      end
+      expect(mail).to have_xpath "//td[text()='#{role.identity.full_name}']/following-sibling::td[text()='#{role.identity.email}']/following-sibling::td[text()='#{role.role.upcase}#{requester_flag}']"
     end
   end
 
-  def assert_email_admin_information
-    #assert correct admin information is included in notification email
-    expect(mail).to have_xpath "//table//strong[text()='Admin Information']"
-    expect(mail).to have_xpath "//th[text()='Current Identity:']/following-sibling::td[text()='#{identity.id}']"
-    expect(mail).to have_xpath "//th[text()='Service Request ID:']/following-sibling::td[text()='#{service_request.id}']"
-    expect(mail).to have_xpath "//th[text()='Sub Service Request IDs:']/following-sibling::td[text()='#{service_request.sub_service_requests.map{ |ssr| ssr.id }.join(", ")}']"
+  def assert_email_srid_information
+    expect(mail).to have_xpath "//th[text()='SRID']/following-sibling::th[text()='Organization']/following-sibling::th[text()='Status']"
+    service_request.protocol.sub_service_requests.each do |ssr|
+      status = AVAILABLE_STATUSES[ssr.status]
+      expect(mail).to have_xpath "//td[text()='#{ssr.display_id}']/following-sibling::td[text()='#{ssr.org_tree_display}']/following-sibling::td[text()='#{status}']"
+    end
   end
 
   def assert_notification_email_tables
     assert_email_project_information
-    assert_email_project_roles
-    assert_email_admin_information
+    assert_email_user_information
+    assert_email_srid_information
   end
 end
 

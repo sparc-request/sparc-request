f44a1eb9efdd8aa60a2a7c7ea7191198078b2c7f
Jason Leonard <jleonardw9@gmail.com>
2016-07-27 10:22:47 -0400

    Updating past status' new completed_by column if it has an audit

--- db/migrate/20160726180819_populate_past_status_changed_by.rb
+++ db/migrate/20160726180819_populate_past_status_changed_by.rb
@@ -4,7 +4,8 @@ class PopulatePastStatusChangedBy < ActiveRecord::Migration
       PastStatus.all.each do |status|
         audit = AuditRecovery.where(auditable_id: status.id).where(auditable_type: 'PastStatus').first
         unless audit.nil?
-          changed_by = Identity.find(audit[:user_id])
+          changed_by = Identity.find(audit[:user_id]).full_name unless audit[:user_id].nil?
+          status.update_attributes(changed_by: changed_by)
         end
       end
     end

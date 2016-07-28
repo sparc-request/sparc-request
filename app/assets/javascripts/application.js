627a6f5283fbb5d530473a86b01176a744d6e1c0
Kyle Glick <glickk@musc.edu>
2016-07-28 10:01:25 -0400

    placing jquery files back in until next commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,6 +40,8 @@
 //= require jquery_nested_form
 //= require jquery.cookie
 //= require jquery.form
+//= require jquery-ui
+//= require jquery.qtip.min
 //= require editable/bootstrap-editable
 //= require editable/rails
 //= require nprogress

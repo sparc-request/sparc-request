fc9f0be4afa8f3dc46fad9e6964b57df35c0fa47
William Holt <holtw@musc.edu>
2016-07-29 09:55:29 -0400

    added tests for model validation

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   it { is_expected.to belong_to(:questionnaire) }
+
+  it { is_expected.to validate_presence_of(:content) }
+
+  it { is_expected.to validate_presence_of(:type) }
+
+  it { is_expected.to validate_presence_of(:required) }
 end

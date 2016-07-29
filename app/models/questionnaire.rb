671ea496ba696b56a8cab00e667478ad4c03cc18
William Holt <holtw@musc.edu>
2016-07-29 09:51:39 -0400

    added validation for form name attribute field

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,6 +1,7 @@
 class Questionnaire < ActiveRecord::Base
   belongs_to :service
-  has_many :items
+  has_many :items, dependent: :destroy
+  validates :name, presence: true
 
   accepts_nested_attributes_for :items
 end
--- spec/models/questionnaire_spec.rb
+++ spec/models/questionnaire_spec.rb
@@ -6,4 +6,6 @@ RSpec.describe Questionnaire, type: :model do
   it { is_expected.to have_many(:items) }
 
   it { is_expected.to accept_nested_attributes_for(:items) }
+
+  it { is_expected.to validate_presence_of(:name) }
 end

28fc8382ee57c428e50d80fea10d1447ccffafe5
William Holt <holtw@musc.edu>
2016-08-03 10:06:01 -0400

    fixed issue when destroying questionnaire

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :questionnaire
-  has_many :questionnaire_responses
+  has_many :questionnaire_responses, dependent: :destroy
   validates :content, :item_type, :required, presence: true
 end

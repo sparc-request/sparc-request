5651a780ce3d30e98df41366e04593a0959551f8
Jason Leonard <jleonardw9@gmail.com>
2016-08-03 09:03:42 -0400

    Adding validation in dashboard that prevents users from entering a subsidy of 0.

--- app/controllers/dashboard/subsidies_controller.rb
+++ app/controllers/dashboard/subsidies_controller.rb
@@ -31,7 +31,7 @@ class Dashboard::SubsidiesController < Dashboard::BaseController
     format_percent_subsidy_param
     @subsidy = PendingSubsidy.new(params[:pending_subsidy].except(:pi_contribution))
     admin_param = params[:admin]
-    if admin_param == true
+    if (admin_param == 'true') && (params[:pending_subsidy][:percent_subsidy] != 0)
       @subsidy.save(validate: false)
       perform_subsidy_creation(admin_param)
     else
@@ -55,7 +55,7 @@ class Dashboard::SubsidiesController < Dashboard::BaseController
     @sub_service_request = @subsidy.sub_service_request
     admin_param = params[:admin]
     format_percent_subsidy_param
-    if admin_param == true
+    if (admin_param == 'true') && (params[:pending_subsidy][:percent_subsidy] != 0)
       @subsidy.assign_attributes(params[:pending_subsidy].except(:pi_contribution))
       @subsidy.save(validate: false)
       perform_subsidy_update(admin_param)
@@ -106,8 +106,8 @@ class Dashboard::SubsidiesController < Dashboard::BaseController
   def perform_subsidy_update(admin_param=false)
     @admin = admin_param == 'true'
     flash[:success] = t(:dashboard)[:subsidies][:updated]
-    unless @admin
-      redirect_to dashboard_sub_service_request_path(@sub_service_request, format: :js)
-    end
+    # unless @admin
+    #   redirect_to dashboard_sub_service_request_path(@sub_service_request, format: :js)
+    # end
   end
 end

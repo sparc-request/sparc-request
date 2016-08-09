639005f930fdc69d5060fb52771f976374d62de3
Jason Leonard <jleonardw9@gmail.com>
2016-08-09 13:40:21 -0400

    Updating the status must come first.

--- app/controllers/service_requests_controller.rb
+++ app/controllers/service_requests_controller.rb
@@ -660,11 +660,9 @@ class ServiceRequestsController < ApplicationController
       service_request.previous_submitted_at = @service_request.submitted_at
       service_request.update_attribute(:submitted_at, Time.now)
     end
-    
+    to_notify = service_request.update_status(status)
     service_request.sub_service_requests.each {|ssr| ssr.update_past_status(current_user)}
 
-    to_notify = service_request.update_status(status)
-    
     to_notify
   end
 

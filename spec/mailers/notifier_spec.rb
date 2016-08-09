f98e0a226c8d9f7fafa9364950a39eaca2d5cfbe
Johanna Wiel <jwiel86@gmail.com>
2016-08-09 07:43:21 -0400

    improved the admin SR Information table spec

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -268,10 +268,12 @@ RSpec.describe Notifier do
       it 'should display the SR Information' do
         expect(mail.body.parts.first.body).to have_xpath "//table//strong[text()='Service Request Information']"
         expect(mail.body.parts.first.body).to have_xpath "//th[text()='SRID']/following-sibling::th[text()='Organization']/following-sibling::th[text()='Status']"
-        # Service Provider is only associated with one of the two SSR's
-        status = AVAILABLE_STATUSES[service_request.protocol.sub_service_requests.first.status]
-        expect(mail.body.parts.first.body).to have_xpath "//td//a[@href='/dashboard/sub_service_requests/#{service_request.protocol.sub_service_requests.first.id}']['#{service_request.protocol.sub_service_requests.first.display_id}']/@href"
-        expect(mail.body.parts.first.body).to have_xpath "//td[text()='#{service_request.protocol.sub_service_requests.first.org_tree_display}']/following-sibling::td[text()='#{status}']"
+
+        service_request.protocol.sub_service_requests.each do |ssr|
+          status = AVAILABLE_STATUSES[ssr.status]
+          expect(mail.body.parts.first.body).to have_xpath "//td//a[@href='/dashboard/sub_service_requests/#{ssr.id}']['#{ssr.display_id}']/@href"
+          expect(mail.body.parts.first.body).to have_xpath "//td[text()='#{ssr.org_tree_display}']/following-sibling::td[text()='#{status}']"
+        end        
       end
 
       it 'should not have Arm information table' do

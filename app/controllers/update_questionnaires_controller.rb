8018c58bb209b5943cc8641366d524e574a64964
William Holt <holtw@musc.edu>
2016-08-12 09:06:51 -0400

    user can now disable/enable questionnaire

--- app/controllers/update_questionnaires_controller.rb
+++ app/controllers/update_questionnaires_controller.rb
@@ -3,11 +3,21 @@ class UpdateQuestionnairesController < ApplicationController
   def update
     @service = Service.find(params[:service_id])
     @questionnaire = Questionnaire.find(params[:id])
-    @questionnaire.update_attribute(:active, true)
+    update_questionnaire(@questionnaire)
     if @questionnaire.save
       redirect_to service_questionnaires_path(@service)
     else
       redirect_to :back
+    end
+  end
+
+  private
+
+  def update_questionnaire(questionnaire)
+    if questionnaire.active?
+      questionnaire.update_attribute(:active, false)
+    else
+      questionnaire.update_attribute(:active, true)
     end
   end
 end

32cf2bb45cf8bf9a22f3e40a7ed4a63913e26a66
William Holt <holtw@musc.edu>
2016-08-17 11:30:04 -0400

    removed validation for required checkbox

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
   belongs_to :questionnaire
   has_many :questionnaire_responses, dependent: :destroy
   has_many :item_options, dependent: :destroy
-  validates :content, :item_type, :required, presence: true
+  validates :content, :item_type, presence: true
 
   accepts_nested_attributes_for :item_options, allow_destroy: true
 end

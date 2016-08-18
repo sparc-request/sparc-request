dfc0c8bc4ece24b04a157bba81a2dd52948cc80a
William Holt <holtw@musc.edu>
2016-08-18 09:40:56 -0400

    added model tests testing associations and validation

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Submission, type: :model do
   it { is_expected.to belong_to(:service) }
 
   it { is_expected.to belong_to(:identity) }
+
+  it { is_expected.to have_many(:questionnaire_responses) }
+
+  it { is_expected.to accept_nested_attributes_for(:questionnaire_responses) }
 end

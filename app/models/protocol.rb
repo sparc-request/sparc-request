374cc6787ec6030d274d3fa4502d737ea522dc8f
Jason Leonard <jleonardw9@gmail.com>
2016-09-13 14:13:20 -0400

    Merge pull request #647 from sparc-request/ac-jh-jw-move-service
    Ac jh jw move service

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -33,6 +33,7 @@ class Protocol < ActiveRecord::Base
   has_many :project_roles,                dependent: :destroy
   has_many :identities,                   through: :project_roles
   has_many :service_requests
+  has_many :services,                     through: :service_requests
   has_many :sub_service_requests,         through: :service_requests
   has_many :organizations,                through: :sub_service_requests
   has_many :affiliations,                 dependent: :destroy

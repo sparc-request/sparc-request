04ebf8c6d0b0c30e8d2d380624e4ca0d982a6456
Johanna Wiel <jwiel86@gmail.com>
2016-09-13 08:27:44 -0400

    renamed variables per Kyle

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -198,27 +198,27 @@ class Protocol < ActiveRecord::Base
     where(archived: boolean)
   }
 
-  scope :with_status, -> (status) {
-    # returns protocols with ssrs in status
-    status = status.split.flatten.reject(&:blank?)
-    return nil if status.empty?
+  scope :with_status, -> (statuses) {
+    # returns protocols with ssrs in statuses
+    statuses = statuses.split.flatten.reject(&:blank?)
+    return nil if statuses.empty?
     joins(:sub_service_requests).
-      where(sub_service_requests: { status: status }).distinct
+      where(sub_service_requests: { status: statuses }).distinct
   }
 
-  scope :with_organization, -> (org_id) {
-    # returns protocols with ssrs in org_id
-    org_id = org_id.split.flatten.reject(&:blank?)
-    return nil if org_id.empty?
+  scope :with_organization, -> (org_ids) {
+    # returns protocols with ssrs in org_ids
+    org_ids = org_ids.split.flatten.reject(&:blank?)
+    return nil if org_ids.empty?
     joins(:sub_service_requests).
-      where(sub_service_requests: { organization_id: org_id }).distinct
+      where(sub_service_requests: { organization_id: org_ids }).distinct
   }
 
-  scope :with_owner, -> (owner_id) {
-    owner_id = owner_id.split.flatten.reject(&:blank?)
-    return nil if owner_id.empty?
+  scope :with_owner, -> (owner_ids) {
+    owner_ids = owner_ids.split.flatten.reject(&:blank?)
+    return nil if owner_ids.empty?
     joins(:sub_service_requests).
-    where(sub_service_requests: {owner_id: owner_id}).
+    where(sub_service_requests: {owner_id: owner_ids}).
       where.not(sub_service_requests: {status: 'first_draft'})
   }
 

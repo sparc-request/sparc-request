c81972ebad1cf3067137bc0aea7fbd7e9f695784
Jerry Hardee <hardeeje@musc.edu>
2016-09-20 16:42:53 -0400

    Starting UI changes [#130561041]
    Dashboard edit finished.
    
    Corrected error message. Finished "View Study Details"
    
    Step 1
    
    Remove unused markup.
    
    Removed console.log.
    
    Rake task should generate a report of Protocols that had an IDE#.
    
    Fix epic_interface_spec.
    
    Move some strings to en.yml
    
    Fixed Step 1 styling problem, I think.
    
    Travis does not like tmp file creation. Probably should not be in a migration anyways.
    
    Changed error messages some

--- spec/factories/investigational_product.rb
+++ spec/factories/investigational_product.rb
@@ -23,7 +23,8 @@ FactoryGirl.define do
   factory :investigational_products_info do
     ind_number  { Random.rand(20000).to_s }
     ind_on_hold { false }
-    ide_number  { Random.rand(20000).to_s }
+    exemption_type "ide"
+    inv_device_number  { Random.rand(20000).to_s }
 
     trait :ind_on_hold do
       ind_on_hold true

6468cf6913604e0819b913ec32fd404d1e1f962c
Johanna Wiel <jwiel86@gmail.com>
2016-09-26 07:54:39 -0400

    fixed epic logic and wrote specs

--- spec/support/user_mailer_emails/tables.rb
+++ spec/support/user_mailer_emails/tables.rb
@@ -9,7 +9,7 @@ module EmailHelpers
     expect(@mail).to have_xpath "//th[text()='Funding Source']/following-sibling::td[text()='#{study.funding_source.capitalize}']"
   end
 
-  def user_information_table
+  def user_information_table_with_epic_col
     expect(@mail).to have_xpath "//table//strong[text()='User Information']"
     expect(@mail).to have_xpath "//th[text()='User Modification']/following-sibling::th[text()='Contact Information']/following-sibling::th[text()='Role']/following-sibling::th[text()='SPARC Proxy Rights']/following-sibling::th[text()='Epic Access']"
     expect(@mail).to have_xpath "//td[text()='#{@modified_identity.full_name}']/following-sibling::td[text()='#{@modified_identity.email}']/following-sibling::td[text()='#{@modified_identity.project_roles.first.role.upcase}']/following-sibling::td[text()='#{@modified_identity.project_roles.first.display_rights}']"
@@ -17,6 +17,19 @@ module EmailHelpers
       expect(@mail).to have_xpath "//td[text()='No']"
     else
       expect(@mail).to have_xpath "//td[text()='Yes']"
+    end
+  end
+
+  def user_information_table_without_epic_col
+    expect(@mail).to have_xpath "//table//strong[text()='User Information']"
+    expect(@mail).to have_xpath "//th[text()='User Modification']/following-sibling::th[text()='Contact Information']/following-sibling::th[text()='Role']/following-sibling::th[text()='SPARC Proxy Rights']"
+    expect(@mail).not_to have_xpath "//following-sibling::th[text()='Epic Access']"
+    expect(@mail).to have_xpath "//td[text()='#{@modified_identity.full_name}']/following-sibling::td[text()='#{@modified_identity.email}']/following-sibling::td[text()='#{@modified_identity.project_roles.first.role.upcase}']/following-sibling::td[text()='#{@modified_identity.project_roles.first.display_rights}']"
+
+    if @modified_identity.project_roles.first.epic_access == false
+      expect(@mail).not_to have_xpath "//td[text()='No']"
+    else
+      expect(@mail).not_to have_xpath "//td[text()='Yes']"
     end
   end
 end

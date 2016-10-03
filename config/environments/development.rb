34406989fbf93cd25173d4e06b65b96464b53ef4
Jerry Hardee <hardeeje@musc.edu>
2016-10-03 10:06:23 -0400

    Add -d hostname to mail config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ SparcRails::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'sparc-d.obis.musc.edu' }
 
   config.log_level = :debug
 

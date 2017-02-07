7fdf601521071458de96ee775d6735bf4f75dc9c
Jason Leonard <jleonardw9@gmail.com>
2017-02-07 08:51:13 -0600

    First pass at testing additional details in sparc proper.

--- spec/features/additional_details/sparc_proper/user_answers_questionnaire_spec.rb
+++ spec/features/additional_details/sparc_proper/user_answers_questionnaire_spec.rb
@@ -1,24 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe 'User creates an additional details questionnaire', js: true do
+RSpec.describe 'User answers an additional details questionnaire', js: true do
   let_there_be_lane
-  scenario 'successfully' do
-    service = create(:service, :with_ctrc_organization)
-    visit new_service_additional_details_questionnaire_path(service)
-    fill_in 'questionnaire_name', with: 'New Questionnaire'
-    fill_in 'questionnaire_items_attributes_0_content', with: 'What is your favorite color?'
-    select 'Radio Button', from: 'questionnaire_items_attributes_0_item_type'
-    fill_in 'questionnaire_items_attributes_0_item_options_attributes_0_content', with: 'Green'
-    click_link 'Add another Option'
-    fill_in 'questionnaire_items_attributes_0_item_options_attributes_1_content', with: 'Red'
+  fake_login_for_each_test
 
-    check 'questionnaire_items_attributes_0_required'
+  before :each do
+    institution   = create(:institution, name: "Institution")
+    provider      = create(:provider, name: "Provider", parent: institution)
+    program       = create(:program, name: "Program", parent: provider, process_ssrs: true)
+    service       = create(:service_with_pricing_map, name: 'Brain Removal')
+    @protocol     = create(:protocol_federally_funded, type: 'Study', primary_pi: jug2)
+    @sr           = create(:service_request_without_validations, status: 'first_draft', protocol: @protocol)
+    ssr           = create(:sub_service_request_without_validations, service_request: @sr, organization: program, status: 'first_draft')
+                    create(:line_item, service_request: @sr, sub_service_request: ssr, service: service)
+                    create(:arm, protocol: @protocol, visit_count: 1)
+    questionnaire = create(:questionnaire, service: service, name: 'Does your brain hurt?')
+    item          = create(:item, questionnaire: questionnaire, content: 'How is your brain? Does it hurt and need to be removed?',
+                           item_type: 'yes_no')
+    visit document_management_service_request_path(@sr)
+    wait_for_javascript_to_finish
+  end
 
-    click_button 'Create Questionnaire'
+  describe 'user visits the documents page' do
 
-    expect(current_path).to eq service_additional_details_questionnaires_path(service)
-    expect(Questionnaire.count).to eq 1
-    expect(Item.count).to eq 1
-    expect(ItemOption.count).to eq 2
+    it 'and should see the questionnaire' do
+      within '.document-management-submissions' do
+        expect(page).to have_content('Brain Removal')
+      end
+    end   
   end
 end

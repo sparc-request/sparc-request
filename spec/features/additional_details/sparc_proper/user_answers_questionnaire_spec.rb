1b3b857f71a57620d5f8b7d630897e822f985597
Jason Leonard <jleonardw9@gmail.com>
2017-03-03 11:53:50 -0600

    Testing for phone validation

--- spec/features/additional_details/sparc_proper/user_answers_questionnaire_spec.rb
+++ spec/features/additional_details/sparc_proper/user_answers_questionnaire_spec.rb
@@ -52,5 +52,12 @@ RSpec.describe 'User has a questionnaire to complete', js: true do
       click_link 'Create Submission'
       expect(page).to have_content('Error')
     end
+
+    it 'should validate phone format' do
+      find('#submission_questionnaire_responses_attributes_1_content').set('This is a not a phone number')
+      sleep 1
+      click_link 'Create Submission'
+      expect(page).to have_content('Error')
+    end
   end
 end

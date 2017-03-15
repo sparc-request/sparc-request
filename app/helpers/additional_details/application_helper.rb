fb31334a8bb54ae221086b5e13e1da5aa6dabbcc
Jason Leonard <jleonardw9@gmail.com>
2017-03-15 11:02:06 -0500

    Cleanup

--- app/helpers/additional_details/application_helper.rb
+++ app/helpers/additional_details/application_helper.rb
@@ -3,10 +3,7 @@ module AdditionalDetails::ApplicationHelper
   def convert_time(time)
     modifier = ''
     time.first == '0' ? modifier = ' AM' : modifier = ' PM'
-    # if time.first == '0'
-    #   modifier = 'AM'
-    # else
-    #   modifier = 'PM'
+
     DateTime.strptime(time.gsub(':', ''), '%H%M').strftime('%l:%M') + modifier
   end
 end

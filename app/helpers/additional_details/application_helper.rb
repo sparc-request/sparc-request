0af5194e92a598f274fff61b19198edd33881a71
Jason Leonard <jleonardw9@gmail.com>
2017-03-15 11:01:29 -0500

    Adding in the am/pm.

--- app/helpers/additional_details/application_helper.rb
+++ app/helpers/additional_details/application_helper.rb
@@ -1,6 +1,12 @@
 module AdditionalDetails::ApplicationHelper
 
   def convert_time(time)
-    DateTime.strptime(time.gsub(':', ''), '%H%M').strftime('%l:%M')
+    modifier = ''
+    time.first == '0' ? modifier = ' AM' : modifier = ' PM'
+    # if time.first == '0'
+    #   modifier = 'AM'
+    # else
+    #   modifier = 'PM'
+    DateTime.strptime(time.gsub(':', ''), '%H%M').strftime('%l:%M') + modifier
   end
 end

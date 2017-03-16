124bd924b9e345938a4c7c6b886a93490f8bbfd5
Stuart Johnson <work.stuart.johnson@gmail.com>
2017-03-16 12:36:08 -0400

    adding selectpicker option for report fields [#141814483]

--- app/reports/survey_responses.rb
+++ app/reports/survey_responses.rb
@@ -31,7 +31,7 @@ class SurveyResponseReport < ReportingModule
   def default_options
     {
       "Date Range" => {:field_type => :date_range, :for => "completed_at", :from => "2012-03-01".to_date, :to => Date.today},
-      Survey => {:field_type => :select_tag, :custom_name_method => :title, :required => true}
+      Survey => {:field_type => :select_tag, :custom_name_method => :title, :required => true, :selectpicker => true}
     }
   end
 

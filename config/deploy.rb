f87c8f72b0bea04e299f784eef78f3a2e0f63e52
William Holt <holtw@musc.edu>
2017-03-17 11:53:03 -0400

    wth - Updating Capistrano to V3
    Update Capistrano from version 2 to version 3. [#141813969]

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,20 @@
-# Copyright © 2011-2016 MUSC Foundation for Research Development
-# All rights reserved.
+# config valid only for current version of Capistrano
+lock "3.8.0"
 
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
-# disclaimer in the documentation and/or other materials provided with the distribution.
-
-# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products
-# derived from this software without specific prior written permission.
-
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
-# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-set :bundle_without, [:development, :test]
-
-set :application, "sparc-rails"
-set :repository,  "git@github.com:bmic-development/sparc-request.git"
-set :deploy_root, "/var/www/rails"
-set :days_to_keep_backups, 30
-
+set :application, "sparc_rails"
+set :repo_url, "git@github.com:bmic-development/sparc-request.git"
 set :scm, :git
-set :deploy_via, :remote_cache
-set :keep_releases, 5
-set :user, "capistrano"
+set :user, 'capistrano'
 set :use_sudo, false
-ssh_options[:forward_agent] = true
 
-set :stages, %w(testing demo demo2 staging production testing_rails_4)
-set :default_stage, "testing"
+set :stages, %w(testing demo demo2 staging production)
+set :default_stage, 'testing'
 
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.ruby-version', '.ruby-gemset')
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets')
 
 after "deploy:update_code", "db:symlink"
 
@@ -48,11 +27,6 @@ namespace :deploy do
   desc "starts the app"
   task :start, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
-  end
-
-  desc "does nothing"
-  task :stop, :roles => :app do
-    #nothing
   end
 end
 
@@ -156,5 +130,3 @@ before "deploy:migrate", 'mysql:backup'
 before "deploy", 'mysql:backup'
 after "mysql:backup", "mysql:cleanup_backups"
 
-require 'capistrano/ext/multistage'
-require 'bundler/capistrano'

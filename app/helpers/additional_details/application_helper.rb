8a903a4f7ab46ba2fdbbf16debbc136e0d32b85b
Jason Leonard <jleonardw9@gmail.com>
2017-03-17 11:07:39 -0500

    Forgot to account for 10 and 11 AM.

--- app/helpers/additional_details/application_helper.rb
+++ app/helpers/additional_details/application_helper.rb
@@ -2,7 +2,7 @@ module AdditionalDetails::ApplicationHelper
 
   def convert_time(time)
     modifier = ''
-    time.first == '0' ? modifier = ' AM' : modifier = ' PM'
+    ((time.first == '0') || (time[0..1] == '10') || (time[0..1] == '11')) ? modifier = ' AM' : modifier = ' PM'
 
     DateTime.strptime(time.gsub(':', ''), '%H%M').strftime('%l:%M') + modifier
   end

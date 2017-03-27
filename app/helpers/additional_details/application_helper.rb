343f10fd0206a7010bdc3b264653a69884d616cd
Jason Leonard <jleonardw9@gmail.com>
2017-03-27 10:07:07 -0500

    Using built in tool for dealing with am/pm for converting military time.

--- app/helpers/additional_details/application_helper.rb
+++ app/helpers/additional_details/application_helper.rb
@@ -1,9 +1,6 @@
 module AdditionalDetails::ApplicationHelper
 
   def convert_time(time)
-    modifier = ''
-    ((time.first == '0') || (time[0..1] == '10') || (time[0..1] == '11')) ? modifier = ' AM' : modifier = ' PM'
-
-    DateTime.strptime(time.gsub(':', ''), '%H%M').strftime('%l:%M') + modifier
+    DateTime.strptime(time, '%H:%M').strftime('%l:%M %p')
   end
 end

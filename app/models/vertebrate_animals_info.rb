5f9090f99e3ef78c962c04fa96384f5defda6f63
William Holt <holtw@musc.edu>
2017-03-29 11:23:45 -0400

    wth -Add in Rails 5 features (Models inherit from ApplicationRecord)
    Converted models to inherit from activerecord. Why are we doing this?
    I'll let this blog explain in detail.
    (http://blog.bigbinary.com/2015/12/28/application-record-in-rails-5.html). [#142698527]

--- app/models/vertebrate_animals_info.rb
+++ app/models/vertebrate_animals_info.rb
@@ -18,7 +18,7 @@
 # INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-class VertebrateAnimalsInfo < ActiveRecord::Base
+class VertebrateAnimalsInfo < ApplicationRecord
   audited
 
   self.table_name = 'vertebrate_animals_info'

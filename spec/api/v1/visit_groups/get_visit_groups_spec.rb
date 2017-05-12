8a08a342cd63799d96aee2736bf6a6e822599b6f
Jason Leonard <jleonardw9@gmail.com>
2017-05-12 08:44:57 -0500

    Removing arm edited callback from specs.

--- spec/api/v1/visit_groups/get_visit_groups_spec.rb
+++ spec/api/v1/visit_groups/get_visit_groups_spec.rb
@@ -25,14 +25,10 @@ RSpec.describe 'SPARCCWF::APIv1', type: :request do
   describe 'GET /v1/visit_groups.json' do
 
     before do
-      VisitGroup.skip_callback(:save, :after, :set_arm_edited_flag_on_subjects)
-
       5.times do
         visit_group = build(:visit_group)
         visit_group.save validate: false
       end
-      
-      VisitGroup.set_callback(:save, :after, :set_arm_edited_flag_on_subjects)
     end
 
 

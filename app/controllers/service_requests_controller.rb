24537288d87d5496f63378a47278c783cda38639
Jason Leonard <jleonardw9@gmail.com>
2017-05-25 13:16:22 -0500

    Needed to check the finished status array to determine whether to add service.

--- app/controllers/service_requests_controller.rb
+++ app/controllers/service_requests_controller.rb
@@ -212,7 +212,7 @@ class ServiceRequestsController < ApplicationController
   end
 
   def add_service
-    existing_service_ids = @service_request.line_items.reject{ |line_item| line_item.status == 'complete' }.map(&:service_id)
+    existing_service_ids = @service_request.line_items.reject{ |line_item| FINISHED_STATUSES.include?(line_item.status) }.map(&:service_id)
 
     if existing_service_ids.include?( params[:service_id].to_i )
       @duplicate_service = true

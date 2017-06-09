e9fbc70b87b04732989f10a36cbb78d94c19e62c
Jason Leonard <jleonardw9@gmail.com>
2017-06-09 13:45:54 -0500

    Fixing typo

--- spec/features/dashboard/admin_edit/admin_interacts_with_template_tab_spec.rb
+++ spec/features/dashboard/admin_edit/admin_interacts_with_template_tab_spec.rb
@@ -112,7 +112,7 @@ RSpec.describe 'User interacts with Template tab', js: true do
       visit dashboard_sub_service_request_path(ssr)
       wait_for_javascript_to_finish
 
-      click_link 'Clnical Services'
+      click_link 'Clinical Services'
       click_link 'Template Tab'
       wait_for_javascript_to_finish
 

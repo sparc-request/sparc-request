f4e55a89351fba9c5925a16f33eec14a5b957648
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2017-06-15 13:20:48 -0400

    More spec work

--- spec/controllers/dashboard/associated_users/put_update_spec.rb
+++ spec/controllers/dashboard/associated_users/put_update_spec.rb
@@ -75,11 +75,7 @@ RSpec.describe Dashboard::AssociatedUsersController do
 
       it 'should update @protocol_role using params[:project_role] using ProtocolUpdater' do
         expect(AssociatedUserUpdater).to have_received(:new).
-<<<<<<< HEAD
-          with id: @project_role.id.to_s, project_role: controller_params( { identity_id: '1' } )
-=======
           with controller_params(id: @project_role.id.to_s, project_role: {identity_id: '1'}).to_unsafe_h
->>>>>>> jtm finished some controller specs
       end
 
       it 'should not set @errors' do

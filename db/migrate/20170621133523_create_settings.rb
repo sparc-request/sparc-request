15875282ca54fbc2444ca1678b27dbceaa32c061
Jason Leonard <jleonardw9@gmail.com>
2017-06-21 09:54:51 -0400

    Creating the settings table.

---
+++ db/migrate/20170621133523_create_settings.rb
@@ -0,0 +1,14 @@
+class CreateSettings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :settings do |t|
+      t.string :key
+      t.text :value
+      t.string :type
+      t.string :friendly_name
+      t.string :description
+      t.integer :group
+      t.string :version
+      t.timestamps null: false 
+    end
+  end
+end

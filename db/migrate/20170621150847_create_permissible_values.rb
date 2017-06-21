3ea1b050c45717e6a777a65bb52e09ae3ed9a669
Andrew Cates <amcates@gmail.com>
2017-06-21 11:49:55 -0400

    changing migration [##3615063]

--- db/migrate/20170621150847_create_permissible_values.rb
+++ db/migrate/20170621150847_create_permissible_values.rb
@@ -6,7 +6,7 @@ class CreatePermissibleValues < ActiveRecord::Migration[5.0]
       t.string :concept_code
       t.integer :parent_id
       t.integer :sortorder
-      t.string :type
+      t.string :category
       t.boolean :default
       t.boolean :reserved
 

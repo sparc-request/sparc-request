7bd0264004d043615230bf29a7f2a758bae7c594
Samantha Word <samword245@gmail.com>
2017-06-21 14:36:57 -0400

    Added methods to get a value/array of values [#147633265]

--- app/models/permissible_value.rb
+++ app/models/permissible_value.rb
@@ -1,2 +1,12 @@
 class PermissibleValue < ApplicationRecord
+
+  # Get the first PermissibleValue value using a category and key
+  def self.get_value(category, key)
+    PermissibleValue.where(category: category, key: key).first.try(:value)
+  end
+
+  # Get an array of PermissibleValue values with the given category
+  def self.get_value_list(category)
+    PermissibleValue.where(category: category).pluck(:value)
+  end
 end

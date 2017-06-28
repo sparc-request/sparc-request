8dd6833da44e781879c428a93bf2ebe4a0748e14
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2017-06-28 15:58:25 -0400

    Leaked file change

--- spec/models/study/display_answers_spec.rb
+++ spec/models/study/display_answers_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe 'Protocol' do
     it 'should return appropriate answers' do
       STUDY_TYPE_ANSWERS_VERSION_3.each do |ans|
         update_answers(3, ans.last)
-        expect(study.display_answers.compact.map(&:answer)).to eq(ans.last)
+        expect(study.display_answers.map(&:answer)).to eq(ans.last)
       end 
     end
   end

e97c605e5e4c0f7c70974450d476bf3f6e69fa6f
William Holt <holtw@musc.edu>
2017-07-12 16:02:26 -0400

    adding specs

--- spec/features/dashboard/protocols/edit/user_edits_epic_answers_spec.rb
+++ spec/features/dashboard/protocols/edit/user_edits_epic_answers_spec.rb
@@ -26,70 +26,269 @@ RSpec.describe 'User edits epic answers', js: true do
   build_study_type_question_groups
   build_study_type_questions
 
-  context 'User is an admin' do
-    before :each do
-      @protocol       = create(:protocol_without_validations,
-                                type: "Study",
-                                primary_pi: jug2,
-                                funding_status: "funded",
-                                funding_source: "foundation")
-      organization    = create(:organization)
-      service_request = create(:service_request_without_validations,
-                               protocol: @protocol)
-                        create(:sub_service_request_without_validations,
-                               organization: organization,
-                               service_request: service_request,
-                               status: 'draft')
-                        create(:super_user, identity: jug2,
-                               organization: organization)
+  before :each do
+    @protocol       = create(:protocol_without_validations,
+                              type: "Study",
+                              primary_pi: jug2,
+                              funding_status: "funded",
+                              funding_source: "foundation")
+    organization    = create(:organization)
+    service_request = create(:service_request_without_validations,
+                              protocol: @protocol)
+                      create(:sub_service_request_without_validations,
+                              organization: organization,
+                              service_request: service_request,
+                              status: 'draft')
+                      create(:super_user, identity: jug2,
+                              organization: organization)
+  end
+
+  context 'use epic = true' do
+
+    scenario 'Study, selected for epic: false, question group 3' do
+      @protocol.update_attribute(:selected_for_epic, false)
+      @protocol.update_attribute(:study_type_question_group_id, 3)
       visit edit_dashboard_protocol_path(@protocol)
       wait_for_javascript_to_finish
-    end
 
-    it 'and sees edit button next to Publish Study in Epic' do
-      expect(page).to have_css(
-        'a.btn.btn-warning.edit-answers',
-        text: 'Edit'
-      )
-    end
-
-    it 'should have a cancel edit button' do
-      find('.edit-answers').click
-
-      expect(page).to have_css(
-        'a.btn.btn-danger.cancel-edit',
-        text: 'Cancel Edit'
-      )
-    end
-
-    scenario 'when edit button is clicked, change answers that give study type 1' do
-      find('a.btn.btn-warning.edit-answers').click
+      find('.edit-answers', match: :first).click
       wait_for_javascript_to_finish
-
       find('#study_selected_for_epic_true_button').click
       wait_for_javascript_to_finish
-
       bootstrap_select '#study_type_answer_certificate_of_conf_answer', 'Yes'
       wait_for_javascript_to_finish
-
-      expect(page).to have_content('De-identified  Research  Participant')
-    end
-
-    scenario 'study type 11' do
-      find('a.btn.btn-warning.edit-answers').click
+      click_button 'Save'
       wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
 
-      find('#study_selected_for_epic_true_button').click
+      within '#study_type_answer_certificate_of_conf' do
+        expect(page).to have_css('div.col-lg-4', text: 'Yes')
+        expect(page).to have_css('a.edit-answers')
+      end
+      find('a.edit-answers', match: :first).click
       wait_for_javascript_to_finish
-
       bootstrap_select '#study_type_answer_certificate_of_conf_answer', 'No'
+      wait_for_javascript_to_finish
       bootstrap_select '#study_type_answer_higher_level_of_privacy_answer', 'No'
+      wait_for_javascript_to_finish
       bootstrap_select '#study_type_answer_epic_inbasket_answer', 'No'
+      wait_for_javascript_to_finish
       bootstrap_select '#study_type_answer_research_active_answer', 'No'
+      wait_for_javascript_to_finish
       bootstrap_select '#study_type_answer_restrict_sending_answer', 'No'
       wait_for_javascript_to_finish
+      click_button 'Save'
+      wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
 
-      expect(page).to have_content('Full Epic  Functionality: no  notification, no  pink  header, no  MyChart access.')
+
+      within '#study_type_answer_certificate_of_conf' do
+        expect(page).to have_css('div.col-lg-4', text: 'No')
+        expect(page).to have_css('a.edit-answers')
+      end
+    end
+
+    scenario 'Study, selected for epic: true, question group 3' do
+      @protocol.update_attribute(:selected_for_epic, true)
+      @protocol.update_attribute(:study_type_question_group_id, 3)
+      visit edit_dashboard_protocol_path(@protocol)
+      wait_for_javascript_to_finish
+
+      find('.edit-answers', match: :first).click
+      wait_for_javascript_to_finish
+      find('#study_selected_for_epic_true_button').click
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_certificate_of_conf_answer', 'Yes'
+      wait_for_javascript_to_finish
+      click_button 'Save'
+      wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
+
+      within '#study_type_answer_certificate_of_conf' do
+        expect(page).to have_css('div.col-lg-4', text: 'Yes')
+        expect(page).to have_css('a.edit-answers')
+      end
+
+      find('a.edit-answers', match: :first).click
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_certificate_of_conf_answer', 'No'
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_higher_level_of_privacy_answer', 'No'
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_epic_inbasket_answer', 'No'
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_research_active_answer', 'No'
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_restrict_sending_answer', 'No'
+      wait_for_javascript_to_finish
+      click_button 'Save'
+      wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
+
+
+      within '#study_type_answer_certificate_of_conf' do
+        expect(page).to have_css('div.col-lg-4', text: 'No')
+        expect(page).to have_css('a.edit-answers')
+      end
+    end
+
+    scenario 'Study, selected for epic: true, question group 2' do
+      @protocol.update_attribute(:selected_for_epic, true)
+      @protocol.update_attribute(:study_type_question_group_id, 2)
+      visit edit_dashboard_protocol_path(@protocol)
+      wait_for_javascript_to_finish
+
+      find('.edit-answers', match: :first).click
+      wait_for_javascript_to_finish
+      find('#study_selected_for_epic_true_button').click
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_certificate_of_conf_answer', 'Yes'
+      wait_for_javascript_to_finish
+      click_button 'Save'
+      wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
+
+      within '#study_type_answer_certificate_of_conf' do
+        expect(page).to have_css('div.col-lg-4', text: 'Yes')
+        expect(page).to have_css('a.edit-answers')
+      end
+
+      find('a.edit-answers', match: :first).click
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_certificate_of_conf_answer', 'No'
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_higher_level_of_privacy_answer', 'No'
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_epic_inbasket_answer', 'No'
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_research_active_answer', 'No'
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_restrict_sending_answer', 'No'
+      wait_for_javascript_to_finish
+      click_button 'Save'
+      wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
+
+
+      within '#study_type_answer_certificate_of_conf' do
+        expect(page).to have_css('div.col-lg-4', text: 'No')
+        expect(page).to have_css('a.edit-answers')
+      end
+    end
+  end
+
+  context 'use epic = false' do
+
+    before(:each) do
+      stub_const('USE_EPIC', false)
+    end
+
+    scenario 'Study, selected for epic: false, question group 3' do
+      @protocol.update_attribute(:selected_for_epic, false)
+      @protocol.update_attribute(:study_type_question_group_id, 3)
+      visit edit_dashboard_protocol_path(@protocol)
+      wait_for_javascript_to_finish
+
+      find('.edit-answers', match: :first).click
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_certificate_of_conf_no_epic_answer', 'Yes'
+      wait_for_javascript_to_finish
+      click_button 'Save'
+      wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
+
+      within '#study_type_answer_certificate_of_conf_no_epic' do
+        expect(page).to have_css('div.col-lg-4', text: 'Yes')
+        expect(page).to have_css('a.edit-answers')
+      end
+
+      find('a.edit-answers', match: :first).click
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_certificate_of_conf_no_epic_answer', 'No'
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_higher_level_of_privacy_no_epic_answer', 'No'
+      wait_for_javascript_to_finish
+      click_button 'Save'
+      wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
+
+
+      within '#study_type_answer_certificate_of_conf_no_epic' do
+        expect(page).to have_css('div.col-lg-4', text: 'No')
+        expect(page).to have_css('a.edit-answers')
+      end
+    end
+
+    scenario 'Study, selected for epic: true, question group 3' do
+      @protocol.update_attribute(:selected_for_epic, true)
+      @protocol.update_attribute(:study_type_question_group_id, 3)
+      visit edit_dashboard_protocol_path(@protocol)
+      wait_for_javascript_to_finish
+
+      find('.edit-answers', match: :first).click
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_certificate_of_conf_no_epic_answer', 'Yes'
+      wait_for_javascript_to_finish
+      click_button 'Save'
+      wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
+
+      within '#study_type_answer_certificate_of_conf_no_epic' do
+        expect(page).to have_css('div.col-lg-4', text: 'Yes')
+        expect(page).to have_css('a.edit-answers')
+      end
+
+      find('a.edit-answers', match: :first).click
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_certificate_of_conf_no_epic_answer', 'No'
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_higher_level_of_privacy_no_epic_answer', 'No'
+      wait_for_javascript_to_finish
+      click_button 'Save'
+      wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
+
+
+      within '#study_type_answer_certificate_of_conf_no_epic' do
+        expect(page).to have_css('div.col-lg-4', text: 'No')
+        expect(page).to have_css('a.edit-answers')
+      end
+    end
+
+    scenario 'Study, selected for epic: true, question group 2' do
+      @protocol.update_attribute(:selected_for_epic, true)
+      @protocol.update_attribute(:study_type_question_group_id, 2)
+      visit edit_dashboard_protocol_path(@protocol)
+      wait_for_javascript_to_finish
+
+      find('.edit-answers', match: :first).click
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_certificate_of_conf_no_epic_answer', 'Yes'
+      wait_for_javascript_to_finish
+      click_button 'Save'
+      wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
+
+      within '#study_type_answer_certificate_of_conf_no_epic' do
+        expect(page).to have_css('div.col-lg-4', text: 'Yes')
+        expect(page).to have_css('a.edit-answers')
+      end
+
+      find('a.edit-answers', match: :first).click
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_certificate_of_conf_no_epic_answer', 'No'
+      wait_for_javascript_to_finish
+      bootstrap_select '#study_type_answer_higher_level_of_privacy_no_epic_answer', 'No'
+      wait_for_javascript_to_finish
+      click_button 'Save'
+      wait_for_javascript_to_finish
+      find('.edit-protocol-information-button').click
+
+
+      within '#study_type_answer_certificate_of_conf_no_epic' do
+        expect(page).to have_css('div.col-lg-4', text: 'No')
+        expect(page).to have_css('a.edit-answers')
+      end
     end
   end
 end

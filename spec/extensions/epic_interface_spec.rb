890856933662fc4ed5f134d92b61e13491814c3a
Ying Wu <ying.wu@pbrc.edu>
2017-07-26 11:32:19 -0500

    fixing merge issues

--- spec/extensions/epic_interface_spec.rb
+++ spec/extensions/epic_interface_spec.rb
@@ -976,7 +976,7 @@ RSpec.describe EpicInterface do
       'rpe' => 'urn:ihe:qrph:rpe:2009',
       'hl7' => 'urn:hl7-org:v3')
 
-      expect(node[0]).to be_equivalent_to(expected.root)
+      expect(node[1]).to be_equivalent_to(expected.root)
     end
 
     it 'return a study type of 1' do

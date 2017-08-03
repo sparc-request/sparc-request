f22bcef975b7bed8dc0001fa0f46453f00cddd9c
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2017-08-03 13:34:13 -0400

    Testing migration issue

--- db/migrate/20170622132807_create_editable_statuses.rb
+++ db/migrate/20170622132807_create_editable_statuses.rb
@@ -7,26 +7,26 @@ class CreateEditableStatuses < ActiveRecord::Migration[5.0]
       t.timestamps                  null: false
     end
 
-    if defined?(EDITABLE_STATUSES)
-      EDITABLE_STATUSES.each do |org_id, statuses|
-        organization = Organization.find(org_id)
-        (statuses << 'first_draft').each do |status|
-          organization.editable_statuses.create(status: status)
-        end
-      end
+    # if defined?(EDITABLE_STATUSES)
+    #   EDITABLE_STATUSES.each do |org_id, statuses|
+    #     organization = Organization.find(org_id)
+    #     (statuses << 'first_draft').each do |status|
+    #       organization.editable_statuses.create(status: status)
+    #     end
+    #   end
 
-      Organization.where.not(id: EDITABLE_STATUSES.keys).each do |org|
-        (AVAILABLE_STATUSES.keys << 'first_draft').each do |status|
-          org.editable_statuses.create(status: status)
-        end
-      end
-    else
-      Organization.all.each do |org|
-        (AVAILABLE_STATUSES.keys << 'first_draft').each do |status|
-          org.editable_statuses.create(status: status)
-        end
-      end
-    end
+    #   Organization.where.not(id: EDITABLE_STATUSES.keys).each do |org|
+    #     (AVAILABLE_STATUSES.keys << 'first_draft').each do |status|
+    #       org.editable_statuses.create(status: status)
+    #     end
+    #   end
+    # else
+    #   Organization.all.each do |org|
+    #     (AVAILABLE_STATUSES.keys << 'first_draft').each do |status|
+    #       org.editable_statuses.create(status: status)
+    #     end
+    #   end
+    # end
   end
 
   def down

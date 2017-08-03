f7f26beac0efd42c4e5748ce3b39016c594860fd
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2017-08-03 13:08:58 -0400

    jtm_generalize_questionnaire

--- db/migrate/20170731165230_generalize_questionnaire.rb
+++ db/migrate/20170731165230_generalize_questionnaire.rb
@@ -2,6 +2,6 @@ class GeneralizeQuestionnaire < ActiveRecord::Migration[5.1]
   def change
     rename_column :questionnaires, :service_id, :questionable_id
     add_column :questionnaires, :questionable_type, :string, after: :questionable_id
-    Questionnaire.find_each{ |q| q.update(questionable_type: 'service')}
+    Questionnaire.find_each{ |q| q.update(questionable_type: 'Service')}
   end
 end

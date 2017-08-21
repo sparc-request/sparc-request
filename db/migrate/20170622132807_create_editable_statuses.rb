f04c7ffe23d00853f6babed3ccb5956eff0f1de8
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2017-08-21 11:32:28 -0400

    Small fixes

--- db/migrate/20170622132807_create_editable_statuses.rb
+++ db/migrate/20170622132807_create_editable_statuses.rb
@@ -26,6 +26,7 @@ class CreateEditableStatuses < ActiveRecord::Migration[5.0]
 
       t.timestamps                  null: false
     end
+    
     if defined?(EDITABLE_STATUSES)
       EDITABLE_STATUSES.each do |org_id, statuses|
         if organization = Organization.find_by_id(org_id)
@@ -35,18 +36,18 @@ class CreateEditableStatuses < ActiveRecord::Migration[5.0]
         end
       end
 
-    #   Organization.where.not(id: EDITABLE_STATUSES.keys).each do |org|
-    #     (AVAILABLE_STATUSES.keys << 'first_draft').each do |status|
-    #       org.editable_statuses.create(status: status)
-    #     end
-    #   end
-    # else
-    #   Organization.all.each do |org|
-    #     (AVAILABLE_STATUSES.keys << 'first_draft').each do |status|
-    #       org.editable_statuses.create(status: status)
-    #     end
-    #   end
-    # end
+      Organization.where.not(id: EDITABLE_STATUSES.keys).each do |org|
+        (AVAILABLE_STATUSES.keys << 'first_draft').each do |status|
+          org.editable_statuses.create(status: status)
+        end
+      end
+    else
+      Organization.all.each do |org|
+        (AVAILABLE_STATUSES.keys << 'first_draft').each do |status|
+          org.editable_statuses.create(status: status)
+        end
+      end
+    end
   end
 
   def down

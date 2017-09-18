5255340c2d695caa3bee97b3f82fcf92ffd5c1bc
Johanna Wiel <jwiel86@gmail.com>
2017-09-18 11:28:26 -0400

    JM/WTH - (SPARCRequest & SPARCDashboard-production) RMID Validation Broken [#151137513]

--- app/controllers/dashboard/protocols_controller.rb
+++ app/controllers/dashboard/protocols_controller.rb
@@ -164,7 +164,9 @@ class Dashboard::ProtocolsController < Dashboard::BaseController
     permission_to_edit  = @authorization.present? ? @authorization.can_edit? : false
     # admin is not able to activate study_type_question_group
 
-    if save_protocol_with_blank_rmid_if_admin(attrs)
+    @protocol.admin = @admin
+
+    if @protocol.update_attributes(attrs)
       flash[:success] = I18n.t('protocols.updated', protocol_type: @protocol.type)
     else
       @errors = @protocol.errors
@@ -336,15 +338,5 @@ class Dashboard::ProtocolsController < Dashboard::BaseController
     end
 
     attrs
-  end
-
-  def save_protocol_with_blank_rmid_if_admin(attrs)
-    @protocol.assign_attributes(attrs)
-    if @admin && !@protocol.valid? && @protocol.errors.full_messages == ["Research master can't be blank"]
-      @protocol.save(validate: false)
-    else
-      @protocol.errors.delete(:research_master_id) if @admin
-      @protocol.save
-    end
   end
 end

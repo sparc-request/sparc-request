b242bf2bd3cc9386f1608695c6a6b32b200dc5db
Andrew Cates <amcates@gmail.com>
2017-10-23 12:12:37 -0400

    fix for migration, top level orgs don't have a parent

--- db/migrate/20170918192021_add_use_default_statuses_column_to_orgs.rb
+++ db/migrate/20170918192021_add_use_default_statuses_column_to_orgs.rb
@@ -2,7 +2,7 @@ class AddUseDefaultStatusesColumnToOrgs < ActiveRecord::Migration[5.1]
   def change
     add_column :organizations, :use_default_statuses, :boolean, default: true
     Organization.all.each do |org|
-      org.update(use_default_statuses: false) if org.available_statuses.any? || org.parent.available_statuses.any?
+      org.update(use_default_statuses: false) if org.available_statuses.any? || (org.parent && org.parent.available_statuses.any?)
     end
   end
 end

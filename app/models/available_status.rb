36f2d5e96e611152005fbb3aceb7a65c5ecbe0df
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2017-11-08 14:24:44 -0500

    Made suggested change [#150414701]

--- app/models/available_status.rb
+++ app/models/available_status.rb
@@ -28,7 +28,7 @@ class AvailableStatus < ApplicationRecord
   scope :alphabetized, -> { all.sort{ |x, y| x.humanize <=> y.humanize } }
 
   def disabled_status?
-    ["Draft", "Get A Cost Estimate", "Submitted"].include?(self.humanize)
+    ["Draft", "Get a Cost Estimate", "Submitted"].include?(self.humanize)
   end
 
   def self.statuses

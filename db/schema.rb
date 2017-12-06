2106d252d3460d2519eb5f612bdac04d10ac7105
William Holt <will.t.holt@gmail.com>
2017-12-06 10:43:13 -0500

    wth - db/schema.rb Table Collation
    We noticed recently that `db/schema.rb` is changing the `COLLATE=....` attribute on various tables that are being altered by migrations. We came to the conclusion that several of our tables which have not been altered in a long time by migrations may need to be updated. It would appear that these should be the options for our `create_table`s:
    
    `options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci"`
    
    Please update the schema table statements to use the updated engine, charset, and collation
    update collation
    
    [#152679915]
    
    Story - https://www.pivotaltracker.com/story/show/152679915

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025145319) do
+ActiveRecord::Schema.define(version: 20171206153259) do
 
   create_table "admin_rates", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin" do |t|
     t.integer "line_item_id"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20171025145319) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "selected", default: false
     t.index ["organization_id"], name: "index_available_statuses_on_organization_id"
   end
 
@@ -180,6 +181,7 @@ ActiveRecord::Schema.define(version: 20171025145319) do
     t.string "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "selected", default: false
     t.index ["organization_id"], name: "index_editable_statuses_on_organization_id"
   end
 

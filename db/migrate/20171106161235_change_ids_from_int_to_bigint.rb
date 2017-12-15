794ce56dcd910b92e8c6cfbe06535123ebb72a00
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2017-12-15 10:09:26 -0500

    more fixes

--- db/migrate/20171106161235_change_ids_from_int_to_bigint.rb
+++ db/migrate/20171106161235_change_ids_from_int_to_bigint.rb
@@ -59,7 +59,6 @@ class ChangeIdsFromIntToBigint < ActiveRecord::Migration[5.1]
 
     tables = ActiveRecord::Base.connection.tables
     models = ActiveRecord::Base.descendants
-    binding.pry
     db_habtms = models.map{ |model| model.reflect_on_all_associations(:has_and_belongs_to_many) }.compact.flatten.map{ |reflection| reflection.join_table }.uniq
     db_models = (models - [Document]).group_by(&:table_name).slice(*tables).except(*db_habtms)
     db_models.each{ |table_name, models| db_models[table_name] = models.first }

176d02ad6c3162a0416387f4b84132d44bc71e52
ctsiteam <ctsiteam@gmail.com>
2018-01-01 17:06:00 -0500

    valdation change

--- app/models/study.rb
+++ app/models/study.rb
@@ -19,7 +19,7 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 class Study < Protocol
-  validates :validate_sponsor_source
+  validate  :validate_sponsor_source
   validates :selected_for_epic,           inclusion: [true, false], :if => [:is_epic?]
   validate  :validate_study_type_answers
   

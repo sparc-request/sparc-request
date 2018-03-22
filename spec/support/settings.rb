7646a3c96a1f1e01f0d6c26c9165b8fd25be1bbf
Stuart Johnson <work.stuart.johnson@gmail.com>
2018-03-22 15:51:59 -0400

    fix for fresh installs/travis [#147540605]

--- spec/support/settings.rb
+++ spec/support/settings.rb
@@ -19,6 +19,11 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.~
 
 def populate_settings_before_suite
+  require 'rake'
+
+  load File.expand_path("../../../lib/tasks/import_epic_yml.rake", __FILE__)
+  Rake::Task.define_task(:environment)
+
   DefaultSettingsPopulator.new().populate
 
   Setting.find_by_key("use_ldap").update_attribute(:value, true)

a0a02b16b84672b53ac1bc7b8e35c43bc56aa04a
Stuart Johnson <work.stuart.johnson@gmail.com>
2018-04-16 14:12:51 -0400

    typo... nothing to see here... [#156813143]

--- db/migrate/20180416170741_correct_available_statuses.rb
+++ db/migrate/20180416170741_correct_available_statuses.rb
@@ -1,6 +1,6 @@
 class CorrectAvailableStatuses < ActiveRecord::Migration[5.1]
   def change
-    statuses = availableStatus.where(status: ['draft', 'get_a_cost_estimate', 'submitted'], selected: false)
+    statuses = AvailableStatus.where(status: ['draft', 'get_a_cost_estimate', 'submitted'], selected: false)
     statuses.each do |status|
       status.update_attributes(selected: true)
     end

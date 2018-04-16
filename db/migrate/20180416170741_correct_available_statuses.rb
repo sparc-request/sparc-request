46238d58ca60f89a374dc079cc974430a152ded5
Stuart Johnson <work.stuart.johnson@gmail.com>
2018-04-16 14:06:52 -0400

    query cleanup [#156813143]

--- db/migrate/20180416170741_correct_available_statuses.rb
+++ db/migrate/20180416170741_correct_available_statuses.rb
@@ -1,6 +1,6 @@
 class CorrectAvailableStatuses < ActiveRecord::Migration[5.1]
   def change
-    statuses = (AvailableStatus.where(status: "draft").or(AvailableStatus.where(status: "get_a_cost_estimate")).or(AvailableStatus.where(status: "submitted"))).where(selected: false)
+    statuses = availableStatus.where(status: ['draft', 'get_a_cost_estimate', 'submitted'], selected: false)
     statuses.each do |status|
       status.update_attributes(selected: true)
     end

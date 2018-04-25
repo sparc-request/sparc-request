dc4e5085fdcccfef594d969d6cdee33a162ee49e
Stuart Johnson <work.stuart.johnson@gmail.com>
2018-04-25 09:57:09 -0400

    fixing route issues [#155629819]

--- config/routes.rb
+++ config/routes.rb
@@ -226,11 +226,6 @@ SparcRails::Application.routes.draw do
     root to: 'catalog#index'
   end
 
-  ### redirect in sparc-fulfillment routes.rb ###
-  namespace :fulfillment do
-    resources :sub_service_request, only: [:show]
-  end
-
   namespace :dashboard do
 
     resources :approvals, only: [:new, :create]

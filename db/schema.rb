e708c0f0229f1ec45a77d27ec89b1a6cac143bb3
Stuart Johnson <work.stuart.johnson@gmail.com>
2018-06-07 11:30:54 -0400

    user rights cleanup/bugfixes, as well as migration to clean up historical rights data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_135959) do
+ActiveRecord::Schema.define(version: 2018_06_07_143421) do
 
   create_table "admin_rates", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin", force: :cascade do |t|
     t.integer "line_item_id"

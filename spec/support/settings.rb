8f2e54db806210c6ca70f7fa5a33ec358b0ebdb7
Kyle Glick <kyleglick99@gmail.com>
2018-07-06 08:54:22 -0400

    [#152826724] ldap spec issues

--- spec/support/settings.rb
+++ spec/support/settings.rb
@@ -24,6 +24,9 @@ def populate_settings_before_suite
   Setting.find_by_key("use_epic").update_attribute(:value, true)
   Setting.find_by_key("use_ldap").update_attribute(:value, true)
   Setting.find_by_key("suppress_ldap_for_user_search").update_attribute(:value, true)
+  Setting.find_by_key("ldap_auth_username").update_attribute(:value, nil)
+  Setting.find_by_key("ldap_auth_password").update_attribute(:value, nil)
+  Setting.find_by_key("ldap_filter").update_attribute(:value, nil)
 
   load File.expand_path("../../../app/lib/directory.rb", __FILE__)
 end

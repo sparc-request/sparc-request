434477f35a0accae134c1e191d8080e0bd6bc7e0
Ying Wu <ying.wu@pbrc.edu>
2018-07-06 11:04:05 -0500

    updated the interaction_subject permissible values

--- db/migrate/20180628192745_add_short_interaction_permissible_values.rb
+++ db/migrate/20180628192745_add_short_interaction_permissible_values.rb
@@ -1,6 +1,6 @@
 class AddShortInteractionPermissibleValues < ActiveRecord::Migration[5.2]
   def change
-    ## add 3 interaction type: email, in-person, telephone ##
+    ## add 3 interaction_type pvs: email, in-person, telephone ##
     unless PermissibleValue.where(category: 'interaction_type').exists?
       PermissibleValue.create(category: 'interaction_type', key: 'email', value: 'Email', sort_order: 1)
       PermissibleValue.create(category: 'interaction_type', key: 'in_person', value: 'In-Person', sort_order: 2)
@@ -8,10 +8,16 @@ class AddShortInteractionPermissibleValues < ActiveRecord::Migration[5.2]
       PermissibleValue.where(category: 'interaction_type').update_all(is_available: true)
     end
 
-    ## add interaction subject - extend the list as you wish! or set is_available to false to exclude the item ##
+    ## add interaction_subject pvs - add or remove items from the following list as you wish ##
     unless PermissibleValue.where(category: 'interaction_subject').exists?
+      PermissibleValue.create(category: 'interaction_subject', key: 'bmi_apps', value: 'Bioinformatics Software Choice, Training and Applications')
+      PermissibleValue.create(category: 'interaction_subject', key: 'bmi__study_design', value: 'Bioinformatics Study Design')
+      PermissibleValue.create(category: 'interaction_subject', key: 'biostatistical_question', value: 'Biostatistical Question')
+      PermissibleValue.create(category: 'interaction_subject', key: 'career_development', value: 'Career Development')
+      PermissibleValue.create(category: 'interaction_subject', key: 'funding_opportunities', value: 'Funding Opportunities')
       PermissibleValue.create(category: 'interaction_subject', key: 'general_question', value: 'General Question')
       PermissibleValue.create(category: 'interaction_subject', key: 'redcap', value: 'REDCap')
+      PermissibleValue.create(category: 'interaction_subject', key: 'regulatory_question', value: 'Regulatory Question')
       PermissibleValue.where(category: 'interaction_subject').update_all(is_available: true)
     end
 

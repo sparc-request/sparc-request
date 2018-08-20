145eb8cfb14d659ff6300c0a94f15e7ac5303001
Mark Lohr <markuslohr@gmail.com>
2018-08-20 11:52:37 -0400

    removing schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_190614) do
+ActiveRecord::Schema.define(version: 2018_07_17_181258) do
 
   create_table "admin_rates", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin", force: :cascade do |t|
     t.integer "line_item_id"

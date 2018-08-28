df8a97b63589ae66381c76f32a1ecb3c81670c89
Kyle Glick <kyleglick99@gmail.com>
2018-08-28 08:20:14 -0500

    random spec failure fixes

--- spec/factories/system_survey.rb
+++ spec/factories/system_survey.rb
@@ -21,7 +21,7 @@
 FactoryBot.define do
   factory :system_survey do
     title                     { Faker::Lorem.word }
-    access_code               { Faker::Lorem.word }
+    sequence(:access_code)    { |n| "survey-#{n}" }
     sequence(:version)        { |n| n }
     active                    { false }
     type                      { 'SystemSurvey' }

297609b3bf23962316578f2386b82790c85d06f7
Kyle Glick <kyleglick99@gmail.com>
2018-09-10 08:50:06 -0500

    [#158504995] add cpt code to line items forms in admin dashboard

--- app/helpers/dashboard/study_level_activities_helper.rb
+++ app/helpers/dashboard/study_level_activities_helper.rb
@@ -72,11 +72,11 @@ module Dashboard::StudyLevelActivitiesHelper
   def sla_form_services_select form, line_item
     service = line_item.service
     if service.present? and not service.is_available
-      service_name = service.name + ' (Disabled)'
+      service_name = service.display_service_name + ' (Disabled)'
       form.select "service_id", options_for_select([service_name], service_name), {include_blank: true}, class: 'form-control selectpicker', disabled: 'disabled'
     else
       service_list = line_item.sub_service_request.candidate_services.select {|x| x.one_time_fee}
-      form.select "service_id", options_from_collection_for_select(service_list, 'id', 'name', line_item.service_id), {include_blank: true}, class: 'form-control selectpicker'
+      form.select "service_id", options_from_collection_for_select(service_list, 'id', 'display_service_name', line_item.service_id), {include_blank: true}, class: 'form-control selectpicker'
     end
   end
 

551b8d93018ad31ac298e3dc58e667b168e48318
Kyle Glick <kyleglick99@gmail.com>
2018-10-19 10:07:42 -0500

    [#159782847] hide create institution button if not overlord

---
+++ spec/views/catalog_manager/catalog/accordion.html.haml_spec.rb
@@ -0,0 +1,53 @@
+# Copyright © 2011-2018 MUSC Foundation for Research Development~
+# All rights reserved.~
+
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:~
+
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.~
+
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following~
+# disclaimer in the documentation and/or other materials provided with the distribution.~
+
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products~
+# derived from this software without specific prior written permission.~
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,~
+# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT~
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL~
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS~
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR~
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.~
+
+require 'rails_helper'
+
+RSpec.describe '/catalog_manager/catalog/_accordion.html.haml', type: :view do
+
+  def render_view(user, institutions=Institution.none, show_available_only=true)
+    render '/catalog_manager/catalog/accordion',
+      current_user: user,
+      institutions: institutions,
+      show_available_only: show_available_only
+  end
+
+  describe 'create institution button' do
+    context 'user is catalog overlord' do
+      before :each do
+        render_view(build_stubbed(:identity, catalog_overlord: true))
+      end
+
+      it 'should show the button' do
+        expect(response).to have_selector('.new_organization_link[data-organization-type=Institution]')
+      end
+    end
+
+    context 'user is not catalog overlord' do
+      before :each do
+        render_view(build_stubbed(:identity, catalog_overlord: false))
+      end
+
+      it 'should hide the button' do
+        expect(response).to_not have_selector('.new_organization_link[data-organization-type=Institution]')
+      end
+    end
+  end
+end

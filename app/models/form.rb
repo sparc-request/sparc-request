e0c062d59f7cf244b44a4ccbfa95459fc26ca98b
Kyle Glick <kyleglick99@gmail.com>
2018-12-07 08:34:51 -0600

    [#157749896] fix sparcforms rights

--- app/models/form.rb
+++ app/models/form.rb
@@ -23,7 +23,7 @@ class Form < Survey
   # 2 Forms can't have the same access_code, surveyable_id, and surveyable_type and both be active
   validates_uniqueness_of :active, scope: [:type, :surveyable_id, :surveyable_type, :access_code], if: -> { self.active }
 
-  def self.for(identity)
+  scope :for, -> (identity) {
     orgs =
       if identity.is_site_admin?
         Organization.all
@@ -35,8 +35,22 @@ class Form < Survey
       
     services = Service.where(organization: orgs)
     
-    Form.where(surveyable: orgs).or(where(surveyable: services)).or(where(surveyable: identity))
-  end
+    where(surveyable: orgs).
+    or(where(surveyable: services)).
+    or(where(surveyable: identity))
+  }
+
+  scope :for_super_user, -> (identity) {
+    where(surveyable: Organization.authorized_for_super_user(identity.id))
+  }
+
+  scope :for_service_provider, -> (identity) {
+    where(surveyable: Organization.authorized_for_service_provider(identity.id))
+  }
+
+  scope :for_catalog_manager, -> (identity) {
+    where(surveyable: Organization.authorized_for_catalog_manager(identity.id))
+  }
 
   def self.yaml_klass
     Form.name
--- app/views/surveyor/responses/index.json.jbuilder
+++ app/views/surveyor/responses/index.json.jbuilder
@@ -1,5 +1,5 @@
 if @type == 'Form'
-  accessible_surveys = Form.for(current_user)
+  accessible_surveys = Form.for_super_user(current_user).or(Form.for_service_provider(current_user))
 else
   accessible_surveys = SystemSurvey.for_super_user(current_user)
 end

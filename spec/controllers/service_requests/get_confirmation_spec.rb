6ba5b07a41d2c0ae1af6419fd6880d6d74986fda
Kyle Glick <kyleglick99@gmail.com>
2019-01-18 13:40:00 -0600

    fix last spec

--- spec/controllers/service_requests/get_confirmation_spec.rb
+++ spec/controllers/service_requests/get_confirmation_spec.rb
@@ -135,7 +135,7 @@ RSpec.describe ServiceRequestsController, type: :controller do
           org      = create(:organization)
           service  = create(:service, organization: org, one_time_fee: true, send_to_epic: true)
           protocol = create(:protocol_federally_funded, primary_pi: logged_in_user, type: 'Study', selected_for_epic: true)
-          sr       = create(:service_request_without_validations, protocol: protocol)
+          sr       = create(:service_request_without_validations, protocol: protocol, submitted_at: (Time.now + 1.day))
           ssr      = create(:sub_service_request_without_validations, service_request: sr, organization: org, status: 'draft', protocol_id: protocol.id)
           li       = create(:line_item, service_request: sr, sub_service_request: ssr, service: service)
                      create(:service_provider, identity: logged_in_user, organization: org)

9e34c9eb7a096f5c1c042d1e348d53e28cf95997
Jason Leonard <jleonardw9@gmail.com>
2019-02-12 18:10:33 -0600

    Adding and modifing merge code from rake task

--- app/controllers/dashboard/protocol_merges_controller.rb
+++ app/controllers/dashboard/protocol_merges_controller.rb
@@ -12,10 +12,90 @@ class Dashboard::ProtocolMergesController < Dashboard::BaseController
 
     if (master_protocol == nil) || (sub_protocol == nil)
       flash[:alert] = 'Protocol(s) not found. Check IDs and try again.'
-    end 
+    else
+      ActiveRecord::Base.transaction do
+
+        #transfer the project roles as needed
+        sub_protocol.project_roles.each do |role|
+          if role.role != 'primary-pi' && role_should_be_assigned?(role, master_protocol)
+            role.update_attributes(protocol_id: master_protocol.id)
+          end
+        end
+
+        # checking for and assigning research types, impact areas, and affiliations...
+        if has_research?(sub_protocol, 'human_subjects') && !has_research?(master_protocol, 'human_subjects')
+          sub_protocol.human_subjects_info.update_attributes(protocol_id: master_protocol.id)
+        elsif has_research?(sub_protocol, 'vertebrate_animals') && !has_research?(master_protocol, 'vertebrate_animals')
+          sub_protocol.vertebrate_animals_info.update_attributes(protocol_id: master_protocol.id)
+        elsif has_research?(sub_protocol, 'investigational_products') && !has_research?(master_protocol, 'investigational_products')
+          sub_protocol.investigational_products_info.update_attributes(protocol_id: master_protocol.id)
+        elsif has_research?(sub_protocol, 'ip_patents') && !has_research?(master_protocol, 'ip_patents')
+          sub_protocol.ip_patents_info.update_attributes(protocol_id: master_protocol.id)
+        end
+
+        sub_protocol.impact_areas.each do |area|
+          area.protocol_id = master_protocol.id
+          area.save(validate: false)
+        end
+
+        sub_protocol.affiliations.each do |affiliation|
+          affiliation.protocol_id = master_protocol.id
+          affiliation.save(validate: false)
+        end
+
+        # assigning service requests...
+        fulfillment_ssrs = []
+        sub_protocol.service_requests.each do |request|
+          request.protocol_id = master_protocol.id
+          request.save(validate: false)
+          request.sub_service_requests.each do |ssr|
+            ssr.update_attributes(protocol_id: master_protocol.id)
+            master_protocol.next_ssr_id = (master_protocol.next_ssr_id + 1)
+            master_protocol.save(validate: false)
+            if ssr.in_work_fulfillment
+              fulfillment_ssrs << ssr
+            end
+          end
+        end
+
+        #assigning arms..."
+        sub_protocol.arms.each do |arm|
+          check_arm_names(arm, master_protocol)
+          arm.protocol_id = master_protocol.id
+          arm.save(validate: false)
+        end
+
+        #assigning documents..."
+        sub_protocol.documents.each do |document|
+          document.protocol_id = master_protocol.id
+          document.save(validate: false)
+        end
+
+        #assigning_notes
+        sub_protocol.notes.each do |note|
+          note.notable_id = master_protocol.id
+          note.save(validate: false)
+        end
+
+        #delete sub protocol
+        sub_protocol.delete
+          end
+        end 
+      end
+      flash[:success] = 'Protocol merge succesful'
+    end
   end
 
   private
+
+  def role_should_be_assigned?(role_to_be_assigned, protocol)
+    protocol.project_roles.each do |role|
+      if (role.role == role_to_be_assigned.role) && (role.identity_id == role_to_be_assigned.identity_id)
+        return false
+      end
+    end
+    return true
+  end
 
   def authorize_overlord
     unless @user.catalog_overlord?

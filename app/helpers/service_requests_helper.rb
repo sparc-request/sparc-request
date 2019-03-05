5ffb91f4770eafa57494223c14f8ce19e1cdc927
royyesud@musc.edu <royyesud@musc.edu>
2019-03-05 10:08:26 -0500

    SPARCRequest confirmation page changes [#164176720]

--- app/helpers/service_requests_helper.rb
+++ app/helpers/service_requests_helper.rb
@@ -78,13 +78,7 @@ module ServiceRequestsHelper
   end
 
   def step_nav_button(text, color, url, inactive_link)
-    if inactive_link
-      content_tag(:div,
-        content_tag(:div, raw(text), class: "btn step-text")+
-        content_tag(:div, '', class: "right-arrow"),
-        class: "step-btn step-btn-#{color} disabled_steps"
-      )
-    else
+    if !inactive_link
       link_to(
         content_tag(:div, raw(text), class: "btn step-text")+
         content_tag(:div, '', class: "right-arrow"),

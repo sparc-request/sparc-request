11db85d6ccd0cd5052850c4dac7c08801ab8c814
Stuart Johnson <work.stuart.johnson@gmail.com>
2019-04-24 14:31:05 -0400

    fixing bad data for visit_group position [#157520869]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_212606) do
+ActiveRecord::Schema.define(version: 2019_04_24_133214) do
 
   create_table "admin_rates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.bigint "line_item_id"
@@ -443,8 +443,8 @@ ActiveRecord::Schema.define(version: 2019_03_01_212606) do
   end
 
   create_table "patient_registrars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
-    t.bigint "identity_id"
-    t.bigint "organization_id"
+    t.integer "identity_id"
+    t.integer "organization_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["identity_id"], name: "index_patient_registrars_on_identity_id"

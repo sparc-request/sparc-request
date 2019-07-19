a57c532318c8fca78f4ccf04b62e02db76f7f6f2
Ying Wu <ying.wu@pbrc.edu>
2019-07-19 12:59:30 -0500

    undoe profile related navigation changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,15 +19,15 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 module ApplicationHelper
-  # SPARCDashboard, SPARCReport
+
   def show_welcome_message(current_user, bootstrap = false)
     returning_html = ""
     if current_user
       logged_in_as = current_user.email ? current_user.email : current_user.full_name
       returning_html +=
-        content_tag(:span, t(:dashboard)[:navbar][:logged_in_as]) +
-        link_to("#{logged_in_as} ", "/identities/edit" ,  {id: 'logged-in-as', target: 'blank' , 'data-toggle' => 'tooltip', title: 'Edit your profile' }) +
-        content_tag(:span, '', class: "glyphicon glyphicon-user") +
+        content_tag(:span,
+          t(:dashboard)[:navbar][:logged_in_as] +
+          logged_in_as + " ") +
         link_to('Logout', destroy_identity_session_path, method: :delete, class: bootstrap ? "btn btn-warning" : "")
     else
       # could be used to provide a login link

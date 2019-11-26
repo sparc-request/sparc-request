60eac64b2c19bedfee3fe800b286b6f3084ee359
kayla-glick <kaylaglick12@gmail.com>
2019-11-26 11:10:55 -0500

    fix syntax error

--- app/helpers/dashboard/sub_service_requests_helper.rb
+++ app/helpers/dashboard/sub_service_requests_helper.rb
@@ -45,7 +45,7 @@ module Dashboard::SubServiceRequestsHelper
         if current_user.go_to_cwf_rights?(sub_service_request.organization)
           if sub_service_request.imported_to_fulfillment?
             # In fulfillment, and user has rights to view in Fulfillment
-            link_to "#{Setting.get_value("clinical_work_fulfillment_url")}/sub_service_request/#{sub_service_request.id}", target: :_blank, id: 'goToFulfillment', class: 'btn btn-success', data: { imported: sub_service_request.imported_to_fulfillment? }) do
+            link_to "#{Setting.get_value("clinical_work_fulfillment_url")}/sub_service_request/#{sub_service_request.id}", target: :_blank, id: 'goToFulfillment', class: 'btn btn-success', data: { imported: sub_service_request.imported_to_fulfillment? } do
               icon('fas', 'eye mr-2') + t('dashboard.sub_service_requests.header.fulfillment.go_to_fulfillment')
             end
           else

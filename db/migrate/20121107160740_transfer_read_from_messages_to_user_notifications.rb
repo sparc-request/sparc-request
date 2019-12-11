550854baa7ba275abefb703bd93263f8979c56ee
kayla-glick <kaylaglick12@gmail.com>
2019-12-11 13:47:12 -0500

    fix schema and bad migration

--- db/migrate/20121107160740_transfer_read_from_messages_to_user_notifications.rb
+++ db/migrate/20121107160740_transfer_read_from_messages_to_user_notifications.rb
@@ -19,6 +19,8 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 class TransferReadFromMessagesToUserNotifications < ActiveRecord::Migration[4.2]
-  remove_column :messages, :read
-  add_column :user_notifications, :read, :boolean
+  def change
+    remove_column :messages, :read
+    add_column :user_notifications, :read, :boolean
+  end
 end

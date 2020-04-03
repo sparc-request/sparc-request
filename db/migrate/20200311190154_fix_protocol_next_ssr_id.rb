2e00d3cd0890744747bd9651c15e7c635b1f5c34
Stuart Johnson <work.stuart.johnson@gmail.com>
2020-04-03 17:12:58 -0400

    quick fix for migration

--- db/migrate/20200311190154_fix_protocol_next_ssr_id.rb
+++ db/migrate/20200311190154_fix_protocol_next_ssr_id.rb
@@ -2,7 +2,7 @@ class FixProtocolNextSsrId < ActiveRecord::Migration[5.2]
   def change
     # See https://www.pivotaltracker.com/story/show/171602442
 
-    Protocol.all.each do |protocol|
+    Protocol.where.not(next_ssr_id: nil).each do |protocol|
       old_next_ssr_id = protocol.next_ssr_id.gsub(/^0(0*)/, '').to_i
       last_ssr_id     = protocol.sub_service_requests.last.ssr_id.gsub(/^0(0*)/, '').to_i
 

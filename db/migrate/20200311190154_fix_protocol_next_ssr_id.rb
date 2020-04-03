6d7bd9ae7cc8ea9e57aa070c3d30adc3502fc3b7
Stuart Johnson <work.stuart.johnson@gmail.com>
2020-04-03 17:20:53 -0400

    another fix for migration... -.-

--- db/migrate/20200311190154_fix_protocol_next_ssr_id.rb
+++ db/migrate/20200311190154_fix_protocol_next_ssr_id.rb
@@ -3,7 +3,7 @@ class FixProtocolNextSsrId < ActiveRecord::Migration[5.2]
     # See https://www.pivotaltracker.com/story/show/171602442
 
     Protocol.where.not(next_ssr_id: nil).each do |protocol|
-      old_next_ssr_id = protocol.next_ssr_id.gsub(/^0(0*)/, '').to_i
+      old_next_ssr_id = protocol.next_ssr_id.to_s.gsub(/^0(0*)/, '').to_i
       last_ssr_id     = protocol.sub_service_requests.last.ssr_id.gsub(/^0(0*)/, '').to_i
 
       # If the protocol's next_ssr_id is less than or equal to the last SSR's ssr_id then

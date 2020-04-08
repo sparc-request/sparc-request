8b3b5568a064a81405471cce68abb00f8c3972cf
Jason Leonard <jleonardw9@gmail.com>
2020-04-08 17:05:57 -0400

    Adding a check for protocols that do not have ssrs.

--- app/lib/dashboard/fix_ssr_ids.rb
+++ app/lib/dashboard/fix_ssr_ids.rb
@@ -10,15 +10,17 @@ module Dashboard
 
     def perform_id_fix
       id = @next_ssr_id
-      @merged_ssr_ids.each do |ssr_id|
-        ssr = SubServiceRequest.find(ssr_id)
-        ssr.ssr_id = "%04d" % id
-        id += 1
-        ssr.save(validate: false)
-      end
+      if @merged_ssr_ids.count != 0
+        @merged_ssr_ids.each do |ssr_id|
+          ssr = SubServiceRequest.find(ssr_id)
+          ssr.ssr_id = "%04d" % id
+          id += 1
+          ssr.save(validate: false)
+        end
 
-      @protocol.next_ssr_id = id
-      @protocol.save(validate: false)
+        @protocol.next_ssr_id = id
+        @protocol.save(validate: false)
+      end
     end
   end
 end

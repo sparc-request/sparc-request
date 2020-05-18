466cb0a387060ea269f3dd7d536e2ca92a7b3e26
Jason Leonard <jleonardw9@gmail.com>
2020-05-18 16:24:26 -0400

    Fixing typo

--- db/migrate/20200518200713_create_fulfillment_synchs.rb
+++ db/migrate/20200518200713_create_fulfillment_synchs.rb
@@ -1,7 +1,7 @@
 class CreateFulfillmentSynchs < ActiveRecord::Migration[5.2]
   def change
     create_table :fulfillment_synchs do |t|
-      t.references :SubServiceRequest
+      t.references :sub_service_request
       t.integer    :line_item_id
       t.string     :action
       t.boolean    :synched, default: false

183c16305e1afd91f43d3dd82e9ce708b7ce7222
kayla-glick <kaylaglick12@gmail.com>
2020-08-24 14:05:40 -0400

    update ids to bigints

--- db/migrate/20200814162116_create_doorkeeper_tables.rb
+++ db/migrate/20200814162116_create_doorkeeper_tables.rb
@@ -2,6 +2,8 @@
 
 class CreateDoorkeeperTables < ActiveRecord::Migration[5.2]
   def change
+    Rake::Task['migrate_ids_to_bigint'].invoke
+
     create_table :oauth_applications do |t|
       t.string  :name,        null: false
       t.string  :description

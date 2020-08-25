23adb4a1ea35a738e33b38970fa9d9fd5b6c91b4
kayla-glick <kaylaglick12@gmail.com>
2020-08-25 16:00:09 -0400

    fix missing class

--- db/migrate/20121016165642_create_document_grouping_and_add_grouping_id_to_document.rb
+++ db/migrate/20121016165642_create_document_grouping_and_add_grouping_id_to_document.rb
@@ -19,6 +19,9 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 class CreateDocumentGroupingAndAddGroupingIdToDocument < ActiveRecord::Migration[4.2]
+  class DocumentGroupings < ActiveRecord::Base
+  end
+
   def up
     create_table :document_groupings do |t|
       t.belongs_to :service_request

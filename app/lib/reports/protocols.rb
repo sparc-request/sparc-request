a94bc2703f77ee886e6e6a76479b5e7234d981a9
Ying Wu <ying.wu@pbrc.edu>
2020-11-05 09:22:35 -0600

    added Combined Protocol ID(s) column to Protocols report

--- app/lib/reports/protocols.rb
+++ app/lib/reports/protocols.rb
@@ -47,6 +47,8 @@ class ProtocolsReport < ReportingModule
 
     attrs["Protocol ID"]                  = "id"
     attrs["Research Master ID"]           = "research_master_id"
+    ## https://www.pivotaltracker.com/n/projects/1918597/stories/174238755
+    attrs["Combined Protocol ID(s)"]      = "protocol_merges.pluck(:merged_protocol_id).join(', ')"
     attrs["Protocol Short Title"]         = "short_title"
     attrs["Protocol Title"]               = "title"
     attrs["Number of Requests"]           = "sub_service_requests.length"

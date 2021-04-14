6b5afa3135cea4b88b5d81859e8edcf67caa1510
Stuart Johnson <stuartjohnson@Stuarts-MacBook-Pro.local>
2021-04-14 11:19:46 -0400

    fixing admin rate migration for nil audit edge case

--- db/migrate/20210323211018_add_identity_to_admin_rate.rb
+++ db/migrate/20210323211018_add_identity_to_admin_rate.rb
@@ -7,7 +7,7 @@ class AddIdentityToAdminRate < ActiveRecord::Migration[5.2]
 
     AdminRate.find_each do |admin_rate|
       audit = AuditRecovery.where(auditable_id: admin_rate.id, auditable_type: "AdminRate", action: "create").first
-      admin_rate.update_attributes(identity_id: audit.user_id)
+      admin_rate.update_attributes(identity_id: audit.try(:user_id))
       bar.increment!
     end
   end

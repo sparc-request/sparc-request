dfdc61351187662eddccc11e3906a0066245bc01
Ying Wu <ying.wu@pbrc.edu>
2021-08-03 11:29:42 -0500

    merge base_controller into application_controlelr

--- app/controllers/admin/permissible_values_controller.rb
+++ app/controllers/admin/permissible_values_controller.rb
@@ -19,7 +19,7 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-class Admin::PermissibleValuesController < Admin::BaseController
+class Admin::PermissibleValuesController < Admin::ApplicationController
 
     def index
       @permissible_values = PermissibleValue.reorder('category , is_available DESC, sort_order')

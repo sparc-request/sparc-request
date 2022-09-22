10a744a9ac1386966a6e4770ec02df57509f1759
James Van Meter <jamesvanmeter@Jamess-MacBook-Pro.local>
2022-09-22 06:39:56 -0400

    Successful transplant of relevant Paperclip data to ActiveStorage tables

---
+++ db/migrate/20220922012650_convert_paperclip_attachments_to_active_storage.rb
@@ -0,0 +1,85 @@
+class ConvertPaperclipAttachmentsToActiveStorage < ActiveRecord::Migration[5.2]
+  require 'open-uri'
+
+  def change
+    get_blob_id = 'LAST_INSERT_ID()'
+
+    active_storage_blob_statement = ActiveRecord::Base.connection.raw_connection.prepare(<<-SQL)
+      INSERT INTO active_storage_blobs (
+        `key`, filename, content_type, metadata, byte_size, checksum, created_at
+      ) VALUES (?, ?, ?, '{}', ?, ?, ?)
+    SQL
+
+    active_storage_attachment_statement = ActiveRecord::Base.connection.raw_connection.prepare(<<-SQL)
+      INSERT INTO active_storage_attachments (
+        name, record_type, record_id, blob_id, created_at
+      ) VALUES (?, ?, ?, #{get_blob_id}, ?)
+    SQL
+
+    Rails.application.eager_load!
+    models = ActiveRecord::Base.descendants.reject(&:abstract_class?)
+
+    transaction do
+      models.each do |model|
+        attachments = model.column_names.map do |c|
+          if c =~ /(.+)_file_name$/
+            $1
+          end
+        end.compact
+
+        if attachments.empty?
+          next
+        end
+
+        model.find_each.each do |instance|
+          attachments.each do |attachment|
+            attachment_path = Dir[path_generator(instance, attachment)]
+            if !attachment_path.empty?
+              active_storage_blob_statement.execute(
+                key(instance, attachment),
+                instance.send("#{attachment}_file_name"),
+                instance.send("#{attachment}_content_type"),
+                instance.send("#{attachment}_file_size"),
+                checksum(attachment_path[0]),
+                instance.updated_at.iso8601
+              )
+
+              active_storage_attachment_statement.execute(
+                attachment,
+                model.name,
+                instance.id,
+                instance.updated_at.iso8601)
+            end
+          end
+        end
+      end
+    end
+
+    active_storage_attachment_statement.close
+    active_storage_blob_statement.close
+  end
+
+  private
+
+  def key(instance, attachment)
+    filename = instance.send("#{attachment}_file_name")
+    klass = instance.class.table_name
+    id = instance.id
+    id_partition = ("%09d".freeze % id).scan(/\d{3}/).join("/".freeze)
+
+    "#{klass}/#{attachment.pluralize}/#{id_partition}/original/#{filename}"
+  end
+
+  def checksum(path)
+    Digest::MD5.base64digest(File.read(path))
+  end
+
+  def path_generator(instance, attachment)
+    filename = instance.send("#{attachment}_file_name")
+    klass = instance.class.table_name
+    id = instance.id
+    id_partition = ("%09d".freeze % id).scan(/\d{3}/).join("/".freeze)
+
+    Rails.root.join("public", "system", "#{klass}", "#{attachment.pluralize}", "#{id_partition}", "original", "*")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_02_143414) do
+ActiveRecord::Schema.define(version: 2022_09_22_012650) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "name", null: false

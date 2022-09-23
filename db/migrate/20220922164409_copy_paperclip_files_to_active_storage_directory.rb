90eec88116248b98f38af90fbc2c6ccd5b00c3de
James Van Meter <jamesvanmeter@Jamess-MacBook-Pro.local>
2022-09-23 12:18:25 -0400

    Successful copying of all Paperclip attachment files from original Paperclip directories to valid ActiveStorage directories.

---
+++ db/migrate/20220922164409_copy_paperclip_files_to_active_storage_directory.rb
@@ -0,0 +1,13 @@
+class CopyPaperclipFilesToActiveStorageDirectory < ActiveRecord::Migration[5.2]
+  def change
+    ActiveStorage::Attachment.find_each do |attachment|
+      source = Rails.root.join("public", "system", attachment.blob.key)
+      as_prefix_subdir = File.join(attachment.blob.key.first(2), attachment.blob.key.first(4).last(2))
+      as_key_subdir = /^(.*[\\\/])/.match(attachment.blob.key)[1]
+      dest_dir = Rails.root.join("storage", as_prefix_subdir, as_key_subdir)
+
+      FileUtils.mkdir_p(dest_dir)
+      FileUtils.cp(source, dest_dir)
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_22_012650) do
+ActiveRecord::Schema.define(version: 2022_09_22_164409) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "name", null: false

e4ea0fd2443ebf516c073720bf18f103883d8b44
James Van Meter <jamesvanmeter@Jamess-MacBook-Pro.local>
2022-09-28 13:40:54 -0400

    Maximum length validation added to :protocol:short_title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_141351) do
+ActiveRecord::Schema.define(version: 2022_09_07_153451) do
 
   create_table "admin_rates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.bigint "line_item_id"

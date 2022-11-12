5e200b1332aa8edfc893d8e2aa80b9fbe466e4d6
Greg Mathias <mathiasgreg@gmail.com>
2022-11-12 09:08:42 -0500

    Add name and type validation test

--- spec/models/external_organization_spec.rb
+++ spec/models/external_organization_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ExternalOrganization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { is_expected.to belong_to(:protocol) }
+
+  it 'has none to begin with' do
+    expect(ExternalOrganization.count).to eq 0
+  end
+
+  it 'validates name and type' do
+    ExternalOrganization.create
+    expect(ExternalOrganization.count).to eq 0
+    ExternalOrganization.create collaborating_org_name:'Other', collaborating_org_type:'Other'
+    expect(ExternalOrganization.count).to eq 1
+  end
+
 end

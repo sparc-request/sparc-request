9e75d8703f9a4477cc6bbe2dac0d0cedb1370187
James Van Meter <jamesvanmeter@Jamess-MacBook-Pro.local>
2022-11-14 20:52:28 -0500

    rails g migration convert_paperclip_attachments_to_activestorage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_14_221711) do
+ActiveRecord::Schema.define(version: 2022_11_15_014326) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "name", null: false

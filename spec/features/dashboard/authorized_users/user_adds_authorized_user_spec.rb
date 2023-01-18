bcca807fab973c1aaa1e937fa74a229d10e3e946
Ito <ito.eta1@gmail.com>
2023-01-18 15:00:48 -0500

    commenting some code back out

--- spec/features/dashboard/authorized_users/user_adds_authorized_user_spec.rb
+++ spec/features/dashboard/authorized_users/user_adds_authorized_user_spec.rb
@@ -112,28 +112,29 @@ RSpec.feature 'User wants to add an authorized user', js: true do
     end
   end
 
-  context 'epic user api is down' do
-    stub_config("use_epic", true)
-    stub_config("validate_epic_users", true)
-    stub_config("epic_endpoint", "a_bad_url_that_will_not_lead_to_the_epic_api")
-    stub_config("epic_user_endpoint", "a_bad_url_that_will_not_lead_to_the_epic_user_api")
+  # context 'epic user api is down' do
+  #   stub_config("use_epic", true)
+  #   stub_config("validate_epic_users", true)
+  #   stub_config("epic_endpoint", "a_bad_url_that_will_not_lead_to_the_epic_api")
+  #   stub_config("epic_user_endpoint", "a_bad_url_that_will_not_lead_to_the_epic_user_api")
 
-    #before :each do
-      #@protocol = create(:study_federally_funded, primary_pi: jug2, selected_for_epic: true)
+  #   before :each do
+  #     @protocol = create(:study_federally_funded, primary_pi: jug2, selected_for_epic: true)
+  #     allow(EpicUser).to receive(:confirm_connection).and_return(true)
 
-      #visit dashboard_protocol_path(@protocol)
-      #wait_for_javascript_to_finish
-    #end
+  #     visit dashboard_protocol_path(@protocol)
+  #     wait_for_javascript_to_finish
+  #   end
 
-    #it 'should not add the new user' do
-      #click_link I18n.t('authorized_users.new')
-      #wait_for_javascript_to_finish
+  #   it 'should not add the new user' do
+  #     click_link I18n.t('authorized_users.new')
+  #     wait_for_javascript_to_finish
 
-      #bootstrap_typeahead '#user_search', 'Doe'
-      #wait_for_javascript_to_finish
+  #     bootstrap_typeahead '#user_search', 'Doe'
+  #     wait_for_javascript_to_finish
 
-      expect(@protocol.reload.project_roles.last.identity).to_not eq(other_user)
-      expect(page).to have_selector('small', text: I18n.t("activerecord.errors.models.project_role.attributes.base.epic_api_down").html_safe)
-    end
-  end
+  #     expect(@protocol.reload.project_roles.last.identity).to_not eq(other_user)
+  #     expect(page).to have_selector('small', text: I18n.t("activerecord.errors.models.project_role.attributes.base.epic_api_down").html_safe)
+  #   end
+  # end
 end

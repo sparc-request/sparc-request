5deb96697ba1ca25f2f0f15f2ccfec1419513cee
Andrew Cates <amcates@gmail.com>
2023-01-25 11:59:22 -0500

    stubbing epic_user_endpoint

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -33,6 +33,10 @@ RSpec.configure do |config|
       to_return(status: 201)
 
     #### EpicUser Stubs ####
+
+    stub_request(:get, /#{Setting.get_value("epic_user_endpoint")}/).
+      to_return(status: 200, body: "")
+
     stub_request(:get, "https://c3po-hadoop-s2-v.obis.musc.edu:8484/v1/epicintc/viewuser.json?userid=jug2").
       with(headers: {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
       to_return(status: 200, body: {"UserID" => "jug2", "IsExist" => true, "IsActive" => true, "IsSER" => "false"}.to_json, headers: { 'Content-Type' => 'application/json' })

c6c8f5a833e45c077a5c6af5f621b547fdf098e6
Andrew Cates <amcates@gmail.com>
2023-06-29 11:28:21 -0400

    fix to missing visits relation

--- app/lib/dashboard/service_calendars.rb
+++ app/lib/dashboard/service_calendars.rb
@@ -85,10 +85,11 @@ module Dashboard
             where(services: { one_time_fee: false })
         else
           arm.line_items_visits.
+            eager_load(:visits).
             includes(sub_service_request: :services).
             where.not(sub_service_requests: { status: statuses_hidden }).
             where(services: { one_time_fee: false }).
-            where.not("research_billing_qty = 0 and insurance_billing_qty = 0 and effort_billing_qty = 0" )
+            where.not("visits.research_billing_qty = 0 and visits.insurance_billing_qty = 0 and visits.effort_billing_qty = 0" )
         end
       else
         (sub_service_request || service_request).line_items_visits.

a6234c68ff045cb58919eb79457dbeb1d1cffd4d
Ito-Eta <32404443+Ito-Eta@users.noreply.github.com>
2023-07-21 14:22:53 -0400

    Update protocols_helper.rb

--- app/helpers/protocols_helper.rb
+++ app/helpers/protocols_helper.rb
@@ -91,7 +91,6 @@ module ProtocolsHelper
     else #On the other hand, if Epic is active...
       !['certificate_of_conf_no_epic', 'higher_level_of_privacy_no_epic'].include?(study_type_answer.study_type_question.friendly_id) && study_type_answer.answer != nil
       if study_type_answer.study_type_question.friendly_id == 'certificate_of_conf'
-        puts "***************CONFIRMED THAT FRIENDLY ID IS 'certificate_of_conf'!  The element should be displaying.****************"
         true
       end
     end

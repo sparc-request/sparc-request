22e23d9062b8d6d9ab6085a38c639b86f1374c92
Greg Mathias <mathiasgreg@gmail.com>
2023-09-10 08:00:15 -0400

    Fix rebase error

--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -263,7 +263,7 @@ class ServiceRequest < ApplicationRecord
   def total_direct_costs_per_patient arms=self.arms, line_items=nil
     total = 0.0
     arms.each do |arm|
-      livs = (line_items.nil? ? 
+      livs = (line_items.nil? ?
         arm.line_items_visits :
         arm.line_items_visits.where(line_item: line_items)).
         includes(line_item: [:admin_rates, :protocol, service: [:pricing_maps, organization: [:pricing_setups, parent: [:pricing_setups, parent: [:pricing_setups, :parent]]]]])
@@ -363,8 +363,6 @@ class ServiceRequest < ApplicationRecord
     self.sub_service_requests.each do |ssr|
       ssr.organization_forms.joins(:responses).where(responses: { respondable: ssr }).each{ |f| forms << [f, ssr] }
       ssr.service_forms.joins(:responses).where(responses: { respondable: ssr }).each{ |f| forms << [f, ssr] }
-      ssr.previous_version_service_forms.joins(:responses).where(responses: { respondable: ssr }).each{ |f| forms << [f, ssr] }
-
     end
     forms
   end

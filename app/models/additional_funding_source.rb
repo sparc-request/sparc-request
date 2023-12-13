13b3e361a8225ab6d77123470918430fc16a9fce
Greg Mathias <mathiasgreg@gmail.com>
2023-12-13 16:09:23 -0500

    Validate funding_source presence and fix display method

--- app/models/additional_funding_source.rb
+++ app/models/additional_funding_source.rb
@@ -3,11 +3,14 @@ class AdditionalFundingSource < ApplicationRecord
 
   validates :fundig_source_other, presence: true, if: Proc.new { |a| a.funding_source == 'Internal Funded Pilot Project' }
 
+  validates :funding_source, presence: true
+
   def display_additional_funding_source_value
     source = "#{PermissibleValue.get_value('funding_source', funding_source)}"
     if funding_source_other.present?
       source += ": #{funding_source_other}"
     end
+    return source
   end
   ## In case we decide to validate federal grant details ##
   # validates :federal_grant_code, :federal_grant_serial_number, :federal_grant_title, presence: true, if: Proc.new { |a| a.funding_source == 'Federal' }

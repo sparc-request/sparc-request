8314a3cc0bed74f98027792d860157e02b592d67
Ito <ito.eta1@gmail.com>
2024-09-30 08:37:09 -0400

    added cache reset to migration

--- db/migrate/20240923125850_insert_system_admin_into_database.rb
+++ db/migrate/20240923125850_insert_system_admin_into_database.rb
@@ -1,5 +1,7 @@
 class InsertSystemAdminIntoDatabase < ActiveRecord::Migration[6.1]
   def change
+    Identity.reset_column_information
+
     Identity.create(ldap_uid: 'sparc_system_admin@musc.edu', first_name: 'System', last_name: 'Admin', system_admin: true, password: SecureRandom.hex(13), email: 'random_fake_email@musc.edu')
   end
 end

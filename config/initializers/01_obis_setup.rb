2ce99d12422b16c827602d6587f1d08d21d0a0db
jbvanmeter <james.blair.vanmeter@gmail.com>
2025-03-21 15:21:53 -0500

    wrapping remaining Settings value references from initializer files in config.to_prepare blocks

--- config/initializers/01_obis_setup.rb
+++ config/initializers/01_obis_setup.rb
@@ -18,16 +18,18 @@
 # INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-begin
-  wkhtmltopdf_location = Setting.get_value('wkhtmltopdf_location')
-rescue
-  wkhtmltopdf_location = nil
-end
+Rails.application.config.to_prepare do
+  begin
+    wkhtmltopdf_location = Setting.get_value('wkhtmltopdf_location')
+  rescue
+    wkhtmltopdf_location = nil
+  end
 
-if wkhtmltopdf_location
-  # Setup PDFKit
-  PDFKit.configure do |config|
-    config.wkhtmltopdf = wkhtmltopdf_location
+  if wkhtmltopdf_location
+    # Setup PDFKit
+    PDFKit.configure do |config|
+      config.wkhtmltopdf = wkhtmltopdf_location
+    end
   end
 end
 

ddf19f8047efeee9282e89d56cbf00e87c033217
Stuart Johnson <work.stuart.johnson@gmail.com>
2025-06-22 16:17:23 -0400

    fixing broken migration

--- db/migrate/20161108135128_add_version_col_to_study_type_question_group.rb
+++ db/migrate/20161108135128_add_version_col_to_study_type_question_group.rb
@@ -22,9 +22,9 @@ class AddVersionColToStudyTypeQuestionGroup < ActiveRecord::Migration[4.2]
   def up
     add_column :study_type_question_groups, :version, :integer, after: :id
     StudyTypeQuestionGroup.reset_column_information
-    StudyTypeQuestionGroup.find(1).update(version: 1)
-    StudyTypeQuestionGroup.find(2).update(version: 2)
-    StudyTypeQuestionGroup.find(3).update(version: 3)
+    StudyTypeQuestionGroup.find(1)&.update(version: 1)
+    StudyTypeQuestionGroup.find(2)&.update(version: 2)
+    StudyTypeQuestionGroup.find(3)&.update(version: 3)
     change_column :study_type_questions, :question, :text
   end
 

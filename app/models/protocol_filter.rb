37eafb61b84ab2ba973b5772e20abb990f85d4ad
Greg Mathias <mathiasgreg@gmail.com>
2025-07-14 14:31:35 -0400

    Fix merge conflict resolution error

--- app/models/protocol_filter.rb
+++ app/models/protocol_filter.rb
@@ -51,9 +51,9 @@ class ProtocolFilter < ApplicationRecord
 
   def self.search_filters
     if Setting.get_value("research_master_enabled")
-        ['Authorized User', 'CPT/EAP Code', 'NCT#', 'PI', 'Protocol ID', 'PRO#', 'RMID', 'Service', 'Short/Long Title']
+        ['Authorized User', 'CPT/EAP Code', 'NCT#', 'PI', 'Protocol ID', 'IRB#', 'RMID', 'Service', 'Short/Long Title']
     else
-        ['Authorized User', 'CPT/EAP Code','NCT#', 'PI', 'Protocol ID', 'PRO#',  'Service', 'Short/Long Title']
+        ['Authorized User', 'CPT/EAP Code','NCT#', 'PI', 'Protocol ID', 'IRB#',  'Service', 'Short/Long Title']
     end
   end
 end

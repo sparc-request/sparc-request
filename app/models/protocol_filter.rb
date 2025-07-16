a6421ebcad3c0cafc37b8a84d7d7ee604e2ca105
Greg Mathias <mathiasgreg@gmail.com>
2025-07-16 17:03:07 -0400

    Revert "SPOSDEV-1325-cpt: Add cpt/eap and service name search"
    This reverts commit c3d5b5a498fbeab222e57d49e74cfd5c93268736.

--- app/models/protocol_filter.rb
+++ app/models/protocol_filter.rb
@@ -51,9 +51,9 @@ class ProtocolFilter < ApplicationRecord
 
   def self.search_filters
     if Setting.get_value("research_master_enabled")
-        ['Authorized User', 'CPT/EAP Code', 'NCT#', 'PI', 'Protocol ID', 'IRB#', 'RMID', 'Service', 'Short/Long Title']
+        ['Authorized User', 'NCT#', 'PI', 'Protocol ID', 'IRB#', 'RMID', 'Short/Long Title']
     else
-        ['Authorized User', 'CPT/EAP Code','NCT#', 'PI', 'Protocol ID', 'IRB#',  'Service', 'Short/Long Title']
+        ['Authorized User', 'NCT#', 'PI', 'Protocol ID', 'IRB#', 'Short/Long Title']
     end
   end
 end

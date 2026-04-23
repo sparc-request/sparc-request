cbaa3872afe3e38963d77a70a7173d39702b39c8
jbvanmeter <james.blair.vanmeter@gmail.com>
2026-04-23 10:27:19 -0500

    require logger in config/boot.rb for rails 7.2

--- config/boot.rb
+++ config/boot.rb
@@ -23,5 +23,6 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 require 'execjs'
+require 'logger'
 
 ExecJS.runtime = ExecJS::Runtimes::Node

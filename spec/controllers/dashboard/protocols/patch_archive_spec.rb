43b76fe6777f7ad19be2106e388e4d09c75baf0d
Matthew Miller <mam449@musc.edu>
2026-06-08 14:25:18 -0400

    fix patch_archive_spec.rb

--- spec/controllers/dashboard/protocols/patch_archive_spec.rb
+++ spec/controllers/dashboard/protocols/patch_archive_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe Dashboard::ProtocolsController do
       allow(controller).to receive(:current_user).and_return(logged_in_user)
 
       protocol  = create(:study_without_validations, primary_pi: logged_in_user, archived: false)
-                  create(:service_request, protocol: protocol)
+                  create(:service_request, protocol: protocol, validate: false)
 
       expect {
         patch :archive, params: { id: protocol.id }, xhr: true

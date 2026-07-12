a629b8ad06426af50c535c062bbc719ffea613a2
Greg Mathias <mathiasgreg@gmail.com>
2026-07-12 16:22:34 -0400

    Patch other report that concatenates when multiple irb

--- app/lib/reports/service_requests.rb
+++ app/lib/reports/service_requests.rb
@@ -100,7 +100,7 @@ class ServiceRequestsReport < ReportingModule
       if params[:apr_data].include?("irb")
         attrs["IRB Checked Y/N"]      = "service_request.try(:protocol).try(:research_types_info).try(:human_subjects) ? 'Y' : 'N'"
         attrs["Number of IRBs"]       = "service_request.try(:protocol).irb_records.length"
-        attrs["If true, IRB #"]       = "service_request.try(:protocol).irb_records.length > 1 ? service_request.try(:protocol).irb_records.try{map.with_index(1){|m, index| ['IRB' + index.to_s + ': ' +  m.pro_number]}}.try(:join, ', ') : service_request.try(:protocol).try{irb_records.first}.try(:pro_number)"
+        attrs["If true, IRB #"]       = "service_request.try(:protocol).irb_records.length > 1 ? service_request.try(:protocol).irb_records.try{map.with_index(1){|m, index| ['IRB' + index.to_s + ': ' +  m.pro_number.to_s]}}.try(:join, ', ') : service_request.try(:protocol).try{irb_records.first}.try(:pro_number)"
         attrs["IRB Approval Date"]    = "service_request.try(:protocol).irb_records.length > 1 ? service_request.try(:protocol).irb_records.try{map.with_index(1){|m, index| ['IRB' + index.to_s + ': ' +  m.irb_approval_date.try(:strftime, '%D').to_s]}}.try(:join, ', ') : service_request.try(:protocol).try{irb_records.first}.try(:irb_approval_date).try(:strftime, '%D')"
         attrs["IRB Expiration Date"]  = "service_request.try(:protocol).irb_records.length > 1 ? service_request.try(:protocol).irb_records.try{map.with_index(1){|m, index| ['IRB' + index.to_s + ': ' +  m.irb_expiration_date.try(:strftime, '%D').to_s]}}.try(:join, ', ') : service_request.try(:protocol).try{irb_records.first}.try(:irb_expiration_date).try(:strftime, '%D')"
 
